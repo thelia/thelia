59e2b29e6af6bb57cacc0c6ffad8a81fe5ccf075
Manuel Raynaud <raynaud.manu@gmail.com>
2013-01-15 14:23:08 +0100

    remove constant THELIA_DB_CACHE

--- local/config/config_db.mysql.php
+++ local/config/config_db.mysql.php
@@ -14,9 +14,3 @@ define('THELIA_DB_PASSWORD', '__DB_PASSWORD__');
 
 //database DSN
 define('THELIA_DB_DSN','mysql:dbname=__DB_NAME__;host:__DB_HOST__');
-
-define('THELIA_DB_CACHE', 'file');
-//define('THELIA_DB_CACHE', 'apc');
-//define('THELIA_DB_CACHE', 'memcache');
-//define('THELIA_DB_CACHE', 'session');
-//define('THELIA_DB_CACHE', 'include');
--- local/config/config_db.pgsql.php
+++ local/config/config_db.pgsql.php
@@ -14,9 +14,3 @@ define('THELIA_BD_PASSWORD', '__DB_PASSWORD__');
 
 //database dsn
 define('THELIA_DB_DSN','pgsql:host=__DB_HOST__;port=__DB_PORT__;dbname=__DB_NAME__;user=__DB_LOGIN__;password=__DB_PASSWORD__');
-
-define('THELIA_DB_CACHE', 'file');
-//define('THELIA_DB_CACHE', 'apc');
-//define('THELIA_DB_CACHE', 'memcache');
-//define('THELIA_DB_CACHE', 'session');
-//define('THELIA_DB_CACHE', 'include');
