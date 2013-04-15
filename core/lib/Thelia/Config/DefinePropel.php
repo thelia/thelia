8db01fa85649f7e5f8e0d0611b1c7a837a468825
Manuel Raynaud <raynaud.manu@gmail.com>
2013-04-15 10:29:58 +0200

    update api documentation

--- core/lib/Thelia/Config/DefinePropel.php
+++ core/lib/Thelia/Config/DefinePropel.php
@@ -56,23 +56,4 @@ class DefinePropel {
                 )
         );
     }
-    /*
-     * $conf = array (
-  'datasources' =>
-  array (
-    'thelia' =>
-    array (
-      'adapter' => THELIA_DB_ADAPTER,
-      'connection' =>
-      array (
-        'dsn' => THELIA_DB_DSN,
-        'user' => THELIA_DB_USER,
-        'password' => THELIA_DB_PASSWORD,
-      ),
-    ),
-  ),
-  'generator_version' => '1.6.8',
-);
-return $conf;
-     */
 }
