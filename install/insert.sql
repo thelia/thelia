02c9184e530cb8717fdb34cee3670e62506853d2
Manuel Raynaud <raynaud.manu@gmail.com>
2013-07-05 14:47:15 +0200

    complete insert sql

--- install/insert.sql
+++ install/insert.sql
@@ -8,3 +8,17 @@ INSERT INTO `config` (`name`, `value`, `secured`, `hidden`, `created_at`, `updat
 ('session_config.default', '1', 1, 1, NOW(), NOW());
 
 INSERT INTO `module` (`code`, `type`, `activate`, `position`, `created_at`, `updated_at`) VALUES ('test', '1', '1', '1', NOW(), NOW());
+
+INSERT INTO `customer_title`(`id`, `by_default`, `position`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, NOW(), NOW()),
+(2, 0, 2, NOW(), NOW()),
+(3, 0, 3, NOW(), NOW());
+
+INSERT INTO `customer_title_i18n` (`id`, `locale`, `short`, `long`) VALUES
+(1, 'en_US', 'Mr', 'Mister'),
+(1, 'fr_FR', 'M', 'Monsieur'),
+(2, 'en_US', 'Mrs', 'Misses'),
+(2, 'fr_FR', 'Mme', 'Madame'),
+(3, 'en_US', 'Miss', 'Miss'),
+(3, 'fr_FR', 'Mlle', 'Madamemoiselle');
+
