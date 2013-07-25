72e9c139670d430c2cbb3430b4d90e7fabea2903
Manuel Raynaud <raynaud.manu@gmail.com>
2013-07-25 09:38:18 +0200

    put getCart into action classes

--- core/lib/Thelia/Action/Cart.php
+++ core/lib/Thelia/Action/Cart.php
@@ -91,4 +91,59 @@ class Cart implements EventSubscriberInterface
             "action.modifyArticle" => array("modifyArticle", 128),
         );
     }
+
+    public function getCart(Request $request)
+    {
+        if ($request->cookies->has("thelia_cart")) {
+            //le cookie de panier existe, on le récupère
+            $cookie = $request->cookies->get("thelia_cart");
+
+            $cart = CartQuery::create()->findOneByToken($cookie);
+
+            if ($cart) {
+                //le panier existe en base
+                $customer = $request->getSession()->getCustomerUser();
+
+                if ($customer) {
+                    if($cart->getCustomerId() != $customer->getId()) {
+                        //le customer du panier n'est pas le mm que celui connecté, il faut cloner le panier sans le customer_id
+                        $cart = $cart->duplicate($customer);
+                    }
+                } else {
+                    if ($cart->getCustomerId() != null) {
+                        //il faut dupliquer le panier sans le customer_id
+                        $cart = $cart->duplicate();
+                    }
+                }
+
+            } else {
+                $cart = $this->createCart();
+            }
+        } else {
+            //le cookie de panier n'existe pas, il va falloir le créer et faire un enregistrement en base.
+            $cart = $this->createCart();
+        }
+
+        return $cart;
+    }
+
+    public function createCart()
+    {
+
+    }
+
+
+    public function generateCookie()
+    {
+        $id = uniqid('', true);
+
+        setcookie("thelia_cart", $id, time());
+
+        return $id;
+    }
+
+    public function addItem()
+    {
+
+    }
 }
