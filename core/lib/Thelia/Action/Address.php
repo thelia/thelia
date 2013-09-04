0b9e2552ed60cc60938344cf24850ccdd10bbf22
Manuel Raynaud <raynaud.manu@gmail.com>
2013-09-04 08:30:24 +0200

    modify travis script

--- core/lib/Thelia/Action/Address.php
+++ core/lib/Thelia/Action/Address.php
@@ -63,6 +63,8 @@ class Address extends BaseAction implements EventSubscriberInterface
             ->setCompany($event->getCompany())
             ->save()
         ;
+
+        $event->setAddress($addressModel);
     }
 
     /**
