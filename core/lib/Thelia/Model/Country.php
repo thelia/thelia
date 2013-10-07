ea94a54280be069d2cac920650f329ecb489826a
Manuel Raynaud <raynaud.manu@gmail.com>
2013-10-07 10:59:58 +0200

    add pre/post CRUD method in Country model

--- core/lib/Thelia/Model/Country.php
+++ core/lib/Thelia/Model/Country.php
@@ -2,11 +2,48 @@
 
 namespace Thelia\Model;
 
+use Propel\Runtime\Connection\ConnectionInterface;
+use Thelia\Core\Event\Country\CountryEvent;
+use Thelia\Core\Event\TheliaEvents;
 use Thelia\Model\Base\Country as BaseCountry;
 
-class Country extends BaseCountry {
+class Country extends BaseCountry
+{
+    use \Thelia\Model\Tools\ModelEventDispatcherTrait;
 
+    public function preInsert(ConnectionInterface $con = null)
+    {
+        $this->dispatchEvent(TheliaEvents::BEFORE_CREATECOUNTRY, new CountryEvent($this));
+        return true;
+    }
 
+    public function postInsert(ConnectionInterface $con = null)
+    {
+        $this->dispatchEvent(TheliaEvents::AFTER_CREATECOUNTRY, new CountryEvent($this));
+    }
 
+    public function preUpdate(ConnectionInterface $con = null)
+    {
+        $this->dispatchEvent(TheliaEvents::BEFORE_UPDATECOUNTRY, new CountryEvent($this));
+
+        return true;
+    }
+
+    public function postUpdate(ConnectionInterface $con = null)
+    {
+        $this->dispatchEvent(TheliaEvents::AFTER_UPDATECOUNTRY, new CountryEvent($this));
+    }
+
+    public function preDelete(ConnectionInterface $con = null)
+    {
+        $this->dispatchEvent(TheliaEvents::BEFORE_DELETECOUNTRY, new CountryEvent($this));
+
+        return true;
+    }
+
+    public function postDelete(ConnectionInterface $con = null)
+    {
+        $this->dispatchEvent(TheliaEvents::AFTER_DELETECOUNTRY, new CountryEvent($this));
+    }
 
 }
