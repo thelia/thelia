10fee02fbf429718373e2981fa3e32b947ef8680
touffies <claffont@openstudio.fr>
2013-10-23 20:10:30 +0200

    Add isSubscribed method

--- core/lib/Thelia/Model/NewsletterQuery.php
+++ core/lib/Thelia/Model/NewsletterQuery.php
@@ -17,5 +17,8 @@ use Thelia\Model\Base\NewsletterQuery as BaseNewsletterQuery;
  */
 class NewsletterQuery extends BaseNewsletterQuery
 {
-
+    public static function isSubscribed($email)
+    {
+        return (null === self::create()->findOneByEmail($email)) ? false : true;
+    }
 } // NewsletterQuery
