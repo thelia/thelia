373fe132871c13944abbb46e1925b070a6e327cd
Manuel Raynaud <raynaud.manu@gmail.com>
2013-10-25 09:10:27 +0200

    save thelia version

--- install/insert.sql
+++ install/insert.sql
@@ -32,6 +32,11 @@ INSERT INTO `config` (`name`, `value`, `secured`, `hidden`, `created_at`, `updat
 ('contact_email','', 0, 0, NOW(), NOW()),
 ('url_site','', 0, 0, NOW(), NOW()),
 ('one_domain_foreach_lang','0', 1, 1, NOW(), NOW())
+('thelia_version','2.0.0-beta1', 1, 1, NOW(), NOW())
+('thelia_major_version','2', 1, 1, NOW(), NOW())
+('thelia_minus_version','0', 1, 1, NOW(), NOW())
+('thelia_release_version','0', 1, 1, NOW(), NOW())
+('thelia_extra_version','beta1', 1, 1, NOW(), NOW())
 ;
 
 
