186b338f48b8971465befa6111b52929e2ea84be
touffies <claffont@openstudio.fr>
2013-11-28 12:03:32 +0100

    Add new SEO form

---
+++ core/lib/Thelia/Form/SeoForm.php
@@ -0,0 +1,81 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Thelia	                                                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : info@thelia.net                                                      */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 3 of the License                */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*	    along with this program. If not, see <http://www.gnu.org/licenses/>.         */
+/*                                                                                   */
+/*************************************************************************************/
+namespace Thelia\Form;
+
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Class SeoForm
+ * @package Thelia\Form
+ * @author Christophe Laffont <claffont@openstudio.fr>
+ */
+class SeoForm extends BaseForm
+{
+    use SeoFieldsTrait;
+
+    /**
+     *
+     * in this function you add all the fields you need for your Form.
+     * Form this you have to call add method on $this->formBuilder attribute :
+     *
+     * $this->formBuilder->add("name", "text")
+     *   ->add("email", "email", array(
+     *           "attr" => array(
+     *               "class" => "field"
+     *           ),
+     *           "label" => "email",
+     *           "constraints" => array(
+     *               new \Symfony\Component\Validator\Constraints\NotBlank()
+     *           )
+     *       )
+     *   )
+     *   ->add('age', 'integer');
+     *
+     * @return null
+     */
+    protected function buildForm()
+    {
+
+        $this->formBuilder
+            ->add("id", "hidden", array(
+                "constraints" => array(
+                    new GreaterThan(array('value' => 0))
+                )
+            ))
+            ->add("locale", "hidden", array(
+                "constraints" => array(
+                    new NotBlank()
+                )
+            ))
+        ;
+
+        // Add SEO Fields
+        $this->addSeoFields();
+    }
+
+    public function getName()
+    {
+        return "thelia_seo";
+    }
+}
