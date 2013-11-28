81fa5a291c8223a08384d2cdcfb4495957f46d8e
Etienne Roudeix <etienne.roudeix@gmail.com>
2013-11-28 15:31:09 +0100

    fix tax rules issue

--- core/lib/Thelia/Controller/Admin/ProductController.php
+++ core/lib/Thelia/Controller/Admin/ProductController.php
@@ -1110,7 +1110,7 @@ class ProductController extends AbstractCrudController
             $this->dispatch(TheliaEvents::PRODUCT_COMBINATION_GENERATION, $event);
 
             // Log object modification
-            $this->adminLogAppend(sprintf("Combination generation for product reference %s", $event->getProduct()->getRef()));
+            $this->adminLogAppend($this->resourceCode, AccessManager::CREATE, sprintf("Combination generation for product reference %s", $event->getProduct()->getRef()));
 
            // Redirect to the success URL
            $this->redirect($changeForm->getSuccessUrl());
