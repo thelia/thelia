056116c554e3fba1386d36596ba0aae153857503
Manuel Raynaud <raynaud.manu@gmail.com>
2014-01-27 17:20:23 +0100

    remove blank line at top of thelia.sql file

--- install/thelia.sql
+++ install/thelia.sql
@@ -1,5 +1,4 @@
-﻿
-# This is a fix for InnoDB in MySQL >= 4.1.x
+﻿# This is a fix for InnoDB in MySQL >= 4.1.x
 # It "suspends judgement" for fkey relationships until are tables are set.
 SET FOREIGN_KEY_CHECKS = 0;
 
