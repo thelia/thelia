bc61ca3d191566957e88bed887f2cff1868feb87
Manuel Raynaud <raynaud.manu@gmail.com>
2014-02-03 13:41:40 +0100

    fix send mail order

--- core/lib/Thelia/Action/Order.php
+++ core/lib/Thelia/Action/Order.php
@@ -333,7 +333,7 @@ class Order extends BaseAction implements EventSubscriberInterface
      */
     public function sendOrderEmail(OrderEvent $event)
     {
-        $contact_email = ConfigQuery::read('contact_email');
+        $contact_email = ConfigQuery::read('store_email');
 
         if ($contact_email) {
 
@@ -358,11 +358,11 @@ class Order extends BaseAction implements EventSubscriberInterface
 
             $instance = \Swift_Message::newInstance()
                 ->addTo($customer->getEmail(), $customer->getFirstname()." ".$customer->getLastname())
-                ->addFrom(ConfigQuery::read('contact_email'), ConfigQuery::read('company_name'))
+                ->addFrom($contact_email, ConfigQuery::read('store_name'))
             ;
 
             // Build subject and body
-            $message->build($parser, $instance);
+            $message->buildMessage($parser, $instance);
 
             $this->getMailer()->send($instance);
         }
