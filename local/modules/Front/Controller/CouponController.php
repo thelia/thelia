784ab68ca53eb3a76d169e3bccc695d10b743bcd
Franck Allimant <franck@cqfdev.fr>
2014-04-18 00:06:49 +0200

    Restored previous version

--- local/modules/Front/Controller/CouponController.php
+++ local/modules/Front/Controller/CouponController.php
@@ -31,7 +31,6 @@ use Thelia\Form\Exception\FormValidationException;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Log\Tlog;
 use Thelia\Model\AddressQuery;
-use Thelia\Module\Exception\DeliveryException;
 
 /**
  * Class CouponController
@@ -75,23 +74,12 @@ class CouponController extends BaseFrontController
 
                 if (null !== $deliveryModule && null !== $deliveryAddress) {
                     $moduleInstance = $this->container->get(sprintf('module.%s', $deliveryModule->getCode()));
+                    $postage = $moduleInstance->getPostage($deliveryAddress->getCountry());
 
                     $orderEvent = new OrderEvent($order);
+                    $orderEvent->setPostage($postage);
 
-                    try {
-                        $postage = $moduleInstance->getPostage($deliveryAddress->getCountry());
-
-                        $orderEvent->setPostage($postage);
-
-                        $this->getDispatcher()->dispatch(TheliaEvents::ORDER_SET_POSTAGE, $orderEvent);
-                    }
-                    catch (DeliveryException $ex) {
-                        // The postage has been chosen, but changes dues to coupon causes an exception.
-                        // Reset the postage data in the order
-                        $orderEvent->setDeliveryModule(0);
-
-                        $this->getDispatcher()->dispatch(TheliaEvents::ORDER_SET_DELIVERY_MODULE, $orderEvent);
-                    }
+                    $this->getDispatcher()->dispatch(TheliaEvents::ORDER_SET_POSTAGE, $orderEvent);
                 }
             }
 
