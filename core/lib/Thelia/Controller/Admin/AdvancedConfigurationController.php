6e5c461c79af020e05254986ef100c19af3e044d
Franck Allimant <franck@cqfdev.fr>
2014-04-24 16:24:20 +0200

    Replaces the CacheController

---
+++ core/lib/Thelia/Controller/Admin/AdvancedConfigurationController.php
@@ -0,0 +1,60 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace Thelia\Controller\Admin;
+
+use Thelia\Core\Event\Cache\CacheEvent;
+use Thelia\Core\Event\TheliaEvents;
+use Thelia\Core\Security\AccessManager;
+use Thelia\Core\Security\Resource\AdminResources;
+use Thelia\Form\Cache\CacheFlushForm;
+use Thelia\Form\Exception\FormValidationException;
+
+/**
+ * Class CacheController
+ * @package Thelia\Controller\Admin
+ * @author Manuel Raynaud <mraynaud@openstudio.fr>
+ */
+class AdvancedConfigurationController extends BaseAdminController
+{
+
+    public function defaultAction()
+    {
+        if (null !== $result = $this->checkAuth(AdminResources::ADVANCED_CONFIGURATION, [], AccessManager::VIEW)) {
+            return $result;
+        }
+
+        return $this->render('advanced-configuration');
+    }
+
+    public function flushCacheAction()
+    {
+        if (null !== $result = $this->checkAuth(AdminResources::ADVANCED_CONFIGURATION, [], AccessManager::UPDATE)) {
+            return $result;
+        }
+
+        $form = new CacheFlushForm($this->getRequest());
+        try {
+            $this->validateForm($form);
+
+            $event = new CacheEvent($this->container->getParameter("kernel.cache_dir"));
+            $this->dispatch(TheliaEvents::CACHE_CLEAR, $event);
+
+            $event = new CacheEvent(THELIA_WEB_DIR . "assets");
+            $this->dispatch(TheliaEvents::CACHE_CLEAR, $event);
+
+            $this->redirectToRoute('admin.configuration.advanced');
+        } catch (FormValidationException $e) {
+
+        }
+    }
+}
