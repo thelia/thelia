7ab119fbaac4fc90573439c0bcdc126cf7ddea22
Franck Allimant <franck@cqfdev.fr>
2014-04-24 12:28:52 +0200

    Added missing permissions

--- setup/update/2.0.1.sql
+++ setup/update/2.0.1.sql
@@ -9,5 +9,59 @@ UPDATE `config` SET `value`='' WHERE `name`='thelia_extra_version';
 
 ALTER TABLE `module` ADD INDEX `idx_module_activate` (`activate`);
 
+SELECT @max := MAX(`id`) FROM `resource`;
+SET @max := @max+1;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+(@max, 'admin.configuration.store', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+(@max, 'en_US', 'Store information configuration'),
+(@max, 'fr_FR', 'Informations sur la boutique');
+
+SET @max := @max+1;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+(@max, 'admin.configuration.variable', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+(@max, 'en_US', 'Configuration variables'),
+(@max, 'fr_FR', 'Variables de configuration');
+
+SET @max := @max+1;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+  (@max, 'admin.configuration.admin-logs', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+  (@max, 'en_US', 'View administration logs'),
+  (@max, 'fr_FR', 'Consulter les logs d\'administration');
+
+SET @max := @max+1;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+  (@max, 'admin.configuration.system-logs', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+  (@max, 'en_US', 'Logging system configuration'),
+  (@max, 'fr_FR', 'Configuration du système de log');
+
+SET @max := @max+1;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+  (@max, 'admin.configuration.advanced', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+  (@max, 'en_US', 'Advanced configuration'),
+  (@max, 'fr_FR', 'Configuration avancée');
+
+SET @max := @max+1;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+  (@max, 'admin.configuration.translations', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+  (@max, 'en_US', 'Translations'),
+  (@max, 'fr_FR', 'Traductions');
 
 SET FOREIGN_KEY_CHECKS = 1;
