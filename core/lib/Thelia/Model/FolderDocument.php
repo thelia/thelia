6084d9ac00ae4565ee096c9615ed3ecda97f24b4
Manuel Raynaud <raynaud.manu@gmail.com>
2014-06-11 14:27:36 +0200

    implement new breadcrumb on image and document edition in the catalog. Fix #460

--- core/lib/Thelia/Model/FolderDocument.php
+++ core/lib/Thelia/Model/FolderDocument.php
@@ -2,13 +2,18 @@
 
 namespace Thelia\Model;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Router;
 use Thelia\Model\Base\FolderDocument as BaseFolderDocument;
 use Propel\Runtime\Connection\ConnectionInterface;
+use Thelia\Model\Breadcrumb\BreadcrumbInterface;
+use Thelia\Model\Breadcrumb\FolderBreadcrumbTrait;
 
-class FolderDocument extends BaseFolderDocument
+class FolderDocument extends BaseFolderDocument implements BreadcrumbInterface
 {
     use \Thelia\Model\Tools\ModelEventDispatcherTrait;
     use \Thelia\Model\Tools\PositionManagementTrait;
+    use FolderBreadcrumbTrait;
 
     /**
      * Calculate next position relative to our parent
@@ -61,5 +66,10 @@ class FolderDocument extends BaseFolderDocument
         );
 
         return true;
+    }
+
+    public function getBreadcrumb(Router $router, ContainerInterface $container, $tab)
+    {
+        return $this->getFolderBreadcrumb($router, $container, $tab);
     }
 }
--- core/lib/Thelia/Model/FolderImage.php
+++ core/lib/Thelia/Model/FolderImage.php
@@ -2,13 +2,18 @@
 
 namespace Thelia\Model;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Router;
 use Thelia\Model\Base\FolderImage as BaseFolderImage;
 use Propel\Runtime\Connection\ConnectionInterface;
+use Thelia\Model\Breadcrumb\BreadcrumbInterface;
+use Thelia\Model\Breadcrumb\FolderBreadcrumbTrait;
 
-class FolderImage extends BaseFolderImage
+class FolderImage extends BaseFolderImage implements BreadcrumbInterface
 {
     use \Thelia\Model\Tools\ModelEventDispatcherTrait;
     use \Thelia\Model\Tools\PositionManagementTrait;
+    use FolderBreadcrumbTrait;
 
     /**
      * Calculate next position relative to our parent
@@ -61,5 +66,16 @@ class FolderImage extends BaseFolderImage
         );
 
         return true;
+    }
+
+    /**
+     *
+     * return the complete breadcrumb for a given resource.
+     *
+     * @return array
+     */
+    public function getBreadcrumb(Router $router, ContainerInterface $container, $tab)
+    {
+        return $this->getFolderBreadcrumb($router, $container, $tab);
     }
 }
