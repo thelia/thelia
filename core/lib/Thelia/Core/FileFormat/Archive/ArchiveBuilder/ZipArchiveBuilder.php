2702c20014f20a2079a9800ff1fa88109b3ed05a
Benjamin Perche <bperche9@gmail.com>
2014-07-04 09:46:21 +0200

    Fix Zip add file, we must first commit and then delete the temporary file 	modifié:         core/lib/Thelia/Core/FileFormat/Archive/ArchiveBuilder/ZipArchiveBuilder.php

--- core/lib/Thelia/Core/FileFormat/Archive/ArchiveBuilder/ZipArchiveBuilder.php
+++ core/lib/Thelia/Core/FileFormat/Archive/ArchiveBuilder/ZipArchiveBuilder.php
@@ -116,10 +116,10 @@ class ZipArchiveBuilder extends AbstractArchiveBuilder
             throw new \ErrorException($translatedErrorMessage);
         }
 
-        // If not too
-        unlink($fileDownloadCache);
-
         $this->commit();
+
+        // Delete the temp file
+        unlink($fileDownloadCache);
 
         return $this;
     }
