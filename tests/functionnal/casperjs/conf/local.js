473402bf8889b9bc226d326349d07987ea8529e9
Julien Chanseaume <julien.chanseaume@gmail.com>
2014-08-11 11:28:23 +0200

    Added functionnal test for front office : home, register, login, cart

--- tests/functionnal/casperjs/conf/local.js
+++ tests/functionnal/casperjs/conf/local.js
@@ -1,3 +1,107 @@
-//LOCAL = ton pc
-var thelia2_base_url = 'http://www.thelia2.dev/index_dev.php/';
-casper.test.done(0);
+// LOCAL = ton pc
+
+var system = require('system');
+
+var thelia2_base_url = '';
+
+
+if (casper.cli.has('thelia2_base_url')){
+    thelia2_base_url = casper.cli.get('thelia2_base_url');
+    casper.test.comment('CasperJS will use Thelia on : '+ thelia2_base_url);
+} else if (system.env.thelia2_base_url){
+    thelia2_base_url = system.env.thelia2_base_url;
+    casper.test.comment('CasperJS will use Thelia (ENV) on : '+ thelia2_base_url);
+} else {
+    thelia2_base_url = 'http://dev.thelia.net/index_dev.php/';
+    casper.test.comment('CasperJS will use fallback URL for tests : '+ thelia2_base_url);
+    casper.test.comment('If you want to use custom URL just set the environment variable `thelia2_base_url`');
+}
+
+var screenshot_dir = 'tests/functionnal/casperjs/screenshot/';
+
+
+casper.options.viewportSize = {width: 1024, height: 768};
+
+var viewports = [
+    {
+        'name': 'samsung-galaxy_y-portrait',
+        'viewport': {width: 240, height: 320}
+    },
+    {
+        'name': 'samsung-galaxy_y-landscape',
+        'viewport': {width: 320, height: 240}
+    },
+    {
+        'name': 'iphone5-portrait',
+        'viewport': {width: 320, height: 568}
+    },
+    {
+        'name': 'iphone5-landscape',
+        'viewport': {width: 568, height: 320}
+    },
+    {
+        'name': 'htc-one-portrait',
+        'viewport': {width: 360, height: 640}
+    },
+    {
+        'name': 'htc-one-landscape',
+        'viewport': {width: 640, height: 360}
+    },
+    {
+        'name': 'nokia-lumia-920-portrait',
+        'viewport': {width: 240, height: 320}
+    },
+    {
+        'name': 'nokia-lumia-920-landscape',
+        'viewport': {width: 320, height: 240}
+    },
+    {
+        'name': 'google-nexus-7-portrait',
+        'viewport': {width: 603, height: 966}
+    },
+    {
+        'name': 'google-nexus-7-landscape',
+        'viewport': {width: 966, height: 603}
+    },
+    {
+        'name': 'ipad-portrait',
+        'viewport': {width: 768, height: 1024}
+    },
+    {
+        'name': 'ipad-landscape',
+        'viewport': {width: 1024, height: 768}
+    },
+    {
+        'name': 'desktop-standard-vga',
+        'viewport': {width: 640, height: 480}
+    },
+    {
+        'name': 'desktop-standard-svga',
+        'viewport': {width: 800, height: 600}
+    },
+    {
+        'name': 'desktop-standard-hd',
+        'viewport': {width: 1280, height: 720}
+    },
+    {
+        'name': 'desktop-standard-sxga',
+        'viewport': {width: 1280, height: 1024}
+    },
+    {
+        'name': 'desktop-standard-sxga-plus',
+        'viewport': {width: 1400, height: 1050}
+    },
+    {
+        'name': 'desktop-standard-uxga',
+        'viewport': {width: 1600, height: 1200}
+    },
+    {
+        'name': 'desktop-standard-wuxga',
+        'viewport': {width: 1920, height: 1200}
+    },
+
+];
+
+;
+
+casper.test.done();
---
+++ tests/functionnal/casperjs/exe/front/40_cart.js
@@ -0,0 +1,51 @@
+casper.test.comment('== Cart ==');
+
+casper.test.begin('Cart', 4, function suite(test) {
+
+    var productUrl = '';
+
+    casper.start(thelia2_base_url, function() {
+
+        casper.viewport(1280, 1024)
+
+        productUrl = this.getElementAttribute('a.product-info', 'href');
+
+        this.echo("product : " + productUrl);
+
+        casper.thenOpen(productUrl, function() {
+            this.echo(this.getTitle());
+        });
+
+    });
+
+    casper.wait(2000, function(){
+        this.capture(screenshot_dir + 'front/40_product.png');
+        this.click("#pse-submit");
+    });
+
+    casper.wait(2000, function() {
+        this.captureSelector(screenshot_dir + 'front/40_added-to-cart.png', '.bootbox');
+        test.assertSelectorHasText('.bootbox h3', 'The product has been added to your cart');
+    });
+
+
+    casper.then(function(){
+
+        this.thenOpen(thelia2_base_url + "cart", function() {
+            this.echo(this.getTitle());
+            test.assertExists("#cart .table-cart", "Cart table exists");
+            test.assertElementCount("#cart .table-cart tbody tr", 2, "Cart contains 1 product")
+            var link = this.getElementInfo('#cart .table-cart tbody tr a.thumbnail');
+            //require('utils').dump(link);
+            test.assertTruthy( link.attributes.href == productUrl, "This is the right product in cart");
+            this.capture(screenshot_dir + 'front/40_cart.png');
+        });
+
+    });
+
+
+    casper.run(function() {
+        test.done();
+    });
+
+});
