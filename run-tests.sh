ebb20e075e5c2ad839120fb45cfa6e3a43375485
Julien Chanseaume <julien.chanseaume@gmail.com>
2014-08-11 14:19:45 +0200

    make a uniq script to execute phpunit and casperjs

---
+++ run-tests.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env/sh
+
+# will exit with non-zero error code if any of the command fails
+set -e
+
+echo "phpunit"
+phpunit
+
+echo "CasperJS"
+cd ../casperjs
+"DISPLAY=:99.0 ./bin/casperjs test ../thelia/tests/functionnal/casperjs/exe/front/ --pre=../thelia/tests/functionnal/casperjs/conf/local.js --verbose --thelia2_base_url='http://localhost:8000/index.php/' --thelia2_screenshot_path='../thelia/tests/functionnal/casperjs/screenshot/'"
