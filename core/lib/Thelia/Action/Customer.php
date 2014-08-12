051dbbef9660cda36ac05c7e528a5851c93a7183
Riotteau <griotteau@eskape.fr>
2014-08-12 10:03:06 +0200

    Success message not displayed when new password email is sent

--- core/lib/Thelia/Action/Customer.php
+++ core/lib/Thelia/Action/Customer.php
@@ -210,7 +210,7 @@ class Customer extends BaseAction implements EventSubscriberInterface
                 $message->setLocale($locale);
 
                 if (false === $message) {
-                    throw new \Exception("Failed to load message 'order_confirmation'.");
+                    throw new \Exception("Failed to load message 'lost_password'.");
                 }
 
                 $this->parser->assign('password', $password);
@@ -227,6 +227,8 @@ class Customer extends BaseAction implements EventSubscriberInterface
                 $this->mailer->send($instance);
 
             }
+        } else {
+            throw new \Exception("Unknown Store email address");
         }
     }
 
