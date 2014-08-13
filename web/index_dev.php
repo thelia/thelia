f6b76a1953b8b496413ced0ff33501d5ba49216f
Riotteau <griotteau@eskape.fr>
2014-08-13 14:20:49 +0200

    Add network whitelist for dev environment

--- web/index_dev.php
+++ web/index_dev.php
@@ -25,11 +25,32 @@ $trustedIp = array(
   '::1',
   '127.0.0.1',
 );
+// Allowed Network
+$trustedNetwork = '192.168.0.0/24'; 
 
+
+/**
+ * Check if an IP is in a network
+ * @param string $IP
+ * @param string $network
+ * @return boolean
+ */
+function ip_in_network ($IP, $network) {
+    if (!$IP or !$network or !strpos($network,'/')) return false;
+    list ($net, $mask) = explode ("/", $network);   
+    $ip_net = ip2long ($net);
+    $ip_mask = ~((1 << (32 - $mask)) - 1);
+    $ip_ip = ip2long ($IP);
+    $ip_ip_net = $ip_ip & $ip_mask;
+    return ($ip_ip_net == $ip_net);
+}
+  
+  
 $request = Request::createFromGlobals();
 $thelia = new Thelia("dev", true);
 
-if ( false === in_array($request->getClientIp(), $trustedIp)) {
+if ( (false === in_array($request->getClientIp(), $trustedIp)) and
+     (false === ($trustedNetwork and ip_in_network($request->getClientIp(), $trustedNetwork)))) {
     $response = Response::create('Forbidden', 403)->send();
     $thelia->terminate($request, $response);
 } else {
@@ -37,5 +58,3 @@ if ( false === in_array($request->getClientIp(), $trustedIp)) {
     $thelia->terminate($request, $response);
 
 }
-
-
