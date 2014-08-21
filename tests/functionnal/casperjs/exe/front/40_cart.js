578824c0480036b4e4825c46c4f3cefdaf54529d
Julien Chanséaume <jchanseaume@openstudio.fr>
2014-08-21 09:44:45 +0200

    Modified tests on cart

--- tests/functionnal/casperjs/exe/front/40_cart.js
+++ tests/functionnal/casperjs/exe/front/40_cart.js
@@ -10,7 +10,7 @@ casper.test.begin('Cart', 5, function suite(test) {
 
         this.echo("product : " + productUrl);
 
-        casper.thenOpen(productUrl, function() {
+        this.thenOpen(productUrl, function() {
             this.echo(this.getTitle());
         });
 
@@ -23,21 +23,22 @@ casper.test.begin('Cart', 5, function suite(test) {
         this.click("#pse-submit");
     });
 
+    /*
     casper.wait(thelia_default_timeout, function() {
         this.echo(this.getHTML());
         this.captureSelector(screenshot_dir + 'front/40_added-to-cart.png', '.bootbox');
         test.assertSelectorHasText('.bootbox h3', 'The product has been added to your cart');
     });
-
+    */
 
     casper.wait(thelia_default_timeout, function() {
 
         this.thenOpen(thelia2_base_url + "cart", function() {
             this.echo(this.getTitle());
+            this.echo(this.getHTML());
             test.assertExists("#cart .table-cart", "Cart table exists");
             test.assertElementCount("#cart .table-cart tbody tr", 2, "Cart contains 1 product")
             var link = this.getElementInfo('#cart .table-cart tbody tr a.thumbnail');
-            //require('utils').dump(link);
             test.assertTruthy( link.attributes.href == productUrl, "This is the right product in cart");
             this.capture(screenshot_dir + 'front/40_cart.png');
         });
