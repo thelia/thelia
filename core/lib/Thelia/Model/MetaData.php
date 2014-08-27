6a51035cbb5c788428f78469f3afde1b7af0da0b
Julien Chanséaume <jchanseaume@openstudio.fr>
2014-08-27 16:32:05 +0200

    Added constant for meta element key

--- core/lib/Thelia/Model/MetaData.php
+++ core/lib/Thelia/Model/MetaData.php
@@ -7,6 +7,30 @@ use Thelia\Model\Base\MetaData as BaseMetaData;
 class MetaData extends BaseMetaData
 {
 
+    const CATEGORY_KEY     = 'category';
+    const PRODUCT_KEY      = 'product';
+    const PSE_KEY          = 'pse';
+    const FEATURE_KEY      = 'feature';
+    const FEATURE_AV_KEY   = 'feature_av';
+    const ATTRIBUTE_KEY    = 'attribute';
+    const ATTRIBUTE_AV_KEY = 'attribute_av';
+    const BRAND_KEY        = 'brand_av';
+
+    const FOLDER_KEY  = 'folder';
+    const CONTENT_KEY = 'content';
+
+    const ORDER_KEY = 'order';
+
+    const MODULE_KEY = 'module';
+
+    const CUSTOMER_KEY = 'customer';
+    const ADDRESS_KEY  = 'address';
+
+    const CURRENCY_KEY = 'currency';
+    const COUNTRY_KEY  = 'country';
+    const LANG_KEY     = 'lang';
+
+
     public function getValue()
     {
         $data = parent::getValue();
