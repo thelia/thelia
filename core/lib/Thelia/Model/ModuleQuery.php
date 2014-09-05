fa26732077380e9d6beacf013cd4351cf1cd9133
Jérôme Billiras <jerome.billiras+github@gmail.com>
2014-09-05 10:06:10 +0200

    Mess in comment section

--- core/lib/Thelia/Model/ModuleQuery.php
+++ core/lib/Thelia/Model/ModuleQuery.php
@@ -12,8 +12,6 @@ use Thelia\Module\BaseModule;
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
  * long as it does not already exist in the output directory.
- *
- *
  */
 class ModuleQuery extends BaseModuleQuery
 {
