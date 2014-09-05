20485f403bb505e5ab1ce89411e54e40952c21b6
Julien Chanséaume <jchanseaume@openstudio.fr>
2014-09-05 17:09:43 +0200

    added exception for Invalid Module

---
+++ core/lib/Thelia/Exception/InvalidModuleException.php
@@ -0,0 +1,25 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace Thelia\Exception;
+
+use OutOfBoundsException;
+
+
+/**
+ * Class InvalidModuleException
+ * @package Thelia\Exception
+ * @author Julien Chanséaume <jchanseaume@openstudio.fr>
+ */
+class InvalidModuleException extends OutOfBoundsException{
+
+} 
