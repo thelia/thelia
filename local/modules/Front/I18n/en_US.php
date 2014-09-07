1577ff96ee172f30082a2e0931cab560d8f1aca6
Franck Allimant <franck@cqfdev.fr>
2014-09-07 19:36:16 +0200

    Added missing translation to front module

---
+++ local/modules/Front/I18n/en_US.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'Please check your input: %s' => 'Please check your input: %s',
+    'Sorry, an error occured: %s' => 'Sorry, an error occured: %s',
+    'Wrong email or password. Please try again' => 'Wrong email or password. Please try again',
+    'You\'re currently logged in. Please log out before requesting a new password.' => 'You\'re currently logged in. Please log out before requesting a new password.',
+);
