8b50190f101790f94d3a8dfaaeb584bd4fa458fb
Franck Allimant <franck@cqfdev.fr>
2014-09-08 13:42:13 +0200

    Added getFromName() method

--- core/lib/Thelia/Model/MessageQuery.php
+++ core/lib/Thelia/Model/MessageQuery.php
@@ -16,4 +16,19 @@ use Thelia\Model\Base\MessageQuery as BaseMessageQuery;
  */
 class MessageQuery extends BaseMessageQuery
 {
+    /**
+     * Load a message from its name, throwing an excemtoipn is none is found.
+     *
+     * @param string $messageName the message name
+     * @return Message the loaded message
+     * @throws \Exception if the message could not be loaded
+     */
+    public static function getFromName($messageName) {
+
+        if (false === $message = MessageQuery::create()->filterByName($messageName)->findOne()) {
+            throw new \Exception("Failed to load message $messageName.");
+        }
+
+        return $message;
+    }
 } // MessageQuery
