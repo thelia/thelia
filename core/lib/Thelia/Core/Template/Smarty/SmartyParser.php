bf271187a273525a0b4071288d2f8ea88c32f70b
Manuel Raynaud <raynaud.manu@gmail.com>
2014-09-20 22:36:01 +0200

    update smarty version

--- core/lib/Thelia/Core/Template/Smarty/SmartyParser.php
+++ core/lib/Thelia/Core/Template/Smarty/SmartyParser.php
@@ -79,6 +79,7 @@ class SmartyParser extends Smarty implements ParserInterface
 
         $this->setCompileDir($compile_dir);
         $this->setCacheDir($cache_dir);
+        $this->inheritance_merge_compiled_includes = false;
 
         // Prevent smarty ErrorException: Notice: Undefined index bla bla bla...
         $this->error_reporting = E_ALL ^ E_NOTICE;
