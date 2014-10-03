a23a3710933e4d36d5e3b4ade4c11f703e60c81b
Julien Chanseaume <julien.chanseaume@gmail.com>
2014-10-03 06:15:43 +0200

    Added missing translations

--- local/modules/Front/I18n/fr_FR.php
+++ local/modules/Front/I18n/fr_FR.php
@@ -1,8 +1,20 @@
 <?php
 
 return array(
+    'Coupon code can\'t be empty' => 'Le code promo ne peut être vide',
+    'Delivery address does not belong to the current customer' => 'L\'adresse de livraison n\'appartient pas au client en cours',
+    'Delivery module cannot be use with selected delivery address' => 'Le module de livraison ne peut pas être utilisé avec cette adresse de livraison',
+    'Error during address deletion process' => 'Désolé. Une erreur s\'est produite lors de la suppression de l\'adresse',
+    'Failed to add this article to your cart, please try again' => 'Impossible d\'ajouter cet article à votre panier. Merci de ré-essayer.',
+    'Invoice address does not belong to the current customer' => 'L\'adresse de facturation n\'appartient pas au client en cours',
+    'No placed order' => 'Aucune commande passée',
+    'Please check your coupon code: %message' => 'Merci de vérifier votre code promo : %message',
     'Please check your input: %s' => 'Merci de vérifier les informations indiquées : %s',
+    'Received failed order id does not belong to the current customer' => 'L\'id de commande refusée n\'appartient pas au client en cours',
+    'Received placed order id does not belong to the current customer' => 'L\'id de commande passée n\'appartient pas au client en cours',
     'Sorry, an error occured: %s' => 'Désolé. Une erreur s\'est produite : %s',
+    'Sorry, an error occurred: %message' => 'Désolé. Une erreur s\'est produite : %s',
+    'The file [%file] does not exist' => 'Le fichier [%file] n\'existe pas',
     'Wrong email or password. Please try again' => 'Adresse email ou mot de passe incorrect. Merci de ré-essayer.',
     'You\'re currently logged in. Please log out before requesting a new password.' => 'Vous être actuellement connecté au site. Vous devez vous déconnecter pour demander un nouveau mot de passe.',
 );
