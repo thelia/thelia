91b2769795dd92c8cd9528848017f670f20bda4b
Manuel Raynaud <raynaud.manu@gmail.com>
2014-10-03 15:31:31 +0200

    refactor functional test suite for virtual product case

--- tests/functionnal/casperjs/exe/front/50-order-process.js
+++ tests/functionnal/casperjs/exe/front/50-order-process.js
@@ -16,14 +16,19 @@ casper.test.begin('Order process', 4, function suite(test) {
 
     casper.thenOpen(thelia2_base_url + "order/delivery", function() {
 
-        test.assertTitle("Billing and delivery - Cart - Thelia V2", "title is the one expected for url : " + this.getCurrentUrl());
-        this.capture(screenshot_dir + 'front/50_delivery_list.png');
+        if (this.getCurrentUrl() == thelia2_base_url + "order/invoice") {
+            test.info("with a virtual product, the delivery page is skipped");
+            test.skip(2);
+        } else {
+            test.assertTitle("Billing and delivery - Cart - Thelia V2", "title is the one expected for url : " + this.getCurrentUrl());
+            this.capture(screenshot_dir + 'front/50_delivery_list.png');
 
-        test.assertEval(function() {
-            return __utils__.findAll("#form-cart-delivery table.table-address tr").length >= 1;
-        }, "We expect at least one delivery address");
+            test.assertEval(function() {
+                return __utils__.findAll("#form-cart-delivery table.table-address tr").length >= 1;
+            }, "We expect at least one delivery address");
 
-        this.click('form#form-cart-delivery button[type="submit"]');
+            this.click('form#form-cart-delivery button[type="submit"]');
+        }
     });
 
     casper.wait(thelia_default_timeout, function(){
