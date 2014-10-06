3293c8a037e2511abd79a1014689ef4149d0f36d
Manuel Raynaud <raynaud.manu@gmail.com>
2014-10-06 13:10:03 +0200

    update sql for 2.1-alpha1

--- setup/update/2.1.0-alpha1.sql
+++ setup/update/2.1.0-alpha1.sql
@@ -2119,13 +2119,31 @@ INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
   (@max_id+8, 'fr_FR', 'Gestion des hooks'),
   (@max_id+9, 'en_US', 'Data import / export'),
   (@max_id+9, 'fr_FR', 'Importation / exportation de données'),
-  (@max_id+10, 'en_US', 'Modules maagement'),
+  (@max_id+10, 'en_US', 'Modules management'),
   (@max_id+10, 'fr_FR', 'Gestion des modules'),
   (@max_id+11, 'en_US', 'Administration profiles management'),
   (@max_id+11, 'fr_FR', 'Gestion des profils d''administration'),
   (@max_id+12, 'en_US', 'Back-office search function'),
   (@max_id+12, 'fr_FR', 'Fonction de recherche dans l''administration')
 ;
+
+# ======================================================================================================================
+# Adding cart id in order table
+# ======================================================================================================================
+
+ALTER TABLE  `order`
+  ADD  `cart_id` INTEGER
+  AFTER  `lang_id`;
+
+ALTER TABLE  `order_version`
+  ADD  `cart_id` INTEGER
+  AFTER  `lang_id`;
+
+ALTER TABLE  `order_version`
+  ADD  `customer_id_version` INTEGER DEFAULT 0
+  AFTER  `version_created_by`;
+
+
 # ======================================================================================================================
 # End of changes
 # ======================================================================================================================
