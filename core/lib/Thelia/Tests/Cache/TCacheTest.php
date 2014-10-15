1f4ec79150714676e209421c2ac36a8ffa57feb6
Julien Chanseaume <julien.chanseaume@gmail.com>
2014-10-15 14:41:57 +0200

    Fixed cache test

--- core/lib/Thelia/Tests/Cache/TCacheTest.php
+++ core/lib/Thelia/Tests/Cache/TCacheTest.php
@@ -14,31 +14,26 @@
 namespace Thelia\Tests\Cache;
 
 use Thelia\Cache\CacheFactory;
+use Thelia\Cache\Driver\BaseCacheDriver;
 use Thelia\Model\ConfigQuery;
 
-class TCacheTest extends \PHPUnit_Framework_TestCase {
-
+class TCacheTest extends \PHPUnit_Framework_TestCase
+{
     const NS_NULL_DRIVER = '\Thelia\Cache\Driver\NullDriver';
     const NS_FILE_DRIVER = '\Thelia\Cache\Driver\FileDriver';
-    const NS_APC_DRIVER = '\Thelia\Cache\Driver\ApcDriver';
-
 
 
     public function testInit()
     {
+        $cacheFactory = new CacheFactory();
 
-        ConfigQuery::write(CacheFactory::CONFIG_CACHE_DRIVER, self::NS_FILE_DRIVER);
+        ConfigQuery::write(BaseCacheDriver::CONFIG_DRIVER, self::NS_NULL_DRIVER);
 
-        $cache = CacheFactory::getInstance();
+        $cache = $cacheFactory->get();
 
-        assertInstanceOf(self::NS_FILE_DRIVER, $cache);
+        $this->assertInstanceOf(self::NS_NULL_DRIVER, $cache);
 
         $cache->save("FILE_KEY_1", "FILE CONTENT KEY 1");
-        $this->assertEquals("FILE CONTENT KEY 1", $cache->fetch("FILE_KEY_1"));
-
+        $this->assertEquals(false, $cache->fetch("FILE_KEY_1"));
     }
-
-
 }
-
-
