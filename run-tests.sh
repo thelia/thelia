edf6b39dc231d7b1681ee9c92de305a8f79551b7
Julien Chanseaume <julien.chanseaume@gmail.com>
2014-10-17 10:49:43 +0200

    Updated script to execute the new tests

--- run-tests.sh
+++ run-tests.sh
@@ -23,4 +23,4 @@ cd ../casperjs
 export DISPLAY=:99.0
 pwd
 ls -la
-./bin/casperjs test ../thelia/tests/functionnal/casperjs/exe/front/ --pre=../thelia/tests/functionnal/casperjs/conf/local.js --verbose --thelia2_base_url="http://localhost:8000/index.php/" --thelia2_screenshot_path="../thelia/tests/functionnal/casperjs/screenshot/"
+./bin/casperjs test ../thelia/tests/functionnal/casperjs/exe/ --pre=../thelia/tests/functionnal/casperjs/conf/local.js --verbose --thelia2_base_url="http://localhost:8000/index.php/" --thelia2_screenshot_path="../thelia/tests/functionnal/casperjs/screenshot/"
