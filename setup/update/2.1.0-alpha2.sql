df0d0b2e1e23e569a844a9d382a3ebac28b1d5cd
Guillaume Barral <guillaume.barral@hotmail.fr>
2014-10-24 11:18:26 +0200

    correction of the sql request

--- setup/update/2.1.0-alpha2.sql
+++ setup/update/2.1.0-alpha2.sql
@@ -7,22 +7,24 @@ UPDATE `config` SET `value`='alpha2' WHERE `name`='thelia_extra_version';
 
 UPDATE `config` SET `name`='form_firewall_active' WHERE `name`='from_firewall_active';
 
-INSERT INTO `hook` (`code`, `type`, `by_module`, `block`, `native`, `activate`, `position`, `created_at`, `updated_at`) VALUES
-  ('order-edit.cart-top', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
-  ('order-edit.cart-bottom', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
-  ('order-edit.bill-top', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
-  ('order-edit.bill-bottom', 2, 0, 0, 1, 1, 1, NOW(), NOW())
+SELECT @max_id := IFNULL(MAX(`id`),0) FROM `hook`;
+
+INSERT INTO `hook` (`id`, `code`, `type`, `by_module`, `block`, `native`, `activate`, `position`, `created_at`, `updated_at`) VALUES
+  (@max_id+1, 'order-edit.cart-top', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
+  (@max_id+2, 'order-edit.cart-bottom', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
+  (@max_id+3, 'order-edit.bill-top', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
+  (@max_id+4, 'order-edit.bill-bottom', 2, 0, 0, 1, 1, 1, NOW(), NOW())
 ;
 
-INSERT INTO  `hook_i18n` (`locale`, `title`, `description`, `chapo`) VALUES
-  ('en_US', 'Order - cart top', '', ''),
-  ('fr_FR', 'Commande - panier haut', '', ''),
-  ('en_US', 'Order - cart bottom', '', ''),
-  ('fr_FR', 'Commande - panier bas', '', ''),
-  ('en_US', 'Order - bill top', '', ''),
-  ('fr_FR', 'Commande - facture haut', '', ''),
-  ('en_US', 'Order - bill bottom', '', ''),
-  ('fr_FR', 'Commande - facture bas', '', '')
+INSERT INTO  `hook_i18n` (`id`, `locale`, `title`, `description`, `chapo`) VALUES
+  (@max_id+1, 'en_US', 'Order - cart top', '', ''),
+  (@max_id+1, 'fr_FR', 'Commande - panier haut', '', ''),
+  (@max_id+2, 'en_US', 'Order - cart bottom', '', ''),
+  (@max_id+2, 'fr_FR', 'Commande - panier bas', '', ''),
+  (@max_id+3, 'en_US', 'Order - bill top', '', ''),
+  (@max_id+3, 'fr_FR', 'Commande - facture haut', '', ''),
+  (@max_id+4, 'en_US', 'Order - bill bottom', '', ''),
+  (@max_id+4, 'fr_FR', 'Commande - facture bas', '', '')
 ;
 
 
