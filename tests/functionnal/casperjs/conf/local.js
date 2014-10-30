eb699047dc202088b3158f1f050a29f85b015356
Benjamin Perche <bperche9@gmail.com>
2014-10-30 14:25:00 +0100

    Replace wait in functionnal tests with waitFor* 	modified:   tests/functionnal/casperjs/conf/local.js 	modified:   tests/functionnal/casperjs/exe/front/20_register.js 	modified:   tests/functionnal/casperjs/exe/front/30_login.js 	modified:   tests/functionnal/casperjs/exe/front/40_cart.js 	modified:   tests/functionnal/casperjs/exe/front/50-order-process.js 	modified:   tests/functionnal/casperjs/exe/front/60-newsletter.js

--- tests/functionnal/casperjs/conf/local.js
+++ tests/functionnal/casperjs/conf/local.js
@@ -28,7 +28,7 @@ casper.test.comment('Screenshots will be saved under : '+ screenshot_dir);
 casper.options.viewportSize = {width: 1024, height: 768};
 casper.test.comment('Viewport size: '+ casper.options.viewportSize.width + 'x' + casper.options.viewportSize.height);
 
-// Default time to wait in ms
+// Default timeout in ms
 var thelia_default_timeout = 15000;
 casper.test.comment('Default timeout: '+ thelia_default_timeout + ' ms');
 
