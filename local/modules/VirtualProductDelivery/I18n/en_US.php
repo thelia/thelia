15c8f8625ba3c77682ecbb9558faaac2fff4531d
Manuel Raynaud <raynaud.manu@gmail.com>
2014-11-14 15:56:11 +0100

    remove unused translations

--- local/modules/VirtualProductDelivery/I18n/en_US.php
+++ local/modules/VirtualProductDelivery/I18n/en_US.php
@@ -4,7 +4,4 @@ return array(
     'Order {$order_ref} validated. Download your files.' => 'Order {$order_ref} validated. Download your files.',
     'This module cannot be used on the current cart.' => 'This module cannot be used on the current cart.',
     'Virtual product download message' => 'Virtual product download message',
-    'List of downloadable files' => 'List of downloadable files',
-    'File' => 'File',
-    'Download' => 'Download',
 );
--- local/modules/VirtualProductDelivery/I18n/fr_FR.php
+++ local/modules/VirtualProductDelivery/I18n/fr_FR.php
@@ -4,7 +4,4 @@ return array(
     'Order {$order_ref} validated. Download your files.' => 'Commande {$order_ref} validée. Téléchargez vos fichiers.',
     'This module cannot be used on the current cart.' => 'Ce module ne peut pas être utilisé avec le panier actuel.',
     'Virtual product download message' => 'Message pour le téléchargement des produits virtuels',
-    'List of downloadable files' => 'Liste des fichiers téléchargeables',
-    'File' => 'Fichier',
-    'Download' => 'Télécharger',
 );
