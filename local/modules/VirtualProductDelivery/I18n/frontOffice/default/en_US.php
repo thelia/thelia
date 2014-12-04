2f2e3e4a3c0cb11802118e3b3ff899a4a9e85e9d
Julien Chanseaume <julien.chanseaume@gmail.com>
2014-12-04 11:44:20 +0100

    Added translations

--- local/modules/VirtualProductDelivery/I18n/frontOffice/default/en_US.php
+++ local/modules/VirtualProductDelivery/I18n/frontOffice/default/en_US.php
@@ -2,5 +2,8 @@
 
 return array(
     'Delivery address' => 'Delivery address',
+    'Download' => 'Download',
+    'File' => 'File',
+    'List of downloadable files' => 'List of downloadable files',
     'No delivery address for this delivery method' => 'No delivery address for this delivery method',
 );
--- local/modules/VirtualProductDelivery/I18n/frontOffice/default/fr_FR.php
+++ local/modules/VirtualProductDelivery/I18n/frontOffice/default/fr_FR.php
@@ -2,5 +2,8 @@
 
 return array(
     'Delivery address' => 'Adresse de livraison',
+    'Download' => 'Télécharger',
+    'File' => 'Fichier',
+    'List of downloadable files' => 'Liste des fichiers téléchargeables',
     'No delivery address for this delivery method' => 'L\'adresse de livraison n\'est pas nécessaire pour cette méthode de livraison',
 );
