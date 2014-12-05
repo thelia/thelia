65baf9a95a91d2d1ed762dfa4b58dd6a5f2c4103
Manuel Raynaud <raynaud.manu@gmail.com>
2014-12-05 12:14:27 +0100

    add missing resource "admin.seach". Fix #951
    modified :
        setup/insert.sql
        setup/update/2.0.5.sql

--- setup/update/2.0.5.sql
+++ setup/update/2.0.5.sql
@@ -9,4 +9,13 @@ UPDATE `config` SET `value`='' WHERE `name`='thelia_extra_version';
 
 UPDATE `config` SET `name`='form_firewall_active' WHERE `name`='from_firewall_active';
 
+SELECT @max_id := IFNULL(MAX(`id`),0) FROM `resource`;
+
+INSERT INTO resource (`id`, `code`, `created_at`, `updated_at`) VALUES
+(@max_id + 1, 'admin.search', NOW(), NOW());
+
+INSERT INTO resource_i18n (`id`, `locale`, `title`) VALUES
+(@max_id + 1, 'en_US', 'search'),
+(@max_id + 1, 'fr_FR', 'Recherche'),
+
 SET FOREIGN_KEY_CHECKS = 1;
