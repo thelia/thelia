321356834e93f7fad16834ea37fa3566e1c2b969
Benjamin Perche <bperche9@gmail.com>
2014-12-23 14:03:04 +0100

    Fix thelia.core_dir parameter

--- core/lib/Thelia/Core/Thelia.php
+++ core/lib/Thelia/Core/Thelia.php
@@ -427,7 +427,7 @@ class Thelia extends Kernel
         $parameters = parent::getKernelParameters();
 
         $parameters["thelia.root_dir"] = THELIA_ROOT;
-        $parameters["thelia.core_dir"] = THELIA_ROOT . "core/lib/Thelia";
+        $parameters["thelia.core_dir"] = dirname(__DIR__); // This class is in core/lib/Thelia/Core.
         $parameters["thelia.module_dir"] = THELIA_MODULE_DIR;
 
         return $parameters;
