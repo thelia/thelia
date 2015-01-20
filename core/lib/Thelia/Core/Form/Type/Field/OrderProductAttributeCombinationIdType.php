fd72efbea5b2dae5fa756b7cbd05baec513f7062
Julien Chanseaume <julien.chanseaume@gmail.com>
2015-01-20 10:24:25 +0100

    Added missing form field type

---
+++ core/lib/Thelia/Core/Form/Type/Field/OrderProductAttributeCombinationIdType.php
@@ -0,0 +1,43 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace Thelia\Core\Form\Type\Field;
+
+use Thelia\Model\OrderProductAttributeCombinationQuery;
+
+/**
+ * Class OrderProductAttributeCombinationIdType
+ * @package Thelia\Core\Form\Type\Field
+ * @author Julien Chanséaume <jchanseaume@openstudio.fr>
+ */
+class OrderProductAttributeCombinationIdType extends AbstractIdType
+{
+    /**
+     * @return \Propel\Runtime\ActiveQuery\ModelCriteria
+     *
+     * Get the model query to check
+     */
+    protected function getQuery()
+    {
+        return new OrderProductAttributeCombinationQuery();
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return "order_product_attribute_combination_id";
+    }
+}
