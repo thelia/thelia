1ac11931274560a635f0fc645258d0fa8280d072
Julien Chanseaume <julien.chanseaume@gmail.com>
2015-01-21 12:19:35 +0100

    Added new hook in backOffice template : order.tab

--- setup/update/sql/2.2.0-alpha1.sql
+++ setup/update/sql/2.2.0-alpha1.sql
@@ -6,7 +6,22 @@ UPDATE `config` SET `value`='2' WHERE `name`='thelia_minus_version';
 UPDATE `config` SET `value`='0' WHERE `name`='thelia_release_version';
 UPDATE `config` SET `value`='alpha1' WHERE `name`='thelia_extra_version';
 
+-- admin hooks
+
+SELECT @max_id := IFNULL(MAX(`id`),0) FROM `hook`;
+
+INSERT INTO `hook` (`id`, `code`, `type`, `by_module`, `block`, `native`, `activate`, `position`, `created_at`, `updated_at`) VALUES
+  (@max_id + 1, 'order.tab', 2, 0, 1, 1, 1, 1, NOW(), NOW())
+;
+
+INSERT INTO  `hook_i18n` (`id`, `locale`, `title`, `description`, `chapo`) VALUES
+  (@max_id + 1, 'fr_FR', 'Commande - Onglet', '', ''),
+  (@max_id + 1, 'en_US', 'Order - Tab', '', '')
+;
+
+
 -- new column in admin_log
+
 ALTER TABLE `admin_log` ADD `resource_id` INTEGER AFTER `resource` ;
 
 SET FOREIGN_KEY_CHECKS = 1;
