95205d131e1d2a03b28055c693da5fdb9e30eb35
Emmanuel Nurit <emmanuelnurit@iMac-de-Emmanuel-2.local>
2015-02-16 12:51:35 +0100

    Adding Pinterest to social hook
    Missing translation

--- local/modules/HookSocial/I18n/en_US.php
+++ local/modules/HookSocial/I18n/en_US.php
@@ -5,6 +5,7 @@ return array(
     'Follow us' => 'Follow us',
     'Google + username' => 'Google + username',
     'Instagram username' => 'Instagram username',
+    'Pinterest username' => 'Pinterest username',
     'RSS URL' => 'RSS URL',
     'Twitter username' => 'Twitter username',
     'Youtube URL' => 'Youtube URL',
