be4dd103be55d98be6e5882c7aa0d8b4835aa7ca
Julien Chanseaume <julien.chanseaume@gmail.com>
2015-03-10 10:47:46 +0100

    Added option to select the locales to generate

--- core/lib/Thelia/Command/GenerateSQLCommand.php
+++ core/lib/Thelia/Command/GenerateSQLCommand.php
@@ -13,16 +13,12 @@
 namespace Thelia\Command;
 
 use Imagine\Exception\RuntimeException;
-use PDO;
 use Propel\Runtime\Propel;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
-use Thelia\Core\Event\Cache\CacheEvent;
-use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\HttpFoundation\Session\Session;
 use Thelia\Core\Translation\Translator;
@@ -42,13 +38,21 @@ class GenerateSQLCommand extends ContainerAwareCommand
     /** @var SmartyParser $parser */
     protected $parser = null;
 
+    /** @var \PDO  */
     protected $con;
 
     protected function configure()
     {
         $this
             ->setName("generate:sql")
-            ->setDescription("Generate SQL files (insert.sql, update*.sql)");
+            ->setDescription("Generate SQL files (insert.sql, update*.sql)")
+            ->addOption(
+                "locale",
+                null,
+                InputOption::VALUE_OPTIONAL,
+                "generate only for only specific locales (separated by a ,) : fr_FR,es_ES or es_ES"
+            )
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -64,6 +68,7 @@ class GenerateSQLCommand extends ContainerAwareCommand
 
         $this->con = Propel::getConnection(ProductTableMap::DATABASE_NAME);
 
+        // load translations
         $finder = Finder::create()
             ->name('*.php')
             ->depth(0)
@@ -71,33 +76,44 @@ class GenerateSQLCommand extends ContainerAwareCommand
 
         $locales = [];
 
+        // limit to only some locale(s)
+        $localesToKeep = $input->getOption("locale");
+        if (!empty($localesToKeep)) {
+            $localesToKeep = explode(',', $localesToKeep);
+        } else {
+            $localesToKeep = null;
+        }
+
         /** @var \SplFileInfo $file */
         foreach ($finder as $file) {
             $locale = $file->getBasename('.php');
-            $locales[] = $locale;
-            $this->translator->addResource(
-                'php',
-                $file->getRealPath(),
-                $locale,
-                'install'
-            );
+            if (empty($localesToKeep) || in_array($locale, $localesToKeep)) {
+                $locales[] = $locale;
+                $this->translator->addResource(
+                    'php',
+                    $file->getRealPath(),
+                    $locale,
+                    'install'
+                );
+            }
         }
 
+        // replace the default intl function
         $this->parser->unregisterPlugin('function', 'intl');
         $this->parser->registerPlugin('function', 'intl', [$this, 'translate']);
 
+        // Main insert.sql file
         $content = file_get_contents(THELIA_SETUP_DIRECTORY . 'insert.sql.tpl');
         $this->parser->assign("locales", $locales);
         $content = $this->parser->renderString($content, [], false);
 
-        // Main sql file
         if (false === file_put_contents(THELIA_SETUP_DIRECTORY . 'insert.sql', $content)) {
             $output->writeln("Can't write file " . THELIA_SETUP_DIRECTORY . 'insert.sql');
         } else {
             $output->writeln("File " . THELIA_SETUP_DIRECTORY . 'insert.sql generated successfully.');
         }
 
-        // sql update file
+        // sql update files
         $finder = Finder::create()
             ->name('*.tpl')
             ->depth(0)
@@ -106,8 +122,6 @@ class GenerateSQLCommand extends ContainerAwareCommand
         /** @var \SplFileInfo $file */
         foreach ($finder as $file) {
             $filename = $file->getBasename();
-
-            $output->writeln("Processing file " . $filename);
 
             $content = file_get_contents($file->getRealPath());
             $content = $this->parser->renderString($content, [], false);
@@ -120,7 +134,6 @@ class GenerateSQLCommand extends ContainerAwareCommand
                 $output->writeln("File " . $destination . ' generated successfully.');
             }
         }
-
     }
 
     public function translate($params, $smarty)
@@ -157,6 +170,4 @@ class GenerateSQLCommand extends ContainerAwareCommand
 
         return $translation;
     }
-
-
 }
