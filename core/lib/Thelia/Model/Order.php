92d8a8346f546da48a141da87f4186847d86010f
zzuutt <zzuutt@users.noreply.github.com>
2015-03-18 10:51:22 +0100

    Update Order.php

--- core/lib/Thelia/Model/Order.php
+++ core/lib/Thelia/Model/Order.php
@@ -237,9 +237,13 @@ class Order extends BaseOrder
      * if false, it will check if the order has been paid, whatever the current status is. The default is false.
      * @return bool true if this order is PAID, false otherwise.
      */
-    public function isPaid()
+    public function isPaid($exact = true)
     {
-        return $this->hasStatusHelper(OrderStatus::CODE_PAID);
+        return $this->hasStatusHelper(
+            $exact ?
+            OrderStatus::CODE_PAID :
+            [ OrderStatus::CODE_PAID] //, OrderStatus::CODE_PROCESSING, OrderStatus::CODE_SENT ]
+        );
     }
 
     /**
