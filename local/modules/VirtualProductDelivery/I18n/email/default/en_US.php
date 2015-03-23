f64dfa4a195bcf7b2b3d294f9edd32bf991a3e67
Julien Chanseaume <julien.chanseaume@gmail.com>
2015-03-23 16:28:37 +0100

    Added translations

---
+++ local/modules/VirtualProductDelivery/I18n/email/default/en_US.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'Best Regards.' => 'Best Regards.',
+    'Feel free to contact us for any further information.' => 'Feel free to contact us for any further information.',
+    'Products:' => 'Products:',
+    'You have to be logged in to your account to download this files.' => 'You have to be logged in to your account to download this files.',
+    'Your order %ref has been validated. You can download your files.' => 'Your order %ref has been validated. You can download your files.',
+    'have to be logged in to your account to download this files.' => 'have to be logged in to your account to download this files.',
+);
---
+++ local/modules/VirtualProductDelivery/I18n/pdf/default/fr_FR.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'No delivery address for this delivery method' => 'Aucune adresse de livraison pour cette méthode de livraison',
+);
