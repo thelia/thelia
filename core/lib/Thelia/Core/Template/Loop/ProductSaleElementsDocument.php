b4016150d4c89ceb89065a6aba7d3f39eee932f8
Benjamin Perche <bperche9@gmail.com>
2015-05-28 15:43:34 +0200

    Add loops for pse image/document. Fix #1098 and #1122

---
+++ core/lib/Thelia/Core/Template/Loop/ProductSaleElementsDocument.php
@@ -0,0 +1,137 @@
+<?php
+/*************************************************************************************/
+/* This file is part of the Thelia package.                                          */
+/*                                                                                   */
+/* Copyright (c) OpenStudio                                                          */
+/* email : dev@thelia.net                                                            */
+/* web : http://www.thelia.net                                                       */
+/*                                                                                   */
+/* For the full copyright and license information, please view the LICENSE.txt       */
+/* file that was distributed with this source code.                                  */
+/*************************************************************************************/
+
+namespace Thelia\Core\Template\Loop;
+
+use Thelia\Core\Template\Element\BaseLoop;
+use Thelia\Core\Template\Element\LoopResultRow;
+use Thelia\Core\Template\Element\PropelSearchLoopInterface;
+use Thelia\Core\Template\Element\LoopResult;
+use Thelia\Core\Template\Loop\Argument\Argument;
+use Thelia\Core\Template\Loop\Argument\ArgumentCollection;
+use Thelia\Model\ProductSaleElementsProductDocumentQuery;
+
+/**
+ * Class ProductSaleElementsDocument
+ * @package Thelia\Core\Template\Loop
+ * @author Benjamin Perche <benjamin@thelia.net>
+ */
+class ProductSaleElementsDocument extends BaseLoop implements PropelSearchLoopInterface
+{
+    /**
+     * @param LoopResult $loopResult
+     *
+     * @return LoopResult
+     */
+    public function parseResults(LoopResult $loopResult)
+    {
+        /** @var \Thelia\Model\ProductSaleElementsProductDocument $productSaleElementDocument */
+        foreach ($loopResult->getResultDataCollection() as $productSaleElementDocument) {
+            $row = new LoopResultRow();
+
+            $row
+                ->set("ID", $productSaleElementDocument->getId())
+                ->set("PRODUCT_SALE_ELEMENTS_ID", $productSaleElementDocument->getProductSaleElementsId())
+                ->set("PRODUCT_DOCUMENT_ID", $productSaleElementDocument->getProductDocumentId())
+            ;
+
+            $loopResult->addRow($row);
+        }
+
+        return $loopResult;
+    }
+
+    /**
+     * Definition of loop arguments
+     *
+     * example :
+     *
+     * public function getArgDefinitions()
+     * {
+     *  return new ArgumentCollection(
+     *
+     *       Argument::createIntListTypeArgument('id'),
+     *           new Argument(
+     *           'ref',
+     *           new TypeCollection(
+     *               new Type\AlphaNumStringListType()
+     *           )
+     *       ),
+     *       Argument::createIntListTypeArgument('category'),
+     *       Argument::createBooleanTypeArgument('new'),
+     *       ...
+     *   );
+     * }
+     *
+     * @return \Thelia\Core\Template\Loop\Argument\ArgumentCollection
+     */
+    protected function getArgDefinitions()
+    {
+        return new ArgumentCollection(
+            Argument::createIntListTypeArgument("id"),
+            Argument::createIntListTypeArgument("product_sale_elements_id"),
+            Argument::createIntListTypeArgument("product_document_id"),
+            Argument::createEnumListTypeArgument(
+                "order",
+                [
+                    "position",
+                    "position-reverse"
+                ],
+                "position"
+            )
+        );
+    }
+
+    /**
+     * this method returns a Propel ModelCriteria
+     *
+     * @return \Propel\Runtime\ActiveQuery\ModelCriteria
+     */
+    public function buildModelCriteria()
+    {
+        $query = ProductSaleElementsProductDocumentQuery::create();
+
+        if (null !== $id = $this->getId()) {
+            $query->filterById($id);
+        }
+
+        if (null !== $pseId = $this->getProductSaleElementsId()) {
+            $query->filterByProductSaleElementsId($pseId);
+        }
+
+        if (null !== $productDocumentId = $this->getProductDocumentId()) {
+            $query->filterByProductDocumentId($id);
+        }
+
+        foreach ($this->getOrder() as $order) {
+            switch ($order) {
+                case "position":
+                    $query
+                        ->useProductDocumentQuery()
+                            ->orderByPosition()
+                        ->endUse()
+                    ;
+                    break;
+
+                case "position-reverse":
+                    $query
+                        ->useProductDocumentQuery()
+                            ->orderByPosition()
+                        ->endUse()
+                    ;
+                    break;
+            }
+        }
+
+        return $query;
+    }
+}
---
+++ core/lib/Thelia/Core/Template/Loop/ProductSaleElementsImage.php
@@ -0,0 +1,137 @@
+<?php
+/*************************************************************************************/
+/* This file is part of the Thelia package.                                          */
+/*                                                                                   */
+/* Copyright (c) OpenStudio                                                          */
+/* email : dev@thelia.net                                                            */
+/* web : http://www.thelia.net                                                       */
+/*                                                                                   */
+/* For the full copyright and license information, please view the LICENSE.txt       */
+/* file that was distributed with this source code.                                  */
+/*************************************************************************************/
+
+namespace Thelia\Core\Template\Loop;
+
+use Thelia\Core\Template\Element\BaseLoop;
+use Thelia\Core\Template\Element\LoopResultRow;
+use Thelia\Core\Template\Element\PropelSearchLoopInterface;
+use Thelia\Core\Template\Element\LoopResult;
+use Thelia\Core\Template\Loop\Argument\Argument;
+use Thelia\Core\Template\Loop\Argument\ArgumentCollection;
+use Thelia\Model\ProductSaleElementsProductImageQuery;
+
+/**
+ * Class ProductSaleElementsImage
+ * @package Thelia\Core\Template\Loop
+ * @author Benjamin Perche <benjamin@thelia.net>
+ */
+class ProductSaleElementsImage extends BaseLoop implements PropelSearchLoopInterface
+{
+    /**
+     * @param LoopResult $loopResult
+     *
+     * @return LoopResult
+     */
+    public function parseResults(LoopResult $loopResult)
+    {
+        /** @var \Thelia\Model\ProductSaleElementsProductImage $productSaleElementImage */
+        foreach ($loopResult->getResultDataCollection() as $productSaleElementImage) {
+            $row = new LoopResultRow();
+
+            $row
+                ->set("ID", $productSaleElementImage->getId())
+                ->set("PRODUCT_SALE_ELEMENTS_ID", $productSaleElementImage->getProductSaleElementsId())
+                ->set("PRODUCT_IMAGE_ID", $productSaleElementImage->getProductImageId())
+            ;
+
+            $loopResult->addRow($row);
+        }
+
+        return $loopResult;
+    }
+
+    /**
+     * Definition of loop arguments
+     *
+     * example :
+     *
+     * public function getArgDefinitions()
+     * {
+     *  return new ArgumentCollection(
+     *
+     *       Argument::createIntListTypeArgument('id'),
+     *           new Argument(
+     *           'ref',
+     *           new TypeCollection(
+     *               new Type\AlphaNumStringListType()
+     *           )
+     *       ),
+     *       Argument::createIntListTypeArgument('category'),
+     *       Argument::createBooleanTypeArgument('new'),
+     *       ...
+     *   );
+     * }
+     *
+     * @return \Thelia\Core\Template\Loop\Argument\ArgumentCollection
+     */
+    protected function getArgDefinitions()
+    {
+        return new ArgumentCollection(
+            Argument::createIntListTypeArgument("id"),
+            Argument::createIntListTypeArgument("product_sale_elements_id"),
+            Argument::createIntListTypeArgument("product_image_id"),
+            Argument::createEnumListTypeArgument(
+                "order",
+                [
+                    "position",
+                    "position-reverse"
+                ],
+                "position"
+            )
+        );
+    }
+
+    /**
+     * this method returns a Propel ModelCriteria
+     *
+     * @return \Propel\Runtime\ActiveQuery\ModelCriteria
+     */
+    public function buildModelCriteria()
+    {
+        $query = ProductSaleElementsProductImageQuery::create();
+
+        if (null !== $id = $this->getId()) {
+            $query->filterById($id);
+        }
+
+        if (null !== $pseId = $this->getProductSaleElementsId()) {
+            $query->filterByProductSaleElementsId($pseId);
+        }
+
+        if (null !== $productImageId = $this->getProductImageId()) {
+            $query->filterByProductImageId($id);
+        }
+
+        foreach ($this->getOrder() as $order) {
+            switch ($order) {
+                case "position":
+                    $query
+                        ->useProductImageQuery()
+                            ->orderByPosition()
+                        ->endUse()
+                    ;
+                    break;
+
+                case "position-reverse":
+                    $query
+                        ->useProductImageQuery()
+                            ->orderByPosition()
+                        ->endUse()
+                    ;
+                    break;
+            }
+        }
+
+        return $query;
+    }
+}
