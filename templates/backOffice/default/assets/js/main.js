84b46760cd0816c1160d6ee95abce3b54c051c8e
Michaël Espeche <michael.espeche@gmail.com>
2015-06-10 19:06:59 +0200

    Add menu script

--- templates/backOffice/default/assets/js/main.js
+++ templates/backOffice/default/assets/js/main.js
@@ -2,6 +2,24 @@
     
     $(function(){
         
+        $(window).bind("load resize", function() {
+            topOffset = 52;
+            width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+            if (width < 768) {
+                $('div.navbar-collapse').addClass('collapse');
+                topOffset = 104;
+            } else {
+                $('div.navbar-collapse').removeClass('collapse');
+            }
+
+            height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+            height = height - topOffset;
+            if (height < 1) height = 1;
+            if (height > topOffset) {
+                $("#page-wrapper").css("min-height", (height) + "px");
+            }
+        });        
+
         // -- Bootstrap tooltip --
         if($('[rel="tooltip"]').length){            
             $('[rel="tooltip"]').tooltip();
