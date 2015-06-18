71ffff1035b39ff9317841ff1a37c586f258c4fb
Guillaume Barral <guillaume.barral@hotmail.fr>
2015-06-18 11:55:24 +0200

    add 2 hooks to personalize order-products list in order-edit template

--- setup/update/sql/2.2.0-beta1.sql
+++ setup/update/sql/2.2.0-beta1.sql
@@ -11,4 +11,19 @@ update currency set by_default = 0 where by_default is NULL;
 
 ALTER TABLE `category_version` ADD COLUMN `default_template_id` INTEGER AFTER  `position`;
 
+-- new hook --
+SELECT @max_id := IFNULL(MAX(`id`),0) FROM `hook`;
+
+INSERT INTO `hook` (`id`, `code`, `type`, `by_module`, `block`, `native`, `activate`, `position`, `created_at`, `updated_at`) VALUES
+(@max_id + 1, 'order-edit.table-header', 2, 0, 0, 1, 1, 1, NOW(), NOW()),
+(@max_id + 2, 'order-edit.table-row', 2, 0, 0, 1, 1, 1, NOW(), NOW())
+;
+
+INSERT INTO  `hook_i18n` (`id`, `locale`, `title`, `description`, `chapo`) VALUES
+(@max_id + 1, 'en_US', 'Order - table header', '', ''),
+(@max_id + 1, 'fr_FR', 'Commande - colonne tableau', '', ''),
+(@max_id + 2, 'en_US', 'Order - table row', '', ''),
+(@max_id + 2, 'fr_FR', 'Commande - ligne tableau', '', '')
+;
+
 SET FOREIGN_KEY_CHECKS = 1;
