e009ddc19893db44bb5d5db1d2f9acc7384a2b62
Julien Chanseaume <julien.chanseaume@gmail.com>
2015-10-19 06:46:50 +0200

    Bump version 2.1.7

--- core/lib/Thelia/Core/Thelia.php
+++ core/lib/Thelia/Core/Thelia.php
@@ -50,7 +50,7 @@ use Thelia\Model\ModuleQuery;
 
 class Thelia extends Kernel
 {
-    const THELIA_VERSION = '2.1.6';
+    const THELIA_VERSION = '2.1.7';
 
     public function __construct($environment, $debug)
     {
