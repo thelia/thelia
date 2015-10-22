2949f5368b5b187dc6301735a0387b56bb30dfe4
gilles <gilles.bourgeat@gmail.com>
2015-10-22 14:28:13 +0200

    Removes deprecated classes and methods in version 2.3

--- core/lib/Thelia/Model/OrderProduct.php
+++ core/lib/Thelia/Model/OrderProduct.php
@@ -11,12 +11,6 @@ class OrderProduct extends BaseOrderProduct
 {
     use \Thelia\Model\Tools\ModelEventDispatcherTrait;
 
-    /**
-     * @var int $cartIemId
-     * @deprecated Since 2.1.3 because it is a typo, will be removed in 2.3
-     */
-    protected $cartIemId;
-
     /** @var int */
     protected $cartItemId;
 
@@ -28,16 +22,6 @@ class OrderProduct extends BaseOrderProduct
     {
         $this->cartItemId = $cartItemId;
 
-        /**
-         * We have to ensure that if the setter is called, it sets this propriety too
-         * for backward compatibility.
-         *
-         * example:
-         *  A module creates a stub of this class
-         *  It directly accesses to the $cartIemId propriety
-         */
-        $this->cartIemId = $cartItemId;
-
         return $this;
     }
 
@@ -47,37 +31,6 @@ class OrderProduct extends BaseOrderProduct
     public function getCartItemId()
     {
         return $this->cartItemId;
-    }
-
-    /**
-     * @param mixed $cartItemId
-     * @deprecated Since 2.1.3 because it is a typo, will be removed in 2.3
-     * @return $this|OrderProduct
-     */
-    public function setCartIemId($cartItemId)
-    {
-        trigger_error(
-            'Thelia\Model\OrderProduct::setCartIemId is deprecated since version 2.1.3 and will be removed in 2.3. '.
-            'You must use Thelia\Model\OrderProduct::setCartItemId instead',
-            E_DEPRECATED
-        );
-
-        return $this->setCartItemId($cartItemId);
-    }
-
-    /**
-     * @return mixed
-     * @deprecated Since 2.1.3 because it is a typo, will be removed in 2.3
-     */
-    public function getCartIemId()
-    {
-        trigger_error(
-            'Thelia\Model\OrderProduct::getCartIemId is deprecated since version 2.1.3 and will be removed in 2.3. '.
-            'You must use Thelia\Model\OrderProduct::getCartItemId instead',
-            E_DEPRECATED
-        );
-
-        return $this->getCartItemId();
     }
 
     /**
