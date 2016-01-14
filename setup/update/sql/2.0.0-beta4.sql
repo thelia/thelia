6fc48cdd9d07bdcce6db7aa75c1e49ddeace17dc
Franck Allimant <github@allimant.org>
2016-01-14 12:52:17 +0100

    Added a variable to disable admin password recovery

--- setup/update/sql/2.0.0-beta4.sql
+++ setup/update/sql/2.0.0-beta4.sql
@@ -6,14 +6,8 @@ INSERT INTO `module` (`code`, `type`, `activate`, `position`, `full_namespace`, 
 ( 'Tinymce', 1, 0, 1, 'Tinymce\\Tinymce', NOW(), NOW());
 
 INSERT INTO  `module_i18n` (`id`, `locale`, `title`, `description`, `chapo`, `postscriptum`) VALUES
-(LAST_INSERT_ID(), 'ar_SA', NULL, NULL,  NULL,  NULL),
-(LAST_INSERT_ID(), 'de_DE', 'Tinymce Wysiwyg Editor', NULL,  NULL,  NULL),
 (LAST_INSERT_ID(), 'en_US', 'tinymce wysiwyg editor', NULL,  NULL,  NULL),
-(LAST_INSERT_ID(), 'es_ES', NULL, NULL,  NULL,  NULL),
-(LAST_INSERT_ID(), 'fr_FR', 'Editeur TinyMCE', NULL,  NULL,  NULL),
-(LAST_INSERT_ID(), 'it_IT', NULL, NULL,  NULL,  NULL),
-(LAST_INSERT_ID(), 'pt_BR', NULL, NULL,  NULL,  NULL),
-(LAST_INSERT_ID(), 'tr_TR', 'TinyMCE WYSIWYG editörü', NULL,  NULL,  NULL)
+(LAST_INSERT_ID(), 'es_ES', NULL, NULL,  NULL,  NULL)
 ;
 
 UPDATE `config` SET `value`='2.0.0-beta4' WHERE `name`='thelia_version';
