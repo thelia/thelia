c4c9c62679ad08052a5ac9d0b025cc1aa6a242a8
gilles <gilles.bourgeat@gmail.com>
2016-04-04 10:20:17 +0200

    Fix old import export breaks compilation

---
+++ core/lib/Thelia/ImportExport/Import/ImportHandler.php
@@ -0,0 +1,25 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace Thelia\ImportExport\Import;
+
+use Thelia\ImportExport\AbstractHandler;
+
+/**
+ * Class ImportHandler
+ * @package Thelia\ImportExport
+ * @author Benjamin Perche <bperche@openstudio.fr>
+ * @deprecated since 2.3, fore more information http://doc.thelia.net/en/documentation/features/import_export/import.html
+ */
+abstract class ImportHandler extends AbstractHandler
+{
+}
