81f1a3685bf819759cfa26345005fa3457c48377
gilles <gilles.bourgeat@gmail.com>
2016-04-11 18:26:25 +0200

    Add test for currency in session

---
+++ tests/phpunit/Thelia/Tests/Model/CurrencyTest.php
@@ -0,0 +1,33 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace Thelia\Tests\Model;
+
+use Thelia\Model\Currency;
+use Thelia\Model\CurrencyQuery;
+
+/**
+ * Class CurrencyTest
+ * @package Thelia\Tests\Model
+ * @author Gilles Bourgeat <gilles@thelia.net>
+ */
+class CurrencyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetDefaultCurrency()
+    {
+        $expectedCurrency = CurrencyQuery::create()->findOneByByDefault(true);
+        $actualCurrency = Currency::getDefaultCurrency();
+
+        $this->assertEquals($expectedCurrency->getId(), $actualCurrency->getId());
+
+    }
+}
