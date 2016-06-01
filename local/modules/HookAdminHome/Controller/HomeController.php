62f5e2c8385c5cf495e6650b38256c2700f856c9
gilles <gilles.bourgeat@gmail.com>
2016-06-01 14:29:19 +0200

    Update home controller for it uses the thelia cache service

--- local/modules/HookAdminHome/Controller/HomeController.php
+++ local/modules/HookAdminHome/Controller/HomeController.php
@@ -12,8 +12,8 @@
 
 namespace HookAdminHome\Controller;
 
-use Doctrine\Common\Cache\FilesystemCache;
 use HookAdminHome\HookAdminHome;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Thelia\Controller\Admin\BaseAdminController;
 use Thelia\Core\Security\AccessManager;
 use Thelia\Model\ConfigQuery;
@@ -27,11 +27,6 @@ use Thelia\Model\OrderQuery;
  */
 class HomeController extends BaseAdminController
 {
-    /**
-     * Folder name for stats cache
-     */
-    const STATS_CACHE_DIR = "stats";
-
     /**
      * Key prefix for stats cache
      */
@@ -47,89 +42,78 @@ class HomeController extends BaseAdminController
 
         $cacheExpire = ConfigQuery::getAdminCacheHomeStatsTTL();
 
-        $cacheContent = false;
+        /** @var AdapterInterface $cacheAdapter */
+        $cacheAdapter = $this->container->get('thelia.cache');
 
         $month = (int) $this->getRequest()->query->get('month', date('m'));
         $year = (int) $this->getRequest()->query->get('year', date('Y'));
 
-        if ($cacheExpire) {
-            $context = "_" . $month . "_" . $year;
+        $cacheKey = self::STATS_CACHE_KEY . "_" . $month . "_" . $year;
 
-            $cacheKey = self::STATS_CACHE_KEY . $context;
+        $cacheItem = $cacheAdapter->getItem($cacheKey);
 
-            $cacheDriver = new FilesystemCache($this->getCacheDir());
-
-            if (!$this->getRequest()->query->get('flush', "0")) {
-                $cacheContent = $cacheDriver->fetch($cacheKey);
-            } else {
-                $cacheDriver->delete($cacheKey);
-            }
+        // force flush
+        if ($this->getRequest()->query->get('flush', "0")) {
+            $cacheAdapter->deleteItem($cacheItem);
         }
 
-        if ($cacheContent === false) {
-            $data = new \stdClass();
+        if (!$cacheItem->isHit()) {
+            $data = $this->getStatus($month, $year);
 
-            $data->title = $this->getTranslator()->trans(
-                "Stats on %month/%year",
-                ['%month' => $month, '%year' => $year],
-                HookAdminHome::DOMAIN_NAME
-            );
-
-            /* sales */
-            $saleSeries = new \stdClass();
-            $saleSeries->color = self::testHexColor('sales_color', '#adadad');
-            $saleSeries->data = OrderQuery::getMonthlySaleStats($month, $year);
-
-            /* new customers */
-            $newCustomerSeries = new \stdClass();
-            $newCustomerSeries->color = self::testHexColor('customers_color', '#f39922');
-            $newCustomerSeries->data = CustomerQuery::getMonthlyNewCustomersStats($month, $year);
-
-            /* orders */
-            $orderSeries = new \stdClass();
-            $orderSeries->color = self::testHexColor('orders_color', '#5cb85c');
-            $orderSeries->data = OrderQuery::getMonthlyOrdersStats($month, $year);
-
-            /* first order */
-            $firstOrderSeries = new \stdClass();
-            $firstOrderSeries->color = self::testHexColor('first_orders_color', '#5bc0de');
-            $firstOrderSeries->data = OrderQuery::getFirstOrdersStats($month, $year);
-
-            /* cancelled orders */
-            $cancelledOrderSeries = new \stdClass();
-            $cancelledOrderSeries->color = self::testHexColor('cancelled_orders_color', '#d9534f');
-            $cancelledOrderSeries->data = OrderQuery::getMonthlyOrdersStats($month, $year, array(5));
-
-            $data->series = array(
-                $saleSeries,
-                $newCustomerSeries,
-                $orderSeries,
-                $firstOrderSeries,
-                $cancelledOrderSeries,
-            );
-
-            $cacheContent = json_encode($data);
+            $cacheItem->set(json_encode($data));
+            $cacheItem->expiresAfter($cacheExpire);
 
             if ($cacheExpire) {
-                $cacheDriver->save($cacheKey, $cacheContent, $cacheExpire);
+                $cacheAdapter->save($cacheItem);
             }
         }
 
-        return $this->jsonResponse($cacheContent);
+        return $this->jsonResponse($cacheItem->get());
     }
 
     /**
-     * get the cache directory for sitemap
-     *
-     * @return mixed|string
+     * @param int $month
+     * @param int $year
+     * @return \stdClass
      */
-    protected function getCacheDir()
+    protected function getStatus($month, $year)
     {
-        $cacheDir = $this->container->getParameter("kernel.cache_dir");
-        $cacheDir = rtrim($cacheDir, '/');
-        $cacheDir .= DIRECTORY_SEPARATOR . self::STATS_CACHE_DIR . DIRECTORY_SEPARATOR;
+        $data = new \stdClass();
 
-        return $cacheDir;
+        $data->title = $this->getTranslator()->trans(
+            "Stats on %month/%year",
+            ['%month' => $month, '%year' => $year],
+            HookAdminHome::DOMAIN_NAME
+        );
+
+        $data->series = [];
+
+        /* sales */
+        $data->series[] = $saleSeries = new \stdClass();
+        $saleSeries->color = self::testHexColor('sales_color', '#adadad');
+        $saleSeries->data = OrderQuery::getMonthlySaleStats($month, $year);
+
+        /* new customers */
+        $data->series[] = $newCustomerSeries = new \stdClass();
+        $newCustomerSeries->color = self::testHexColor('customers_color', '#f39922');
+        $newCustomerSeries->data = CustomerQuery::getMonthlyNewCustomersStats($month, $year);
+
+        /* orders */
+        $data->series[] = $orderSeries = new \stdClass();
+        $orderSeries->color = self::testHexColor('orders_color', '#5cb85c');
+        $orderSeries->data = OrderQuery::getMonthlyOrdersStats($month, $year);
+
+        /* first order */
+        $data->series[] = $firstOrderSeries = new \stdClass();
+        $firstOrderSeries->color = self::testHexColor('first_orders_color', '#5bc0de');
+        $firstOrderSeries->data = OrderQuery::getFirstOrdersStats($month, $year);
+
+        /* cancelled orders */
+        $data->series[] = $cancelledOrderSeries = new \stdClass();
+        $cancelledOrderSeries->color = self::testHexColor('cancelled_orders_color', '#d9534f');
+        $cancelledOrderSeries->data = OrderQuery::getMonthlyOrdersStats($month, $year, array(5));
+
+        return $data;
     }
 
     /**
