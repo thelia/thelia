37b403f4477553490ac9203ced5f24b77df2811f
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2016-06-24 18:17:41 +0200

    Build all Propel table maps on boot
    Add a compiler pass to build and store a classmap of cached table map classes in the application container.
    Add an event subscriber to call 'buildTableMaps()' on table map classes on boot.

---
+++ core/lib/Thelia/Core/EventListener/PropelListener.php
@@ -0,0 +1,53 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace Thelia\Core\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Thelia\Core\Event\TheliaEvents;
+
+/**
+ * Actions related to Propel.
+ */
+class PropelListener implements EventSubscriberInterface
+{
+    /**
+     * Table map classes to be built.
+     * @var string[]
+     */
+    protected $tableMapClasses = [];
+
+    /**
+     * @param string[] $tableMapClasses Table map classes to be built.
+     */
+    public function __construct(array $tableMapClasses = [])
+    {
+        $this->tableMapClasses = $tableMapClasses;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            TheliaEvents::BOOT => 'buildTableMaps',
+        ];
+    }
+
+    /**
+     * Build table maps.
+     */
+    public function buildTableMaps()
+    {
+        foreach ($this->tableMapClasses as $tableMapClass) {
+            call_user_func([$tableMapClass, 'buildTableMap']);
+        }
+    }
+}
