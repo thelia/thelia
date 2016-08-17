d66ef7702b8cfd81facd94468ce0e20d01ac0b7c
Julien Chanseaume <julien.chanseaume@gmail.com>
2016-08-17 10:44:50 +0200

    Fixed cancelPayment method in BasePaymentModuleController class

--- core/lib/Thelia/Module/BasePaymentModuleController.php
+++ core/lib/Thelia/Module/BasePaymentModuleController.php
@@ -19,13 +19,12 @@ use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\HttpKernel\Exception\RedirectException;
 use Thelia\Log\Tlog;
 use Thelia\Model\OrderQuery;
-use Thelia\Model\OrderStatus;
 use Thelia\Model\OrderStatusQuery;
 
 /**
  * This class implement the minimum
  *
- * @package Paypal\Controller
+ * @package Thelia\Module
  * @author Thelia <info@thelia.net>
  */
 abstract class BasePaymentModuleController extends BaseFrontController
@@ -66,22 +65,22 @@ abstract class BasePaymentModuleController extends BaseFrontController
      */
     protected function getLogFilePath()
     {
-        return sprintf(THELIA_ROOT."log".DS."%s.log", strtolower($this->getModuleCode()));
+        return sprintf(THELIA_ROOT . "log" . DS . "%s.log", strtolower($this->getModuleCode()));
     }
 
     /**
      * Process the confirmation of an order. This method should be  called
      * once the module has performed the required checks to confirm a valid payment.
      *
-     * @param int $order_id the order ID
+     * @param int $orderId the order ID
      * @throws \Exception
      */
-    public function confirmPayment($order_id)
+    public function confirmPayment($orderId)
     {
         try {
-            $order_id = intval($order_id);
+            $orderId = intval($orderId);
 
-            if (null !== $order = $this->getOrder($order_id)) {
+            if (null !== $order = $this->getOrder($orderId)) {
                 $this->getLog()->addInfo(
                     $this->getTranslator()->trans(
                         "Processing confirmation of order ref. %ref, ID %id",
@@ -108,8 +107,8 @@ abstract class BasePaymentModuleController extends BaseFrontController
                     "Error occured while processing order ref. %ref, ID %id: %err",
                     array(
                         '%err' => $ex->getMessage(),
-                        '%ref' => ! isset($order) ? "?" : $order->getRef(),
-                        '%id' => ! isset($order) ? "?" : $order->getId()
+                        '%ref' => !isset($order) ? "?" : $order->getRef(),
+                        '%id' => !isset($order) ? "?" : $order->getId()
                     )
                 )
             );
@@ -119,59 +118,76 @@ abstract class BasePaymentModuleController extends BaseFrontController
     }
 
     /**
-     * Process the cancelation of a payment on the payment gateway. The order will go back to the
+     * Process the cancellation of a payment on the payment gateway. The order will go back to the
      * "not paid" status.
      *
-     * @param int $order_id the order ID
+     * @param int $orderId the order ID
      */
-    public function cancelPayment($order_id)
+    public function cancelPayment($orderId)
     {
-        $order_id = intval($order_id);
+        try {
+            $orderId = intval($orderId);
 
-        if (null !== $order = $this->getOrder($order_id)) {
-            $this->getLog()->addInfo(
+            if (null !== $order = $this->getOrder($orderId)) {
+                $this->getLog()->addInfo(
+                    $this->getTranslator()->trans(
+                        "Processing cancelation of payment for order ref. %ref",
+                        array('%ref' => $order->getRef())
+                    )
+                );
+
+                $event = new OrderEvent($order);
+
+                $event->setStatus(OrderStatusQuery::getNotPaidStatus()->getId());
+
+                $this->dispatch(TheliaEvents::ORDER_UPDATE_STATUS, $event);
+
+                $this->getLog()->addInfo(
+                    $this->getTranslator()->trans(
+                        "Order ref. %ref is now unpaid.",
+                        array('%ref' => $order->getRef())
+                    )
+                );
+            }
+        } catch (\Exception $ex) {
+            $this->getLog()->addError(
                 $this->getTranslator()->trans(
-                    "Processing cancelation of payment for order ref. %ref",
-                    array('%ref' => $order->getRef())
+                    "Error occurred while cancelling order ref. %ref, ID %id: %err",
+                    array(
+                        '%err' => $ex->getMessage(),
+                        '%ref' => !isset($order) ? "?" : $order->getRef(),
+                        '%id' => !isset($order) ? "?" : $order->getId()
+                    )
                 )
             );
 
-            $event = new OrderEvent($order);
-
-            $event->setStatus(OrderStatus::CODE_NOT_PAID);
-
-            $this->getLog()->addInfo(
-                $this->getTranslator()->trans(
-                    "Order ref. %ref is now unpaid.",
-                    array('%ref' => $order->getRef())
-                )
-            );
-
-            $this->dispatch(TheliaEvents::ORDER_UPDATE_STATUS, $event);
+            throw $ex;
         }
     }
 
     /**
      * Get an order and issue a log message if not found.
-     * @param $order_id
+     *
+     * @param $orderId
      * @return null|\Thelia\Model\Order
      */
-    protected function getOrder($order_id)
+    protected function getOrder($orderId)
     {
-        if (null == $order = OrderQuery::create()->findPk($order_id)) {
+        if (null == $order = OrderQuery::create()->findPk($orderId)) {
             $this->getLog()->addError(
-                $this->getTranslator()->trans("Unknown order ID:  %id", array('%id' => $order_id))
+                $this->getTranslator()->trans("Unknown order ID:  %id", array('%id' => $orderId))
             );
         }
 
         return $order;
     }
+
     /**
      * Redirect the customer to the successful payment page.
      *
-     * @param int $order_id the order ID
+     * @param int $orderId the order ID
      */
-    public function redirectToSuccessPage($order_id)
+    public function redirectToSuccessPage($orderId)
     {
         $this->getLog()->addInfo("Redirecting customer to payment success page");
 
@@ -180,7 +196,7 @@ abstract class BasePaymentModuleController extends BaseFrontController
                 'order.placed',
                 [],
                 [
-                    'order_id' => $order_id
+                    'order_id' => $orderId
                 ],
                 Router::ABSOLUTE_PATH
             )
@@ -190,10 +206,10 @@ abstract class BasePaymentModuleController extends BaseFrontController
     /**
      * Redirect the customer to the failure payment page. if $message is null, a generic message is displayed.
      *
-     * @param int         $order_id the order ID
-     * @param string|null $message  an error message.
+     * @param int $orderId the order ID
+     * @param string|null $message an error message.
      */
-    public function redirectToFailurePage($order_id, $message)
+    public function redirectToFailurePage($orderId, $message)
     {
         $this->getLog()->addInfo("Redirecting customer to payment failure page");
 
@@ -202,7 +218,7 @@ abstract class BasePaymentModuleController extends BaseFrontController
                 'order.failed',
                 [],
                 [
-                    'order_id' => $order_id,
+                    'order_id' => $orderId,
                     'message' => $message
                 ],
                 Router::ABSOLUTE_PATH
