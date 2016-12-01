1a7d250a7ab32dcebb3980b8a68bdbb1e647cd9a
netounet <netounet@hotmail.fr>
2016-12-01 09:39:19 +0100

    Update Content.php

--- core/lib/Thelia/Core/Template/Loop/Content.php
+++ core/lib/Thelia/Core/Template/Loop/Content.php
@@ -170,7 +170,7 @@ class Content extends BaseI18nLoop implements PropelSearchLoopInterface, SearchL
         }
 
         $search->withColumn(
-            'CASE WHEN ISNULL(`FolderSelect`.POSITION) THEN \'' . PHP_INT_MAX . '\' ELSE `FolderSelect`.POSITION END',
+            'CAST(CASE WHEN ISNULL(`FolderSelect`.POSITION) THEN \'' . PHP_INT_MAX . '\' ELSE `FolderSelect`.POSITION END AS SIGNED)',
             'position_delegate'
         );
         $search->withColumn('`FolderSelect`.FOLDER_ID', 'default_folder_id');
