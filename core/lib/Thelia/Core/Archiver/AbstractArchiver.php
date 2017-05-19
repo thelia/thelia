d91166850ba2a8172a910a9bfd188b1998c0c9c9
InformatiqueProg <contact@informatiqueprog.net>
2017-05-19 17:18:42 +0200

    Only check if is available on demand

--- core/lib/Thelia/Core/Archiver/AbstractArchiver.php
+++ core/lib/Thelia/Core/Archiver/AbstractArchiver.php
@@ -14,7 +14,6 @@ namespace Thelia\Core\Archiver;
 
 use Thelia\Core\Translation\Translator;
 
-
 /**
  * Class AbstractArchiver
  * @author Jérôme Billiras <jbilliras@openstudio.fr>
@@ -31,9 +30,9 @@ abstract class AbstractArchiver implements ArchiverInterface
      */
     protected $archivePath;
 
-    public function __construct()
+    public function __construct($checkIsAvailable = false)
     {
-      if (!$this->isAvailable()) {
+      if ($checkIsAvailable && !$this->isAvailable()) {
         throw new \Exception(
           Translator::getInstance()->trans(
             "The archiver :name is not available. Please install the php extension :extension first.",
--- core/lib/Thelia/Form/ModuleInstallForm.php
+++ core/lib/Thelia/Form/ModuleInstallForm.php
@@ -129,7 +129,7 @@ class ModuleInstallForm extends BaseForm
     protected function unzipModule(UploadedFile $file)
     {
         $extractPath = false;
-        $zip = new ZipArchiver();
+        $zip = new ZipArchiver(true);
         if (!$zip->open($file->getRealPath())) {
           throw new \Exception("unable to open zipfile");
         }
