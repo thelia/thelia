8a3c62bb8648303e711e8a30bf281d1026c4c065
Gilles <gilles.bourgeat@gmail.com>
2018-03-04 22:57:31 +0100

    Composer add symfony/var-dumper

--- core/lib/Thelia/Action/Pdf.php
+++ core/lib/Thelia/Action/Pdf.php
@@ -40,6 +40,7 @@ class Pdf extends BaseAction implements EventSubscriberInterface
         $html2pdf->pdf->SetDisplayMode('real');
 
         $html2pdf->writeHTML($event->getContent());
+
         $event->setPdf($html2pdf->output('output.pdf', 'S'));
     }
 
