bb9d43baf63bcac271d6f7d32d309f9bc1424165
Bertrand Tourlonias <btourlonias@openstudio.fr>
2019-03-28 11:15:10 +0100

    Add option to show/Hide stats bloc

--- local/modules/HookAdminHome/Hook/AdminHook.php
+++ local/modules/HookAdminHome/Hook/AdminHook.php
@@ -26,46 +26,59 @@ class AdminHook extends BaseHook
 {
     public function blockStatistics(HookRenderEvent $event)
     {
-        $event->add($this->render('block-statistics.html'));
+        if (1 == HookAdminHome::getConfigValue(HookAdminHome::ACTIVATE_STATS, 1)) {
+            $event->add($this->render('block-statistics.html'));
+        }
+
+        $event->add($this->render('hook-admin-home-config.html'));
+
     }
 
     public function blockStatisticsJs(HookRenderEvent $event)
     {
-        $event->add($this->render('block-statistics-js.html'));
+        if (1 == HookAdminHome::getConfigValue(HookAdminHome::ACTIVATE_STATS, 1)) {
+            $event->add($this->render('block-statistics-js.html'));
+        }
     }
 
     public function blockSalesStatistics(HookRenderBlockEvent $event)
     {
-        $content = trim($this->render("block-sales-statistics.html"));
-        if (!empty($content)) {
-            $event->add([
-                "id" => "block-sales-statistics",
-                "title" => $this->trans("Sales statistics", [], HookAdminHome::DOMAIN_NAME),
-                "content" => $content
-            ]);
+        if (1 == HookAdminHome::getConfigValue(HookAdminHome::ACTIVATE_SALES, 1)) {
+            $content = trim($this->render("block-sales-statistics.html"));
+            if (!empty($content)) {
+                $event->add([
+                    "id" => "block-sales-statistics",
+                    "title" => $this->trans("Sales statistics", [], HookAdminHome::DOMAIN_NAME),
+                    "content" => $content
+                ]);
+            }
         }
     }
 
     public function blockNews(HookRenderBlockEvent $event)
     {
-        $content = trim($this->render("block-news.html"));
-        if (!empty($content)) {
-            $event->add([
-                "id" => "block-news",
-                "content" => $content
-            ]);
+        if (1 == HookAdminHome::getConfigValue(HookAdminHome::ACTIVATE_NEWS, 1)) {
+            $content = trim($this->render("block-news.html"));
+            if (!empty($content)) {
+                $event->add([
+                    "id" => "block-news",
+                    "content" => $content
+                ]);
+            }
         }
     }
 
     public function blockTheliaInformation(HookRenderBlockEvent $event)
     {
-        $content = trim($this->render("block-thelia-information.html"));
-        if (!empty($content)) {
-            $event->add([
-                "id" => "block-thelia-information",
-                "title" => $this->trans("Thelia informations", [], HookAdminHome::DOMAIN_NAME),
-                "content" => $content
-            ]);
+        if (1 == HookAdminHome::getConfigValue(HookAdminHome::ACTIVATE_INFO, 1)) {
+            $content = trim($this->render("block-thelia-information.html"));
+            if (!empty($content)) {
+                $event->add([
+                    "id" => "block-thelia-information",
+                    "title" => $this->trans("Thelia informations", [], HookAdminHome::DOMAIN_NAME),
+                    "content" => $content
+                ]);
+            }
         }
     }
 }
