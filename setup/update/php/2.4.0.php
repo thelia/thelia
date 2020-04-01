c9e9d17e161a70ad74ef553a7be8f0ededf46497
Vincent Lopes-Vicente <vlopesvicente@gmail.com>
2020-04-01 15:51:47 +0200

    Fix ignored_module_hook table update

--- setup/update/php/2.4.0.php
+++ setup/update/php/2.4.0.php
@@ -19,3 +19,20 @@ $sql = "INSERT INTO `config` (`name`, `value`, `secured`, `hidden`, `created_at`
 $now = (new \DateTime())->format('Y-m-d H:i:s');
 
 $pdo->prepare($sql)->execute([ $lastOrderId, $now, $now ]);
+
+
+// Add ignored_module_hook.created_at if not exist
+$ignoredModuleHookCreatedAtColumnExitSql = "SHOW COLUMNS FROM `ignored_module_hook` LIKE 'created_at'";
+if (0 === count($pdo->query($ignoredModuleHookCreatedAtColumnExitSql)->fetchAll())) {
+    $addIgnoredModuleHookCreatedAtColumnSql = "ALTER TABLE `ignored_module_hook` ADD `created_at` DATETIME NOT NULL;";
+    $pdo->query($addIgnoredModuleHookCreatedAtColumnSql);
+}
+
+// Add ignored_module_hook.updated_at if not exist
+$ignoredModuleHookUpdatedAtColumnExitSql = "SHOW COLUMNS FROM `ignored_module_hook` LIKE 'updated_at'";
+if (0 === count($pdo->query($ignoredModuleHookUpdatedAtColumnExitSql)->fetchAll())) {
+    $addIgnoredModuleHookUpdatedAtColumnSql = "ALTER TABLE `ignored_module_hook` ADD `updated_at` DATETIME NOT NULL;";
+    $pdo->query($addIgnoredModuleHookUpdatedAtColumnSql);
+}
+
+
