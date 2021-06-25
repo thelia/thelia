f9b9d856743fe10d103d90222363803eab4e8213
Damien Foulhoux <Lucanis@users.noreply.github.com>
2021-06-25 09:33:22 +0200

    codefix for latest release of react-hook-form (#3)

--- templates/frontOffice/modern/components/React/AddCoupon/index.js
+++ templates/frontOffice/modern/components/React/AddCoupon/index.js
@@ -8,46 +8,48 @@ import { useForm } from 'react-hook-form';
 import { useIntl } from 'react-intl';
 
 export default function AddCoupon() {
-	const { register, handleSubmit, errors, formState } = useForm();
-	const { mutate: create, error } = useCouponCreate();
-	const intl = useIntl();
-	return (
-		<div className="flex flex-col items-stretch flex-wrap leading-none">
-			<form
-				className="flex justify-between items-center flex-wrap"
-				onSubmit={handleSubmit((values) => {
-					create(values.coupon);
-				})}
-			>
-				<label htmlFor="coupon" className="text-base uppercase m-0">
-					{intl.formatMessage({ id: 'COUPON' })}
-				</label>
-				<div className="flex items-stretch">
-					<Input
-						id="coupon"
-						name="coupon"
-						ref={register({
-							required: intl.formatMessage({ id: 'MANDATORY' })
-						})}
-						placeholder={intl.formatMessage({ id: 'COUPON' })}
-						className="w-full h-auto"
-					/>
-					<SubmitButton
-						type="submit"
-						isSubmitting={formState.isSubmitting}
-						className="py-0"
-						label={intl.formatMessage({ id: 'OK' })}
-					/>
-				</div>
-			</form>
-			{errors.coupon?.message ? <Error error={errors.coupon?.message} /> : null}
-			{error ? (
-				<Alert
-					type="error"
-					title={error.response?.data?.title}
-					message={error.response?.data?.description}
-				/>
-			) : null}
-		</div>
-	);
+  const { register, handleSubmit, formState } = useForm();
+  const { mutate: create, error } = useCouponCreate();
+  const intl = useIntl();
+
+  return (
+    <div className="flex flex-col flex-wrap items-stretch leading-none">
+      <form
+        className="flex flex-wrap items-center justify-between"
+        onSubmit={handleSubmit((values) => {
+          create(values.coupon);
+        })}
+      >
+        <label htmlFor="coupon" className="m-0 text-base uppercase">
+          {intl.formatMessage({ id: 'COUPON' })}
+        </label>
+        <div className="flex items-stretch">
+          <Input
+            id="coupon"
+            {...register('coupon', {
+              required: intl.formatMessage({ id: 'MANDATORY' })
+            })}
+            placeholder={intl.formatMessage({ id: 'COUPON' })}
+            className="w-full h-auto"
+          />
+          <SubmitButton
+            type="submit"
+            isSubmitting={formState.isSubmitting}
+            className="py-0"
+            label={intl.formatMessage({ id: 'OK' })}
+          />
+        </div>
+      </form>
+      {formState.errors?.coupon?.message ? (
+        <Error error={formState.errors.coupon?.message} />
+      ) : null}
+      {error ? (
+        <Alert
+          type="error"
+          title={error.response?.data?.title}
+          message={error.response?.data?.description}
+        />
+      ) : null}
+    </div>
+  );
 }
--- templates/frontOffice/modern/components/React/Checkbox/index.js
+++ templates/frontOffice/modern/components/React/Checkbox/index.js
@@ -1,19 +1,19 @@
 import React, { forwardRef } from 'react';
 
 const Checkbox = forwardRef(({ label, name, small = false, ...props }, ref) => {
-	return (
-		<label className="inline-flex items-center">
-			<input
-				type="checkbox"
-				name={name}
-				{...props}
-				ref={ref}
-				className={`border-gray-300 border text-main focus:border-gray-300 focus:ring-main`}
-			/>
+  return (
+    <label className="inline-flex items-center">
+      <input
+        type="checkbox"
+        name={name}
+        {...props}
+        ref={ref}
+        className={`border-gray-300 border text-main focus:border-gray-300 focus:ring-main`}
+      />
 
-			<span className="ml-2">{label}</span>
-		</label>
-	);
+      <span className="ml-2">{label}</span>
+    </label>
+  );
 });
 
 export default Checkbox;
