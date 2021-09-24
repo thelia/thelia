0d73fc5b0d6ca2636456982b7fe80755261e1c1b
Mathis Chambon <matchambon60@gmail.com>
2021-09-24 10:27:33 +0200

    Fix a i18n mistake

--- local/modules/Cheque/I18n/frontOffice/default/fr_FR.php
+++ local/modules/Cheque/I18n/frontOffice/default/fr_FR.php
@@ -11,6 +11,6 @@
  */
 
 return [
-    'Be sure to sign your cheque !' => 'N\'oubliez par de signer votre chèque !',
+    'Be sure to sign your cheque !' => 'N\'oubliez pas de signer votre chèque !',
     'Please make your cheque payable to <b>%name</b>, and send it to the following address :' => 'Merci de libeller votre chèque à l\'ordre de %name, et de l\'expédier à l\'adresse suivante :',
 ];
