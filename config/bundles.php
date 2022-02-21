e3d4c18662bd3b02a963ee1e09b011c40d44a2ac
Damien Foulhoux <dfoulhoux@openstudio.fr>
2022-02-21 14:16:50 +0100

    remove wrongly commited files

--- config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-];
