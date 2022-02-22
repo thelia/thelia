3ab3a1fb6f95cf72c72dfe0d0759c11fab5509f8
Liz <lmaestracci@openstudio.fr>
2022-02-22 16:47:31 +0100

    tabs

---
+++ templates/backOffice/modern-bo/components/Tabs/Tabs.js
@@ -0,0 +1,17 @@
+export const Tabs = () => {
+    console.log('prout')
+    document.querySelectorAll('.Tabs').forEach(
+        e => {
+            console.log(e)
+        },
+
+        document.addEventListener("click", function() {
+        
+            document.querySelector("Tabs").querySelectorAll(".TabsContent > div");
+
+            },
+        )
+    )
+};
+
+
