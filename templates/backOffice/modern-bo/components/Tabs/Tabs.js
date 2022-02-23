4b484512428bccfeee201fcc4832f23afb045d1e
Liz <lmaestracci@openstudio.fr>
2022-02-23 16:39:36 +0100

    tabs js alone

--- templates/backOffice/modern-bo/components/Tabs/Tabs.js
+++ templates/backOffice/modern-bo/components/Tabs/Tabs.js
@@ -2,21 +2,31 @@
 
 
 export const Tabs = () => {
-    console.log('prout')
-    document.querySelectorAll('.Tabs').forEach(
-        e => {
-            console.log(e)
-        },
-
-        document.addEventListener("click", function() {
-        
-            document.querySelector("Tabs").querySelectorAll(".TabsContent > div");
-
-            },
-        )
-    )
     
-};
+    let tabs = document.querySelectorAll(".TabHeader a");
+    // let content = document.querySelectorAll(".TabsContent");
+
+    for(let i = 0 ; i < tabs.length; i++){
+        tabs[i].addEventListener('click', () => click(i));
+     }
+
+        function removeActive() {
+            for (let i = 0; i < tabs.length; i++) {
+                tabs[i].classList.remove('active');
+                // content[i].classList.remove('active');
+            }
+        }
+
+        function click(currentTab) {
+            removeActive();
+            tabs[currentTab].classList.add('active');
+            // content[currentTab].classList.add('active');
+            console.log(currentTab);
+        }
+
+       
+    }
+    
 
 
 
