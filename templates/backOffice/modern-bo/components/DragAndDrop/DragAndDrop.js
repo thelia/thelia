146059b59c5e79621ee89a7d7b3ae6ebc0af42cb
Liz <lmaestracci@openstudio.fr>
2022-03-15 17:02:02 +0100

    templates configuration

--- templates/backOffice/modern-bo/components/DragAndDrop/DragAndDrop.js
+++ templates/backOffice/modern-bo/components/DragAndDrop/DragAndDrop.js
@@ -1,4 +1,26 @@
 export const DragAndDrop = () => {
- 
+    
+    const fileUpload = document.getElementById('file_upload');
+    for (let i = 0; i < fileUpload.length; i++){
+        fileUpload[i].addEventListener("click", e => {
+            uploadFiles(e);
+        })
+    }
+
+
+    function uploadFiles() {
+        const files = document.getElementById('file_upload').files;
+
+        if(files.length === 0){
+          alert("Attention, vous devez choisir un fichier");
+          return;
+        }
+
+        let filenames="";
+        for(let i = 0; i < files.length; i++){
+          filenames+=files[i].name+"\n";
+        }
+        alert("Vous avez sélectionné:"+filenames);
+      }
 }
   
