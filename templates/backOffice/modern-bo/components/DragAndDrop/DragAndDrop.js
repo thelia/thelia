460757c8b0c9fba2fc351e2b4e4abeff8b6e5829
Liz <lmaestracci@openstudio.fr>
2022-03-17 16:49:22 +0100

    configuration pages

--- templates/backOffice/modern-bo/components/DragAndDrop/DragAndDrop.js
+++ templates/backOffice/modern-bo/components/DragAndDrop/DragAndDrop.js
@@ -1,26 +1,51 @@
 export const DragAndDrop = () => {
+  var dropzone = document.getElementById('dropzone');
+  var dropzone_input = dropzone.querySelector('.dropzone-input');
+  var multiple = dropzone_input.getAttribute('multiple') ? true : false;
+  
+  ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(function(event) {
+    dropzone.addEventListener(event, function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    });
+  });
+  
+  dropzone.addEventListener('dragover', function(e) {
+    this.classList.add('dropzone-dragging');
+  }, false);
+  
+  dropzone.addEventListener('dragleave', function(e) {
+    this.classList.remove('dropzone-dragging');
+  }, false);
+  
+  dropzone.addEventListener('drop', function(e) {
+    this.classList.remove('dropzone-dragging');
+    var files = e.dataTransfer.files;
+    var dataTransfer = new DataTransfer();
     
-    const fileUpload = document.getElementById('file_upload');
-    for (let i = 0; i < fileUpload.length; i++){
-        fileUpload[i].addEventListener("click", e => {
-            uploadFiles(e);
-        })
-    }
-
-
-    function uploadFiles() {
-        const files = document.getElementById('file_upload').files;
-
-        if(files.length === 0){
-          alert("Attention, vous devez choisir un fichier");
-          return;
-        }
-
-        let filenames="";
-        for(let i = 0; i < files.length; i++){
-          filenames+=files[i].name+"\n";
-        }
-        alert("Vous avez sélectionné:"+filenames);
+    var for_alert = "";
+    Array.prototype.forEach.call(files, file => {
+      for_alert += "# " + file.name +
+      " (" + file.type + " | " + file.size +
+      " bytes)\r\n";
+      dataTransfer.items.add(file);
+      if (!multiple) {
+        return false;
       }
+    });
+  
+    var filesToBeAdded = dataTransfer.files;
+    dropzone_input.files = filesToBeAdded;
+    alert(for_alert);
+    
+  }, false);
+  
+  dropzone.addEventListener('click', function(e) {
+    dropzone_input.click();
+  });
+  
+  
+  
+  
 }
   
