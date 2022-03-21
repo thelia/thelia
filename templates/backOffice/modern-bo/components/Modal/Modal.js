be03ce6c55df0e02430017ce31f999c9fec9a5c2
Liz <lmaestracci@openstudio.fr>
2022-03-21 17:25:12 +0100

    configuration pages

--- templates/backOffice/modern-bo/components/Modal/Modal.js
+++ templates/backOffice/modern-bo/components/Modal/Modal.js
@@ -1,16 +1,51 @@
 export const Modal = () => {
-    let updateButton = document.getElementById('updateDetails');
-    let favDialog = document.getElementById('favDialog');
-    let selectEl = document.querySelector('select');
-    let confirmBtn = document.getElementById('confirmBtn');
-    
-    updateButton.addEventListener('click', function onOpen() {
-      if (typeof favDialog.showModal === "function") {
-        favDialog.showModal();
-      } 
-    });
-    selectEl.addEventListener('change', function onSelect(e) {
-      confirmBtn.value = selectEl.value;
-    });
+  const openBtn = document.querySelectorAll('[data-modal-target]')
+  const closeBtn = document.querySelectorAll("[data-modal-close]")
+  const overlay = document.getElementById("overlay")
+  const nextbtn = document.querySelector('.nextbtn')
+  const currentParagraph = document.querySelector('.current')
+  const nextParagraph = document.querySelector('.next')
+  const tooltip = document.querySelector('.tooltip')
+  
+  nextbtn.addEventListener('click', (() => {
+      currentParagraph.classList.toggle('inactive')
+      nextParagraph.classList.toggle('active')
+      tooltip.classList.toggle('active')
+  }))
+  
+  openBtn.forEach((btn) => {
+      const modal = document.querySelector(btn.dataset.modalTarget) //Checks the target of our data-modal-target. could have also used '.modal'
+      btn.addEventListener('click', (() => {
+      openModal(modal)
+      }))
+  })
+  
+  closeBtn.forEach((btn) => {
+      const modal = btn.closest(".modal")
+      btn.addEventListener('click', (() => {
+      closeModal(modal)
+      }))  
+  })
+  
+  overlay.addEventListener('click', (() => {
+      const modals = document.querySelectorAll('.modal.active')
+      modals.forEach((modal) => {
+      closeModal(modal)
+      })  
+  }))
+  
+  
+  function openModal(modal){
+      if(modal === undefined) return
+      modal.classList.add('active')
+      overlay.classList.add('active')
+  }
+  
+  function closeModal(modal){
+      if(modal === undefined) return
+      modal.classList.remove('active')
+      overlay.classList.remove('active')
+  }
+  
 }
 
