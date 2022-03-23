4566e924f8a3c3954004269567bb9e425c677e9b
Liz <lmaestracci@openstudio.fr>
2022-03-23 16:51:29 +0100

    change <button> to <a>

--- templates/backOffice/modern-bo/components/Modal/Modal.js
+++ templates/backOffice/modern-bo/components/Modal/Modal.js
@@ -1,51 +1,13 @@
 export const Modal = () => {
-  const openBtn = document.querySelectorAll('[data-modal-target]')
-  const closeBtn = document.querySelectorAll("[data-modal-close]")
-  const overlay = document.getElementById("overlay")
-  const nextbtn = document.querySelector('.nextbtn')
-  const currentParagraph = document.querySelector('.current')
-  const nextParagraph = document.querySelector('.next')
-  const tooltip = document.querySelector('.tooltip')
-  
-  nextbtn.addEventListener('click', (() => {
-      currentParagraph.classList.toggle('inactive')
-      nextParagraph.classList.toggle('active')
-      tooltip.classList.toggle('active')
-  }))
-  
-  openBtn.forEach((btn) => {
-      const modal = document.querySelector(btn.dataset.modalTarget) //Checks the target of our data-modal-target. could have also used '.modal'
-      btn.addEventListener('click', (() => {
-      openModal(modal)
-      }))
-  })
-  
-  closeBtn.forEach((btn) => {
-      const modal = btn.closest(".modal")
-      btn.addEventListener('click', (() => {
-      closeModal(modal)
-      }))  
-  })
-  
-  overlay.addEventListener('click', (() => {
-      const modals = document.querySelectorAll('.modal.active')
-      modals.forEach((modal) => {
-      closeModal(modal)
-      })  
-  }))
-  
-  
-  function openModal(modal){
-      if(modal === undefined) return
-      modal.classList.add('active')
-      overlay.classList.add('active')
-  }
-  
-  function closeModal(modal){
-      if(modal === undefined) return
-      modal.classList.remove('active')
-      overlay.classList.remove('active')
-  }
-  
+    const modal= document.querySelector('.modal');
+    const openModal= document.querySelector('.open-button');
+    const closeModal= document.querySelector('.close-button');
+    
+    openModal.addEventListener('click', () => {
+      modal.showModal();
+    });
+    
+    closeModal.addEventListener('click', () => {
+      modal.close();
+    });
 }
-
