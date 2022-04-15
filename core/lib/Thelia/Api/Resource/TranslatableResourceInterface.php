ecd6fcde24e4511d7157e449c1912a62df105701
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2022-04-15 16:59:39 +0200

    Add persistence to api

--- core/lib/Thelia/Api/Resource/TranslatableResourceInterface.php
+++ core/lib/Thelia/Api/Resource/TranslatableResourceInterface.php
@@ -6,6 +6,8 @@ interface TranslatableResourceInterface
 {
     public function setI18ns(array $i18ns): self;
 
+    public function getI18ns(): array;
+
     public static function getTranslatableFields(): array;
 
     public static function getI18nResourceClass(): string;
