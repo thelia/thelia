247a2b993ec3292fef91a19f1107c8f50d8b05a9
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2022-04-15 16:59:39 +0200

    Add persistence to api

--- core/lib/Thelia/Api/Resource/PropelResourceInterface.php
+++ core/lib/Thelia/Api/Resource/PropelResourceInterface.php
@@ -4,5 +4,9 @@ namespace Thelia\Api\Resource;
 
 interface PropelResourceInterface
 {
+    public function getId();
+
+    public function setId(int $id);
+
     public static function getPropelModelClass(): string;
 }
