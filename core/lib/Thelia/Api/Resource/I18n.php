0852883de9dadf8a1ae459a527a22339caabdf13
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2022-04-15 16:59:39 +0200

    Add persistence to api

--- core/lib/Thelia/Api/Resource/I18n.php
+++ core/lib/Thelia/Api/Resource/I18n.php
@@ -14,70 +14,45 @@ abstract class I18n
 
     private ?string $chapo;
 
-    /**
-     * @return int
-     */
+
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
     public function setId(int $id): self
     {
         $this->id = $id;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getLocale(): string
     {
         return $this->locale;
     }
 
-    /**
-     * @param string $locale
-     */
     public function setLocale(string $locale): self
     {
         $this->locale = $locale;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @param string|null $title
-     * @return I18n
-     */
     public function setTitle(?string $title): self
     {
         $this->title = $title;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getChapo(): string
     {
         return $this->chapo;
     }
 
-    /**
-     * @param string|null $chapo
-     * @return I18n
-     */
     public function setChapo(?string $chapo): self
     {
         $this->chapo = $chapo;
