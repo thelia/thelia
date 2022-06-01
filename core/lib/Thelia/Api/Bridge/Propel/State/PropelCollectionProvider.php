9b662e8cddd17c5308d41dd4bdccb70522c1cd97
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2022-06-01 09:35:11 +0200

    Try to update api-platform

--- core/lib/Thelia/Api/Bridge/Propel/State/PropelCollectionProvider.php
+++ core/lib/Thelia/Api/Bridge/Propel/State/PropelCollectionProvider.php
@@ -1,29 +1,59 @@
 <?php
 
-namespace Thelia\Api\Bridge\Propel\Extension;
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
-use Doctrine\ORM\QueryBuilder;
+namespace Thelia\Api\Bridge\Propel\State;
+
+use ApiPlatform\Exception\RuntimeException;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
 use Propel\Runtime\ActiveQuery\ModelCriteria;
+use Thelia\Api\Bridge\Propel\Extension\QueryResultCollectionExtensionInterface;
 use Thelia\Api\Resource\I18n;
+use Thelia\Api\Resource\PropelResourceInterface;
 use Thelia\Api\Resource\TranslatableResourceInterface;
 use Thelia\Model\LangQuery;
 
-/**
- * Transform propel models to api resources
- */
-final class ResourceResultExtension
+class PropelCollectionProvider implements ProviderInterface
 {
-    public function supportsResult(string $resourceClass, string $operationName = null): bool
+    public function __construct(private iterable $propelCollectionExtensions = [])
     {
-        return true;
     }
 
-    public function getResult(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = [])
+    public function provide(Operation $operation, array $uriVariables = [], array $context = [])
     {
+        $resourceClass = $operation->getClass();
+
+        if (!is_subclass_of($resourceClass, PropelResourceInterface::class)) {
+            throw new RuntimeException('Bad provider');
+        }
+
+        /** @var ModelCriteria $queryClass */
+        $queryClass = $resourceClass::getPropelModelClass().'Query';
+
+        /** @var ModelCriteria $query */
+        $query = $queryClass::create();
+
+        foreach ($this->propelCollectionExtensions as $extension) {
+            $extension->applyToCollection($query, $resourceClass, $operation->getName(), $context);
+
+            if ($extension instanceof QueryResultCollectionExtensionInterface && $extension->supportsResult($resourceClass, $operation->getName(), $context)) {
+                return $extension->getResult($query, $resourceClass, $operation->getName(), $context);
+            }
+        }
+
         $langs = LangQuery::create()->filterByActive(1)->find();
 
-        return array_map(
+
+        $items = array_map(
             function ($propelModel) use ($resourceClass, $langs) {
                 $apiResource = new $resourceClass();
                 foreach (get_class_methods($apiResource) as $methodName) {
@@ -54,10 +84,13 @@ final class ResourceResultExtension
                     }
                 }
 
+
                 return $apiResource;
             },
             iterator_to_array($query->find())
         );
+
+        return $items;
     }
 
     private function setI18nFieldValue(I18n $i18nResource, $lang, $field, $propelModel): void
@@ -78,8 +111,4 @@ final class ResourceResultExtension
         $i18nResource->$setter($value);
     }
 
-    public function applyToCollection(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = [])
-    {
-        // TODO: Implement applyToCollection() method.
-    }
 }
