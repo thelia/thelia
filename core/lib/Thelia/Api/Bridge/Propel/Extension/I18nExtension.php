36972a7274c9010a0a903ed079259208008fee11
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2022-06-01 09:35:11 +0200

    Try to update api-platform

--- core/lib/Thelia/Api/Bridge/Propel/Extension/I18nExtension.php
+++ core/lib/Thelia/Api/Bridge/Propel/Extension/I18nExtension.php
@@ -1,25 +1,34 @@
 <?php
 
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Thelia\Api\Bridge\Propel\Extension;
 
 use Propel\Runtime\ActiveQuery\ModelCriteria;
 use Thelia\Api\Resource\TranslatableResourceInterface;
 use Thelia\Model\LangQuery;
-use Thelia\Model\Map\ProductI18nTableMap;
 
 final class I18nExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
-    public function applyToCollection(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = [])
+    public function applyToCollection(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = []): void
     {
         $this->apply($query, $resourceClass, $operationName, $context);
     }
 
-    public function applyToItem(ModelCriteria $query, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    public function applyToItem(ModelCriteria $query, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
     {
         $this->apply($query, $resourceClass, $operationName, $context);
     }
 
-    public function apply(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = [])
+    public function apply(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = []): void
     {
         if (!is_subclass_of($resourceClass, TranslatableResourceInterface::class)) {
             return;
@@ -27,7 +36,7 @@ final class I18nExtension implements QueryCollectionExtensionInterface, QueryIte
 
         $langs = LangQuery::create()->filterByActive(1)->find();
         $resourceName = (new \ReflectionClass($resourceClass))->getShortName();
-        $joinMethodName = "join".$resourceName."I18n";
+        $joinMethodName = 'join'.$resourceName.'I18n';
         foreach ($langs as $lang) {
             $joinAlias = 'lang_'.$lang->getLocale().'_';
             $query->$joinMethodName($joinAlias);
