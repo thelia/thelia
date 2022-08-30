5daa4aca1963c46ae3e21ca31a7e5e63f430c599
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2022-08-30 08:54:24 +0200

    Fix feature loop (#3028)

--- core/lib/Thelia/Core/Template/Loop/Feature.php
+++ core/lib/Thelia/Core/Template/Loop/Feature.php
@@ -146,7 +146,7 @@ class Feature extends BaseI18nLoop implements PropelSearchLoopInterface
             }
         }
 
-        if (!\count($template) > 0) {
+        if (\count($template) > 0) {
             // Join with feature_template table to get position, if a manual order position is required
             if (\count(array_diff(['manual_reverse', 'manual'], $this->getOrder())) < 2) {
                 $search
