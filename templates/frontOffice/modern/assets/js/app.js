96d7b495eebcce0c6b15d49ad7c9c0dc300cd560
yann <ymetier@openstudio.fr>
2022-09-02 11:11:09 +0200

    apiUtils accessible globaly on window object (provisional solution)

--- templates/frontOffice/modern/assets/js/app.js
+++ templates/frontOffice/modern/assets/js/app.js
@@ -4,6 +4,9 @@ import MiniCart from '@components/React/MiniCart';
 import MiniLogin from '@components/React/MiniLogin';
 import SvgAjax from '@utils/SvgAjax';
 import axios from 'axios';
+import * as apiUtils from '@openstudio/thelia-api-utils';
+
+window.apiUtils = apiUtils;
 
 function main() {
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
