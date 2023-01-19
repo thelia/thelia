cc68971084f05a27db23137b05ec50bbb4062422
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2023-01-19 17:04:54 +0100

    Add JWT auth

--- core/lib/Thelia/Core/Security/UserProvider/CustomerUserProvider.php
+++ core/lib/Thelia/Core/Security/UserProvider/CustomerUserProvider.php
@@ -13,16 +13,50 @@
 namespace Thelia\Core\Security\UserProvider;
 
 use Propel\Runtime\ActiveQuery\Criteria;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Thelia\Model\Customer;
 use Thelia\Model\CustomerQuery;
 
 class CustomerUserProvider implements UserProviderInterface
 {
-    public function getUser($key)
+    public function loadUserByIdentifier(string $identifier): UserInterface
     {
         $customer = CustomerQuery::create()
-            ->filterByEmail($key, Criteria::EQUAL)
+            ->filterByEmail($identifier, Criteria::EQUAL)
             ->findOne();
 
+        if (null === $customer) {
+            $e = new UserNotFoundException(sprintf('User "%s" not found.', $identifier));
+            $e->setUserIdentifier($identifier);
+
+            throw $e;
+        }
+
         return $customer;
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof Customer) {
+            throw new UnsupportedUserException(sprintf('Invalid user class "%s".', get_class($user)));
+        }
+
+        $user =  CustomerQuery::create()
+            ->filterByLogin($user->getUserIdentifier(), Criteria::EQUAL)
+            ->findOne();
+
+        if (null === $user) {
+            throw new UserNotFoundException("User not exist");
+        }
+
+        return $user;
+    }
+
+    public function supportsClass(string $class)
+    {
+        return Customer::class === $class || is_subclass_of($class, Customer::class);
     }
 }
