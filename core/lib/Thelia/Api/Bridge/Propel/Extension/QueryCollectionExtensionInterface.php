ef2daea524eb15d9c6f978cddc860176678b256d
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2023-01-27 10:35:18 +0100

    Add dynamic join for api ressources

--- core/lib/Thelia/Api/Bridge/Propel/Extension/QueryCollectionExtensionInterface.php
+++ core/lib/Thelia/Api/Bridge/Propel/Extension/QueryCollectionExtensionInterface.php
@@ -3,9 +3,10 @@
 namespace Thelia\Api\Bridge\Propel\Extension;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
 use Propel\Runtime\ActiveQuery\ModelCriteria;
 
 interface QueryCollectionExtensionInterface
 {
-    public function applyToCollection(ModelCriteria $query, string $resourceClass, string $operationName = null, array $context = []);
+    public function applyToCollection(ModelCriteria $query, string $resourceClass, Operation $operation = null, array $context = []);
 }
