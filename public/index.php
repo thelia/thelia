cf71903b1d571133a8d5d348e658787c87942250
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2023-04-13 14:39:22 +0200

    Prevent error on index.php in public directory

--- public/index.php
+++ public/index.php
@@ -10,23 +10,4 @@
  * file that was distributed with this source code.
  */
 
-use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__.\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'web'.\DIRECTORY_SEPARATOR.'index.php';
