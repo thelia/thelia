3c607eb62df0931fad78ece248264e7b55c6ddef
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-04-24 12:31:15 +0200

    api content and folder

---
+++ core/lib/Thelia/Api/Resource/FolderI18n.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Thelia\Api\Resource;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class FolderI18n extends I18n
+{
+    #[Groups([I18n::GROUP_READ])]
+    protected string $locale;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $title;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $description;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $chapo;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $postscriptum;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $metaTitle;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $metaDescription;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $metaKeywords;
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): FolderI18n
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): FolderI18n
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): FolderI18n
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getChapo(): ?string
+    {
+        return $this->chapo;
+    }
+
+    public function setChapo(?string $chapo): FolderI18n
+    {
+        $this->chapo = $chapo;
+        return $this;
+    }
+
+    public function getPostscriptum(): ?string
+    {
+        return $this->postscriptum;
+    }
+
+    public function setPostscriptum(?string $postscriptum): FolderI18n
+    {
+        $this->postscriptum = $postscriptum;
+        return $this;
+    }
+
+    public function getMetaTitle(): ?string
+    {
+        return $this->metaTitle;
+    }
+
+    public function setMetaTitle(?string $metaTitle): FolderI18n
+    {
+        $this->metaTitle = $metaTitle;
+        return $this;
+    }
+
+    public function getMetaDescription(): ?string
+    {
+        return $this->metaDescription;
+    }
+
+    public function setMetaDescription(?string $metaDescription): FolderI18n
+    {
+        $this->metaDescription = $metaDescription;
+        return $this;
+    }
+
+    public function getMetaKeywords(): ?string
+    {
+        return $this->metaKeywords;
+    }
+
+    public function setMetaKeywords(?string $metaKeywords): FolderI18n
+    {
+        $this->metaKeywords = $metaKeywords;
+        return $this;
+    }
+}
