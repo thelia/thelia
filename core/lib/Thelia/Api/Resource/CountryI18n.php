949814dbfe1d1e97d52d6fb23014d35591e2b403
ArthurLashermes <76913541+ArthurLashermes@users.noreply.github.com>
2023-04-24 09:40:59 +0200

    [API] Order, customer api ressource,column attribute, range and date filter (#3099)
    * order api feature
    
    * module api
    
    * create column attribute and adding ressources
    
    * adding afterModelToResource, taxed price
    
    * order api feature
    
    * module api
    
    * create column attribute and adding ressources
    
    * adding afterModelToResource, taxed price
    
    * fixing group for order and customer
    
    * Range filter and delete unused getCollection route
    
    * adding DateFilter
    
    * fix cart get
    
    * adding relationsAlias
    
    * fix/ add return this and add plurial on name route
    
    * add relations for fix double filter relation
    
    * fix/ add return this and add plurial on name route
    
    * fix relations alias of delivery address
    
    ---------
    
    Co-authored-by: ArthurLashermes <alashermes@openstudio.fr>

---
+++ core/lib/Thelia/Api/Resource/CountryI18n.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Thelia\Api\Resource;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class CountryI18n extends I18n
+{
+    #[Groups([I18n::GROUP_READ])]
+    protected string $locale;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $title;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $description;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $chapo;
+
+    #[Groups([I18n::GROUP_READ])]
+    protected ?string $postscriptum;
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): CountryI18n
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): CountryI18n
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): CountryI18n
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getChapo(): ?string
+    {
+        return $this->chapo;
+    }
+
+    public function setChapo(?string $chapo): CountryI18n
+    {
+        $this->chapo = $chapo;
+        return $this;
+    }
+
+    public function getPostscriptum(): ?string
+    {
+        return $this->postscriptum;
+    }
+
+    public function setPostscriptum(?string $postscriptum): CountryI18n
+    {
+        $this->postscriptum = $postscriptum;
+        return $this;
+    }
+}
