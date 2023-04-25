b7a77d845f611ba5645370c9bfa6414570254069
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-04-25 15:10:52 +0200

    api Config and ModuleConfig

--- core/lib/Thelia/Api/Resource/ModuleConfigI18n.php
+++ core/lib/Thelia/Api/Resource/ModuleConfigI18n.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Thelia\Api\Resource;
 
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -14,9 +24,10 @@ class ModuleConfigI18n extends I18n
         return $this->value;
     }
 
-    public function setValue(?string $value): ModuleConfigI18n
+    public function setValue(?string $value): self
     {
         $this->value = $value;
+
         return $this;
     }
 }
