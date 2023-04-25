9d1409a2281e97fdff949e8528a7233e862a94d1
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-04-25 09:28:42 +0200

    api attribute / attribute_av and feature / feature_av

---
+++ core/lib/Thelia/Api/Resource/AttributeAvI18n.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Thelia\Api\Resource;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class AttributeAvI18n extends I18n
+{
+    #[Groups([AttributeAv::GROUP_READ, AttributeAv::GROUP_WRITE])]
+    protected string $locale;
+
+    #[Groups([AttributeAv::GROUP_READ, AttributeAv::GROUP_WRITE])]
+    protected ?string $title;
+
+    #[Groups([AttributeAv::GROUP_READ, AttributeAv::GROUP_WRITE])]
+    protected ?string $description;
+
+    #[Groups([AttributeAv::GROUP_READ, AttributeAv::GROUP_WRITE])]
+    protected ?string $chapo;
+
+    #[Groups([AttributeAv::GROUP_READ, AttributeAv::GROUP_WRITE])]
+    protected ?string $postscriptum;
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): AttributeAvI18n
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): AttributeAvI18n
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): AttributeAvI18n
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getChapo(): ?string
+    {
+        return $this->chapo;
+    }
+
+    public function setChapo(?string $chapo): AttributeAvI18n
+    {
+        $this->chapo = $chapo;
+        return $this;
+    }
+
+    public function getPostscriptum(): ?string
+    {
+        return $this->postscriptum;
+    }
+
+    public function setPostscriptum(?string $postscriptum): AttributeAvI18n
+    {
+        $this->postscriptum = $postscriptum;
+        return $this;
+    }
+}
