38f365159d6857e1595fc0a491474049bd546ca1
Théo Cherblanc <tcherblanc@mbp-de-sysadmin.home>
2023-08-22 11:13:08 +0200

    convert React title component to TypeScript

--- templates/frontOffice/modern/components/React/Title/Title.tsx
+++ templates/frontOffice/modern/components/React/Title/Title.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
+import { TitleProps } from './Title.types';
 
-export default function Title({ title = 'HOME', className }) {
+export default function Title({ title = 'HOME', className }: TitleProps) {
   const intl = useIntl();
 
   return (
