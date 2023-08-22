178921c54cd42f063cbd2397d6b02ae7ba1ae132
Théo Cherblanc <tcherblanc@mbp-de-sysadmin.home>
2023-08-22 14:29:51 +0200

    convert React Steps component to TypeScript

--- templates/frontOffice/modern/components/React/Steps/Steps.tsx
+++ templates/frontOffice/modern/components/React/Steps/Steps.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCheckoutStep } from '@js/redux/modules/checkout';
+import { StepsProps } from './Steps.types';
 
-export default function Steps({ steps }) {
+export default function Steps({ steps }: StepsProps) {
   const intl = useIntl();
   const dispatch = useDispatch();
-  const { checkoutStep } = useSelector((state) => state.checkout);
+  const { checkoutStep } = useSelector((state: any) => state.checkout);
 
   return (
     <nav className="Steps">
       <ul className="Steps-list">
         {Object.values(steps).map((step, index) => {
           return (
-            <li key={index} onClick={() => dispatch(setCheckoutStep(step.id))} className={`Steps-item ${step.id === checkoutStep ? 'Steps-item--active' : ''}`}
+            <li
+              key={index}
+              onClick={() => dispatch(setCheckoutStep(step.id))}
+              className={`Steps-item ${
+                step.id === checkoutStep ? 'Steps-item--active' : ''
+              }`}
             >
               <span className="Steps-number">{step.id}</span>
-              <span className="Steps-name">{intl.formatMessage({ id: `${step.label}` })}</span>
+              <span className="Steps-name">
+                {intl.formatMessage({ id: `${step.label}` })}
+              </span>
             </li>
           );
         })}
