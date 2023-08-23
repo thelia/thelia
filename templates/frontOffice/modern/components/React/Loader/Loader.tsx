54811fbb6be0b9c2f51874a00bf0352bb06bb4b4
Théo Cherblanc <tcherblanc@mbp-de-sysadmin.home>
2023-08-23 15:11:23 +0200

    convert React Loader component to TypeScript

--- templates/frontOffice/modern/components/React/Loader/Loader.tsx
+++ templates/frontOffice/modern/components/React/Loader/Loader.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ReactComponent as IconLoader } from '@icons/loader.svg';
+import { LoaderProps } from './Loader.types';
 
-function Loader({ color = '#76b82a', className = 'w-40' }) {
+export default function Loader({
+  color = '#76b82a',
+  className = 'w-40'
+}: LoaderProps) {
   return (
     <div className={`${className} mx-auto block`}>
       <IconLoader className="h-6 w-6" />
     </div>
   );
 }
-
-export default Loader;
