51fdaebb83057da471c3b826a3601e10659edaec
Théo Cherblanc <tcherblanc@mbp-de-sysadmin.home>
2023-08-24 14:05:45 +0200

    convert React Checkout/Delivery component to TypeScript

--- templates/frontOffice/modern/components/React/Checkout/Pages/Delivery.tsx
+++ templates/frontOffice/modern/components/React/Checkout/Pages/Delivery.tsx
@@ -7,28 +7,37 @@ import Loader from '../../Loader';
 import AddressBook from '../AddressBook';
 import Map from '../../PickupMap';
 import { useAddressQuery } from '@openstudio/thelia-api-utils';
+import { CheckoutPageType, CheckoutResponse } from '@js/types/checkout.types';
 
-export default function Delivery({ isVisible, checkout, page }) {
-  const { mode: selectedMode } = useSelector((state) => state.checkout);
+export default function Delivery({
+  isVisible,
+  checkout,
+  page
+}: {
+  isVisible: boolean;
+  checkout?: CheckoutResponse;
+  page: CheckoutPageType;
+}) {
+  const { mode: selectedMode } = useSelector((state: any) => state.checkout);
   const { data: addresses = [] } = useAddressQuery();
   const { title } = page;
 
   if (!isVisible) return null;
   return (
-    <div className="col-span-2 Checkout-page">
-      <Title title={`${title}`} className="mb-8 Title--2" />
+    <div className="Checkout-page col-span-2">
+      <Title title={`${title}`} className="Title--2 mb-8" />
       <DeliveryModes />
       {selectedMode === 'delivery' ? (
         <Suspense fallback={<Loader />}>
           <AddressBook
             mode="delivery"
-            title='SELECT_DELIVERY_ADDRESS'
+            title="SELECT_DELIVERY_ADDRESS"
             addresses={addresses}
           />
         </Suspense>
       ) : null}
       {selectedMode === 'pickup' ||
-        (selectedMode === 'delivery' && checkout?.deliveryAddressId) ? (
+      (selectedMode === 'delivery' && checkout?.deliveryAddressId) ? (
         <Suspense fallback={<Loader />}>
           <DeliveryModules />
         </Suspense>
@@ -38,7 +47,6 @@ export default function Delivery({ isVisible, checkout, page }) {
           <Map addresses={addresses} />
         </div>
       ) : null}
-
     </div>
-  )
+  );
 }
