3f12eeed22fa4f3f240f9d6f1540419c936d9d89
Théo Cherblanc <tcherblanc@mbp-de-sysadmin.home>
2023-08-25 10:21:57 +0200

    convert React PickupMap component to TypeScript

--- templates/frontOffice/modern/components/React/PickupMap/PickupMap.tsx
+++ templates/frontOffice/modern/components/React/PickupMap/PickupMap.tsx
@@ -15,7 +15,7 @@ import React, {
   useMemo,
   useState
 } from 'react';
-import { orderBy, size } from 'lodash-es';
+import { orderBy, size } from 'lodash';
 import {
   useGetCheckout,
   usePickupLocations,
@@ -26,16 +26,32 @@ import Alert from '../Alert';
 import Loader from '../Loader';
 import axios from 'axios';
 import { getLatLngCenter } from '@utils/map';
+
+//@ts-ignore
 import { icon } from 'leaflet/src/layer/marker/Icon';
+
+//@ts-ignore
 import markerChronopostImg from './images/chronopost-logo.png';
+//@ts-ignore
 import markerColissimoImg from './images/colissimo-logo.png';
+//@ts-ignore
 import markerDpDImg from './images/dpd-logo.png';
+//@ts-ignore
 import markerImg from './images/marker-icon.png';
+//@ts-ignore
 import markerImgShadow from './images/marker-shadow.png';
+
 import { useForm } from 'react-hook-form';
 import { useIntl } from 'react-intl';
 import Title from '../Title';
 import { useRef } from 'react';
+import { PickupLocation } from '@js/types/common';
+import {
+  InfoPopupProps,
+  MapDisplayProps,
+  PickupMapProps,
+  ZipCodeSearcherProps
+} from './PickupMap.types';
 // import { pickupFixtures } from '../Checkout/constants';
 
 const customIcon = icon({
@@ -67,7 +83,7 @@ const WEEKDAYS = [
   'Dimanche'
 ];
 
-function findIcon(code) {
+function findIcon(code: string) {
   switch (code) {
     case 'ColissimoPickupPoint':
       return colissimoCustomIcon;
@@ -80,18 +96,20 @@ function findIcon(code) {
   }
 }
 
-function InfoPopUp({ location, selected, onChooseLocation }) {
+interface Location extends PickupLocation {
+  ref: React.RefObject<any>;
+  refList: React.RefObject<any>;
+}
+
+function InfoPopUp({ location, selected, onChooseLocation }: InfoPopupProps) {
   return (
     <Popup ref={location.ref}>
       <div className="p-2 text-center">
         <div className="font-bold">{location.title}</div>
-        {location.module?.i18n?.title ? (
-          <div className="mt-2 font-bold">{location.module?.i18n?.title}</div>
-        ) : null}
         {location.address ? (
           <div className="mt-4">
             {location.address.address1} <br />
-            {location.address.city} {location.address.zipcode}
+            {location.address.city} {location.address.zipCode}
           </div>
         ) : null}
         {location.openingHours ? (
@@ -109,7 +127,7 @@ function InfoPopUp({ location, selected, onChooseLocation }) {
         {!selected ? (
           <button
             type="button"
-            className="mt-4 Button Button--primary"
+            className="Button Button--primary mt-4"
             onClick={() => {
               onChooseLocation(location.id);
               if (location.refList?.current) {
@@ -129,24 +147,27 @@ function InfoPopUp({ location, selected, onChooseLocation }) {
   );
 }
 
-function MapDisplay({ locations, selectedLocation, onChooseLocation }) {
+function MapDisplay({
+  locations,
+  selectedLocation,
+  onChooseLocation
+}: MapDisplayProps) {
   const map = useMap();
   const groupRef = useRef(null);
 
   const mapCenter = useMemo(() => {
-    if (locations.length > 0) {
-      const markers = locations.map((point) => {
-        return {
-          latitude: parseFloat(point.latitude || 0),
-          longitude: parseFloat(point.longitude || 0)
-        };
-      });
-      const center = getLatLngCenter(markers);
+    if (!locations.length) return;
+    const markers = locations.map((point) => {
       return {
-        center: [center.latitude, center.longitude],
-        bounds: markers.map((e) => [e.latitude, e.longitude])
+        latitude: parseFloat(point.latitude),
+        longitude: parseFloat(point.longitude)
       };
-    }
+    });
+    const center = getLatLngCenter(markers);
+    return {
+      center: [center.latitude, center.longitude],
+      bounds: markers.map((e) => [e.latitude, e.longitude])
+    };
   }, [locations]);
 
   useEffect(() => {
@@ -166,6 +187,7 @@ function MapDisplay({ locations, selectedLocation, onChooseLocation }) {
     <div>
       <TileLayer
         url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+        //@ts-ignore
         subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
       />
       <FeatureGroup ref={groupRef}>
@@ -174,6 +196,7 @@ function MapDisplay({ locations, selectedLocation, onChooseLocation }) {
             <Marker
               key={location.id}
               position={[location.latitude, location.longitude]}
+              // @ts-ignore
               icon={findIcon(location?.moduleOptionCode)}
             >
               <InfoPopUp
@@ -189,12 +212,16 @@ function MapDisplay({ locations, selectedLocation, onChooseLocation }) {
   );
 }
 
-function PickupPointsList({ locations, selectedLocation, onChooseLocation }) {
+function PickupPointsList({
+  locations,
+  selectedLocation,
+  onChooseLocation
+}: MapDisplayProps) {
   const intl = useIntl();
 
   return (
     <div
-      className="overflow-y-auto bg-white divide-y divide-gray-200 rounded-l 2xl:w-5/12"
+      className="divide-y divide-gray-200 overflow-y-auto rounded-l bg-white 2xl:w-5/12"
       style={{ height: '50vh' }}
     >
       {size(locations) > 0 ? (
@@ -212,15 +239,12 @@ function PickupPointsList({ locations, selectedLocation, onChooseLocation }) {
                 onChooseLocation(location.id);
               }}
             >
-              <div className="w-3/4 pr-4 mb-4 leading-tight text-left normal-case">
+              <div className="mb-4 w-3/4 pr-4 text-left normal-case leading-tight">
                 <div className="font-bold">{location.title}</div>
-                <div className="mt-2 text-sm">
-                  {location.module?.i18n?.title}
-                </div>
                 {location.address ? (
                   <div className="mt-2 text-sm leading-tight ">
                     {location.address.address1} <br />
-                    {location.address.city} {location.address.zipcode}
+                    {location.address.city} {location.address.zipCode}
                   </div>
                 ) : null}
               </div>
@@ -236,18 +260,16 @@ function PickupPointsList({ locations, selectedLocation, onChooseLocation }) {
   );
 }
 
-export function PickupMap({ query, defaultAddressId }) {
+export function PickupMap({ query, defaultAddressId }: PickupMapProps) {
   const { data: checkout } = useGetCheckout();
   const { mutateAsync: setCheckout } = useSetCheckout();
   const intl = useIntl();
 
-  const {
-    data: pickupPoints = [],
-    error,
-    isLoading
-  } = usePickupLocations(query);
+  const { data, error, isLoading } = usePickupLocations(query);
 
-  const locations = useMemo(() => {
+  const pickupPoints: PickupLocation[] = data ?? [];
+
+  const locations: Location[] = useMemo(() => {
     if (!checkout?.deliveryModuleId) return [];
     return pickupPoints
       .filter((l) => l.moduleId === checkout?.deliveryModuleId)
@@ -256,8 +278,8 @@ export function PickupMap({ query, defaultAddressId }) {
           ...p,
           ref: createRef(),
           refList: createRef(),
-          latitude: parseFloat(p.latitude || 0),
-          longitude: parseFloat(p.longitude || 0),
+          latitude: p.latitude,
+          longitude: p.longitude,
           moduleOptionCode: checkout?.deliveryModuleOptionCode
         };
       });
@@ -268,7 +290,7 @@ export function PickupMap({ query, defaultAddressId }) {
   ]);
 
   const onSelect = useCallback(
-    async (id) => {
+    async (id: string) => {
       const location = locations.find((l) => l.id === id);
       if (location?.address) {
         try {
@@ -296,17 +318,18 @@ export function PickupMap({ query, defaultAddressId }) {
   }, [checkout, locations]);
 
   const mapCenter = useMemo(() => {
-    if (locations.length > 0) {
-      const center = getLatLngCenter(
-        locations.map((point) => {
-          return {
-            latitude: parseFloat(point.latitude || 0),
-            longitude: parseFloat(point.longitude || 0)
-          };
-        })
-      );
-      return [center.latitude, center.longitude];
-    }
+    if (!locations.length) return;
+    const markers = locations.map((point) => {
+      return {
+        latitude: point.latitude,
+        longitude: point.longitude
+      };
+    });
+    const center = getLatLngCenter(markers);
+    return {
+      center: [center.latitude, center.longitude],
+      bounds: markers.map((e) => [e.latitude, e.longitude])
+    };
   }, [locations]);
 
   if (isLoading) {
@@ -326,13 +349,14 @@ export function PickupMap({ query, defaultAddressId }) {
   return locations.length > 0 ? (
     <div className="lg:flex">
       <MapContainer
+        //@ts-ignore
         center={mapCenter}
         zoom={15}
         style={{ height: '50vh', width: '100%' }}
         className="z-0 overflow-hidden"
       >
         <div>
-          <div className="relative h-full py-8 overflow-hidden border-t border-gray-300 xl:flex">
+          <div className="relative h-full overflow-hidden border-t border-gray-300 py-8 xl:flex">
             <MapDisplay
               locations={locations}
               selectedLocation={selected}
@@ -357,14 +381,14 @@ export function PickupMap({ query, defaultAddressId }) {
   );
 }
 
-export function ZipCodeSearcher({ onSubmit }) {
+export function ZipCodeSearcher({ onSubmit }: ZipCodeSearcherProps) {
   const intl = useIntl();
   const { register, handleSubmit } = useForm();
   const [hasError, setHasError] = useState(false);
 
   return (
     <form
-      className="w-full mb-4"
+      className="mb-4 w-full"
       onSubmit={handleSubmit(async (values) => {
         try {
           setHasError(false);
@@ -377,7 +401,7 @@ export function ZipCodeSearcher({ onSubmit }) {
         }
       })}
     >
-      <Title title="FIND_RELAY" className="mb-4 text-2xl Title--3" />
+      <Title title="FIND_RELAY" className="Title--3 mb-4 text-2xl" />
 
       <div className="PhoneCheck">
         <div className="PhoneCheck-field">
@@ -406,8 +430,8 @@ export function ZipCodeSearcher({ onSubmit }) {
   );
 }
 
-export default function Map({ addresses }) {
-  const defaultAddress = addresses?.find((a) => a.isDefault === 1);
+export default function Map({ addresses }: { addresses: any }) {
+  const defaultAddress = addresses?.find((a: any) => a.isDefault === 1);
 
   const [query, setQuery] = useState({
     address: defaultAddress?.address1 || '',
