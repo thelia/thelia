661be989866ed19658cc1e466d18ab02c6427219
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2023-09-08 15:50:00 +0200

    Add module api addons

--- core/lib/Thelia/Api/Bridge/Propel/Service/ApiResourceService.php
+++ core/lib/Thelia/Api/Bridge/Propel/Service/ApiResourceService.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
- * This file is part of the Thelia package.
- * http://www.thelia.net
- *
- * (c) OpenStudio <info@thelia.net>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+namespace Thelia\Api\Bridge\Propel\Service;
 
-namespace Thelia\Api\Bridge\Propel\State;
-
-use ApiPlatform\State\ProviderInterface;
 use Propel\Runtime\ActiveRecord\ActiveRecordInterface;
 use Propel\Runtime\Collection\Collection;
 use Propel\Runtime\Collection\ObjectCollection;
@@ -20,12 +9,28 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Thelia\Api\Bridge\Propel\Attribute\Column;
 use Thelia\Api\Bridge\Propel\Attribute\CompositeIdentifiers;
 use Thelia\Api\Bridge\Propel\Attribute\Relation;
+use Thelia\Api\Resource\AbstractPropelResource;
 use Thelia\Api\Resource\PropelResourceInterface;
+use Thelia\Api\Resource\ResourceAddonInterface;
 use Thelia\Api\Resource\TranslatableResourceInterface;
 use Thelia\Model\LangQuery;
 
-abstract class AbstractPropelProvider implements ProviderInterface
+class ApiResourceService
 {
+    public function __construct(
+        private readonly array $apiResourceAddons
+    )
+    {
+    }
+
+    public function getResourceAddonDefinitions($resourceClass): array
+    {
+        return $this->apiResourceAddons[$resourceClass] ?? [];
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
     public function modelToResource(
         string $resourceClass,
         ActiveRecordInterface $propelModel,
@@ -34,7 +39,8 @@ abstract class AbstractPropelProvider implements ProviderInterface
         \ReflectionClass $parentReflector = null,
         ActiveRecordInterface $parentModel = null,
         bool $withRelation = true,
-        ActiveRecordInterface $baseModel = null
+        ActiveRecordInterface $baseModel = null,
+        bool $withAddon = true
     ): PropelResourceInterface {
 
         if ($langs === null) {
@@ -105,7 +111,8 @@ abstract class AbstractPropelProvider implements ProviderInterface
                             resourceClass: $parentReflector->getName(),
                             propelModel: $parentModel,
                             context: $context,
-                            withRelation: false
+                            withRelation: false,
+                            withAddon: $withAddon
                         )
                     );
                     continue 2;
@@ -121,7 +128,8 @@ abstract class AbstractPropelProvider implements ProviderInterface
                                 context: $context,
                                 parentReflector: $reflector,
                                 parentModel: $propelModel,
-                                baseModel: $baseModel
+                                baseModel: $baseModel,
+                                withAddon: $withAddon
                             )
                         );
                     }
@@ -134,7 +142,8 @@ abstract class AbstractPropelProvider implements ProviderInterface
                         context: $context,
                         parentReflector: $reflector,
                         parentModel: $propelModel,
-                        baseModel: $baseModel
+                        baseModel: $baseModel,
+                        withAddon: $withAddon
                     );
                 }
             }
@@ -206,8 +215,104 @@ abstract class AbstractPropelProvider implements ProviderInterface
         }
 
         $apiResource->setPropelModel($propelModel);
+
+        if ($withAddon) {
+            foreach ($this->getResourceAddonDefinitions($resourceClass) as $addonShortName => $addonClass) {
+                if (is_subclass_of($addonClass, ResourceAddonInterface::class)) {
+                    $addon = (new $addonClass())->buildFromModel($propelModel, $apiResource);
+                    $apiResource->setResourceAddon($addonShortName, $addon);
+                }
+            }
+        }
+
         $apiResource->afterModelToResource($context);
 
         return $apiResource;
+    }
+
+
+    public function resourceToModel(mixed $data)
+    {
+        $propelModel = $data->getPropelModel();
+
+        if (null === $propelModel) {
+            $propelModelClass = $data::getPropelModelClass();
+            $propelModel = new $propelModelClass();
+        }
+
+        $resourceReflection = new \ReflectionClass($data);
+        foreach ($resourceReflection->getProperties() as $property) {
+            if (!$property->isInitialized($data)) {
+                continue;
+            }
+
+            $setterForced = false;
+            $propelSetter = 'set'.ucfirst($property->getName());
+
+            foreach ($property->getAttributes(Column::class) as $columnAttribute) {
+                if (isset($columnAttribute->getArguments()['propelFieldName'])) {
+                    $propelSetter = 'set'.ucfirst($columnAttribute->getArguments()['propelFieldName']);
+                }
+                if (isset($columnAttribute->getArguments()['propelSetter'])) {
+                    $setterForced = true;
+                    $propelSetter = $columnAttribute->getArguments()['propelSetter'];
+                }
+            }
+
+
+            if (method_exists($propelModel, $propelSetter)) {
+                $possibleGetters = [
+                    'get'.ucfirst($property->getName()),
+                    'is'.ucfirst($property->getName()),
+                ];
+
+                $availableMethods = array_filter(array_intersect($possibleGetters, get_class_methods($data)));
+
+                $value = null;
+                while (!empty($availableMethods) && $value === null) {
+                    $method = array_pop($availableMethods);
+                    $value = $data->$method();
+                }
+
+                if ($value instanceof AbstractPropelResource) {
+                    if (!$setterForced) {
+                        $propelSetter = $propelSetter.'Id';
+                    }
+                    $value = $value->getPropelModel()->getId();
+                }
+
+                if (\is_array($value)) {
+                    $value = new Collection(array_map(function ($value) {return $this->resourceToModel($value); }, $value));
+                }
+
+                $propelModel->$propelSetter($value);
+            }
+        }
+
+        if (is_subclass_of($data, TranslatableResourceInterface::class)) {
+            foreach ($data->getI18ns() as $locale => $i18n) {
+                $i18nGetters = array_filter(
+                    array_map(
+                        function (\ReflectionProperty $reflectionProperty) use ($i18n) {
+                            return $reflectionProperty->isInitialized($i18n) ? 'get'.ucfirst($reflectionProperty->getName()) : null;
+                        },
+                        (new \ReflectionClass($i18n))->getProperties()
+                    )
+                );
+
+                $propelModel->setLocale($locale);
+                foreach ($i18nGetters as $i18nGetter) {
+                    if ($i18nGetter === 'getId') {
+                        continue;
+                    }
+                    $propelSetter = substr_replace($i18nGetter, 's', 0, 1);
+                    if (method_exists($propelModel, $propelSetter)) {
+                        $propelModel->$propelSetter($i18n->$i18nGetter());
+                    }
+                }
+            }
+        }
+
+        return $propelModel;
     }
 }
