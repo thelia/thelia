d0ad47cd5d2572d8917ccee5c4916cc8b110556d
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2023-09-26 15:04:54 +0200

    Fix relation validation error path + fix PSE groups

--- core/lib/Thelia/Api/Resource/Currency.php
+++ core/lib/Thelia/Api/Resource/Currency.php
@@ -50,10 +50,10 @@ class Currency extends AbstractTranslatableResource
     public const GROUP_READ_SINGLE = 'currency:read:single';
     public const GROUP_WRITE = 'currency:write';
 
-    #[Groups([self::GROUP_READ, Order::GROUP_READ, Cart::GROUP_READ_SINGLE, ProductPrice::GROUP_READ, Product::GROUP_READ_SINGLE, Product::GROUP_WRITE, ProductSaleElements::GROUP_WRITE])]
+    #[Groups([self::GROUP_READ, Order::GROUP_READ, Cart::GROUP_READ_SINGLE, ProductSaleElements::GROUP_READ_SINGLE, ProductPrice::GROUP_READ, Product::GROUP_READ_SINGLE, Product::GROUP_WRITE, ProductSaleElements::GROUP_WRITE])]
     public ?int $id = null;
 
-    #[Groups([self::GROUP_READ, self::GROUP_WRITE, Order::GROUP_READ_SINGLE, Cart::GROUP_READ_SINGLE, Product::GROUP_READ_SINGLE])]
+    #[Groups([self::GROUP_READ, self::GROUP_WRITE, Order::GROUP_READ_SINGLE, ProductSaleElements::GROUP_READ_SINGLE, Cart::GROUP_READ_SINGLE, Product::GROUP_READ_SINGLE])]
     public ?string $code;
 
     #[Groups([self::GROUP_READ, self::GROUP_WRITE, Order::GROUP_READ_SINGLE, Cart::GROUP_READ_SINGLE, Product::GROUP_READ_SINGLE])]
