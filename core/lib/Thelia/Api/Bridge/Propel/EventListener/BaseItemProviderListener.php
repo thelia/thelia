24a80d8ef5ec66b55646c67bbc7a134e51e2f492
ArthurLashermes <76913541+ArthurLashermes@users.noreply.github.com>
2023-10-09 10:29:03 +0200

    [API] Add more constraint + fix filters issues
    * adding order constraint and orderProductTax
    
    * solve filterBy propel problem
    
    * fix pse post issue 2
    
    * fix delete issue 1
    
    * adding get for status by code, issue 3
    
    * better way to filter
    
    * delete useless isset
    
    * improvement filter listener
    
    ---------
    
    Co-authored-by: ArthurLashermes <alashermes@openstudio.fr>

--- core/lib/Thelia/Api/Bridge/Propel/EventListener/BaseItemProviderListener.php
+++ core/lib/Thelia/Api/Bridge/Propel/EventListener/BaseItemProviderListener.php
@@ -4,22 +4,49 @@ namespace Thelia\Api\Bridge\Propel\EventListener;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Api\Bridge\Propel\Event\ItemProviderEvent;
+use Thelia\Api\Bridge\Propel\Service\ApiResourceService;
 
 class BaseItemProviderListener implements EventSubscriberInterface
 {
+    public function __construct(
+        private readonly ApiResourceService $apiResourceService,
+    ) {
+    }
     public function baseProvider(ItemProviderEvent $event)
     {
         $query = $event->getQuery();
 
+        $reflector = new \ReflectionClass($event->getResourceClass());
+
+        $compositeIdentifiers = $this->apiResourceService->getResourceCompositeIdentifierValues(reflector: $reflector, param: 'keys');
+
+        $columnValues = $this->apiResourceService->getColumnValues(reflector: $reflector,columns: $compositeIdentifiers);
+
         foreach ($event->getUriVariables() as $field => $value) {
+            $filterMethod = null;
+            $filterName = $columnValues[$field]["propelQueryFilter"] ?? null;
+            if ($filterName && method_exists($query, $filterName)){
+                $filterMethod = $columnValues[$field]["propelQueryFilter"];
+                $value = $event->getUriVariables()[$field];
+            }
+
+            $filterName = "filterBy".ucfirst($field)."Id";
+            if (null === $filterMethod && method_exists($query, $filterName)) {
+                $filterMethod = $filterName;
+            }
+
             $filterName = "filterBy".ucfirst($field);
-            if (method_exists($query, $filterName)) {
-                $query->$filterName($value);
+            if (null === $filterMethod && method_exists($query, $filterName)) {
+                $filterMethod = $filterName;
+            }
+
+            if ($filterMethod !== null){
+                $query->$filterMethod($value);
             }
         }
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             ItemProviderEvent::class => [
