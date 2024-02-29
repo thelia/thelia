d9c1ceb34b067bf0faf67363310c3017ac17ce3e
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2024-02-29 11:52:37 +0100

    Allow to modify pse from psesByProduct plugin by event

--- local/modules/TheliaSmarty/Template/Plugins/FrontUtils.php
+++ local/modules/TheliaSmarty/Template/Plugins/FrontUtils.php
@@ -13,6 +13,7 @@
 namespace TheliaSmarty\Template\Plugins;
 
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\Security\SecurityContext;
 use Thelia\Model\CategoryQuery;
@@ -21,6 +22,7 @@ use Thelia\Model\ProductPriceQuery;
 use Thelia\Model\ProductSaleElementsQuery;
 use Thelia\TaxEngine\TaxEngine;
 use Thelia\Tools\URL;
+use TheliaSmarty\Events\PseByProductEvent;
 use TheliaSmarty\Template\AbstractSmartyPlugin;
 use TheliaSmarty\Template\SmartyPluginDescriptor;
 
@@ -42,14 +44,19 @@ class FrontUtils extends AbstractSmartyPlugin
     /** @var string */
     protected $assetsPublicPath;
 
+    /** @var EventDispatcherInterface */
+    protected $eventDispatcher;
+
     public function __construct(
         RequestStack $requestStack,
         TaxEngine $taxEngine,
         SecurityContext $securityContext,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->request = $requestStack->getCurrentRequest();
         $this->taxEngine = $taxEngine;
         $this->securityContext = $securityContext;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function getPluginDescriptors()
@@ -150,6 +157,8 @@ class FrontUtils extends AbstractSmartyPlugin
             foreach ($pse->getAttributeCombinations() as $attribute) {
                 $attributes[$attribute->getAttributeId()] = $attribute->getAttributeAvId();
             }
+
+            $this->eventDispatcher->dispatch(new PseByProductEvent($pse));
 
             $result[] = [
                 'id' => $pse->getId(),
