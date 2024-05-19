48df3da9b65d00a7191bd11703753c835fc0316a
Alexandre Nozière <anoziere@openstudio.fr>
2024-05-19 11:20:07 +0200

    feat: can call api platform GET routes from twig

--- core/lib/Thelia/Core/Service/DataAccessService.php
+++ core/lib/Thelia/Core/Service/DataAccessService.php
@@ -12,29 +12,89 @@
 
 namespace Thelia\Core\Service;
 
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\Security\SecurityContext;
 use Thelia\Core\Template\Element\BaseLoop;
 
-class DataAccessService
+readonly class DataAccessService
 {
     public function __construct(
-        private readonly ParameterBagInterface $parameterBag,
-        private readonly ContainerInterface $container,
-        private readonly EventDispatcherInterface $eventDispatcher,
-        private readonly SecurityContext $securityContext,
-        private readonly TranslatorInterface $translator,
-        private readonly RequestStack $requestStack,
+        private ParameterBagInterface $parameterBag,
+        private ContainerInterface $container,
+        private EventDispatcherInterface $eventDispatcher,
+        private SecurityContext $securityContext,
+        private TranslatorInterface $translator,
+        private RequestStack $requestStack,
+        private Security $security,
+        private JWTTokenManagerInterface $jwtManager,
+        private HttpKernelInterface $httpKernel,
+        private CacheInterface $cache,
     ) {
     }
 
-    public function datas(string $path, array $params = [], $locale = null, $cache = true): mixed
+    /**
+     * @throws \JsonException
+     * @throws InvalidArgumentException
+     */
+    public function resources(string $path, array $params = [], bool $useCache = true): array
     {
-        return ['test' => 'success'];
+        $cacheKey = md5($path.http_build_query($params));
+
+        /**
+         * @throws \JsonException
+         * @throws \Exception
+         */
+        $fetchData = function () use ($path, $params) {
+            $queryString = http_build_query($params);
+            $fullUrl = $path.'?'.$queryString;
+
+            $session = $this->requestStack->getSession();
+            $request = Request::create($fullUrl);
+            $request->setSession($session);
+            $request->headers->set('Accept', 'application/json');
+
+            $user = $this->security->getUser();
+            if ($user) {
+                $jwtToken = $this->jwtManager->create($user);
+                $request->headers->set('Authorization', 'Bearer '.$jwtToken);
+            }
+
+            $response = $this->httpKernel->handle($request, HttpKernelInterface::SUB_REQUEST);
+            if ($response->isSuccessful()) {
+                $datas = json_decode($response->getContent(), true, 512, \JSON_THROW_ON_ERROR);
+
+                return $this->extractHydraMembers($datas);
+            }
+
+            throw new NotFoundHttpException('API route not found or error occurred');
+        };
+
+        if ($useCache) {
+            return $this->cache->get($cacheKey, function (ItemInterface $item) use ($fetchData) {
+                $item->expiresAfter(3600);
+
+                return $fetchData();
+            });
+        }
+
+        return $fetchData();
+    }
+
+    private function extractHydraMembers(array $data): array
+    {
+        return $data['hydra:member'] ?? [];
     }
 
     /** @deprecated use new data access layer */
