50eae74b819e9b2d5e01fdeb56c1caa3d6b16262
Pierre Baguet <pbaguet@openstudio.fr>
2024-06-18 17:16:18 +0200

    remove useless type argument

--- templates/frontOffice/modern/components/React/Checkout/hooks/useValidPaymentModules.ts
+++ templates/frontOffice/modern/components/React/Checkout/hooks/useValidPaymentModules.ts
@@ -1,7 +1,7 @@
 import { DeliveryModule, PaymentModule } from '@js/types/common';
 import { usePaymentModulessQuery } from '@openstudio/thelia-api-utils';
 
-export default function useValidPaymentModules(type?: any) {
+export default function useValidPaymentModules() {
   const { data = [], isLoading } = usePaymentModulessQuery();
 
   const validModules = (data as PaymentModule[]).filter((m) => m.valid);
