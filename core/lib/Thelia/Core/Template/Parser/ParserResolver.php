6f9df3bb9bb59fec6aed5c857d76570da1069e10
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2024-06-25 09:54:07 +0200

    Feat/twig integration (#3223)
    * feat: add twig extension for attributes, better management of template path, twig as module
    
    * feat: manage form, loop count, hooks, url + refacto assetsResolver

--- core/lib/Thelia/Core/Template/Parser/ParserResolver.php
+++ core/lib/Thelia/Core/Template/Parser/ParserResolver.php
@@ -14,28 +14,35 @@ namespace Thelia\Core\Template\Parser;
 
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
+use Thelia\Core\Template\Assets\AssetResolverInterface;
 use Thelia\Core\Template\ParserInterface;
 
 #[AsAlias(id: 'thelia.parser.resolver', public: true)]
 class ParserResolver
 {
+    private static ?ParserInterface $currentParser = null;
+
     public function __construct(
         #[TaggedIterator('thelia.parser.template', exclude: [ParserFallback::class])]
-        private readonly iterable $parsers
+        private readonly iterable $parsers,
+        #[TaggedIterator('thelia.parser.asset', exclude: [ParserAssetResolverFallback::class])]
+        private readonly iterable $assetResolvers
     ) {
     }
 
     /**
      * @throws \Exception
      */
-    public function getParser(?string $templateName): ParserInterface
+    public function getParser(string $pathTemplate, ?string $templateName): ParserInterface
     {
         if ('' === (string) $templateName) {
             $templateName = 'index';
         }
         /** @var ParserInterface $parser */
         foreach ($this->parsers as $parser) {
-            if ($parser->supportTemplateRender($templateName)) {
+            if ($parser->supportTemplateRender($pathTemplate, $templateName)) {
+                self::$currentParser = $parser;
+
                 return $parser;
             }
         }
@@ -43,8 +50,30 @@ class ParserResolver
         throw new \Exception(sprintf('Parser %s not found', $templateName));
     }
 
+    /**
+     * @throws \Exception
+     */
+    public function getAssetResolver(ParserInterface $parser): AssetResolverInterface
+    {
+        if (null === self::$currentParser) {
+            throw new \Exception('Parser not found');
+        }
+        /* @var AssetResolverInterface $parserAsset */
+        foreach ($this->assetResolvers as $assetResolvers) {
+            if ($assetResolvers->supportParser($parser)) {
+                return $assetResolvers;
+            }
+        }
+        throw new \Exception('Assets parser not found');
+    }
+
     public function getParsers(): iterable
     {
         return $this->parsers;
+    }
+
+    public static function getCurrentParser(): ?ParserInterface
+    {
+        return self::$currentParser;
     }
 }
---
+++ local/modules/TheliaTwig/Extension/FormExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TheliaTwig\Extension;
+
+use Symfony\Component\Form\FormView;
+use TheliaTwig\Service\FormService;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class FormExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly FormService $formService
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('getForm', [$this, 'getForm'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function getForm(string $name, array $data = []): FormView
+    {
+        return $this->formService->getFormByName($name, $data);
+    }
+}
