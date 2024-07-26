c08cd9095caf294636e61688533ce09a9eccf356
lnormandon <lnormandon@openstudio.fr>
2024-07-26 10:19:03 +0200

    Add SVG Extension + path in twig.yaml + WebpackEncoreBundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
