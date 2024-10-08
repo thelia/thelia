bd5c571815a68ce4fdcc22c8535a8fec05122742
ArthurLashermes <76913541+ArthurLashermes@users.noreply.github.com>
2024-10-08 11:56:57 +0200

    Fix HTTP return codes not working in production environment (#3250)
    Co-authored-by: ArthurLashermes <alashermes@openstudio.fr>

--- core/lib/Thelia/Core/EventListener/ErrorListener.php
+++ core/lib/Thelia/Core/EventListener/ErrorListener.php
@@ -82,6 +82,9 @@ class ErrorListener implements EventSubscriberInterface
 
     public function handleException(ExceptionEvent $event): void
     {
+        if($event->getRequest()->get('_api_operation_name',false)){
+            return;
+        }
         if ('prod' === $this->env && ConfigQuery::isShowingErrorMessage()) {
             $this->eventDispatcher
                 ->dispatch(
