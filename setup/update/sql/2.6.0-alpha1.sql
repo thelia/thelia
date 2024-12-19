1c416d386b2f37c18a42e3f4797c562165d6b47d
Alexandre <anoziere@openstudio.fr>
2024-12-19 20:01:11 +0100

    feat: composer update + end clean dependencies

--- setup/update/sql/2.6.0-alpha1.sql
+++ setup/update/sql/2.6.0-alpha1.sql
@@ -89,17 +89,18 @@ CREATE TABLE `choice_filter_other_i18n`
 --
 
 INSERT INTO `choice_filter_other` (`id`, `type`, `visible`) VALUES
-                                                                (2, 'brand', 1),
-                                                                (3, 'category', 1);
+(2, 'brand', 1),
+(3, 'category', 1);
 
 --
 -- Contenu de la table `choice_filter_other_i18n`
 --
 
 INSERT INTO `choice_filter_other_i18n` (`id`, `locale`, `title`, `description`) VALUES
-                                                                                    (2, 'en_US', 'Brand', NULL),
-                                                                                    (2, 'fr_FR', 'Marque', NULL),
-                                                                                    (3, 'en_US', 'Category', NULL),
-                                                                                    (3, 'fr_FR', 'Catégorie', NULL);
+(2, 'en_US', 'Brand', NULL),
+(2, 'fr_FR', 'Marque', NULL),
+(3, 'en_US', 'Category', NULL),
+(3, 'fr_FR', 'Catégorie', NULL);
+
 
 SET FOREIGN_KEY_CHECKS = 1;
