d23112d966ff20a3182ca3d3d88b699ef3a8709b
Alexandre <anoziere@openstudio.fr>
2024-12-19 18:18:58 +0100

    fix: update conf

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of the Thelia package.
- * http://www.thelia.net
- *
- * (c) OpenStudio <info@thelia.net>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    require dirname(__DIR__).'/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
