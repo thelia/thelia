36eee63cde9165e4f32ff5e9bc28b2188dba0c98
Alexandre <anoziere@openstudio.fr>
2025-01-02 12:18:56 +0100

    fix: pseImage routes

--- core/lib/Thelia/Api/Resource/ProductSaleElementsProductImage.php
+++ core/lib/Thelia/Api/Resource/ProductSaleElementsProductImage.php
@@ -13,7 +13,6 @@
 namespace Thelia\Api\Resource;
 
 use ApiPlatform\Metadata\ApiFilter;
-use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
@@ -21,23 +20,16 @@ use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
-use ApiPlatform\OpenApi\Model\Operation;
 use Propel\Runtime\Map\TableMap;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Validator\Constraints as Assert;
-use Thelia\Api\Bridge\Propel\Attribute\Relation;
 use Thelia\Api\Bridge\Propel\Filter\OrderFilter;
-use Thelia\Api\Controller\Admin\BinaryFileController;
-use Thelia\Api\Controller\Admin\PostItemFileController;
+use Thelia\Api\Bridge\Propel\Filter\SearchFilter;
 use Thelia\Model\Map\ProductSaleElementsProductImageTableMap;
 
 #[ApiResource(
     operations: [
         new Post(
             uriTemplate: '/admin/product_sale_elements_product_image',
-            inputFormats: ['multipart' => ['multipart/form-data']],
-            controller: PostItemFileController::class,
             normalizationContext: ['groups' => [self::GROUP_ADMIN_READ, self::GROUP_ADMIN_READ_SINGLE]],
             denormalizationContext: ['groups' => [self::GROUP_ADMIN_WRITE, self::GROUP_ADMIN_WRITE_FILE]],
             deserialize: false
@@ -48,17 +40,6 @@ use Thelia\Model\Map\ProductSaleElementsProductImageTableMap;
         new Get(
             uriTemplate: '/admin/product_sale_elements_product_image/{id}',
             normalizationContext: ['groups' => [self::GROUP_ADMIN_READ, self::GROUP_ADMIN_READ_SINGLE]]
-        ),
-        new Get(
-            uriTemplate: '/admin/product_sale_elements_product_image/{id}/file',
-            controller: BinaryFileController::class,
-            openapi: new Operation(
-                responses: [
-                    '200' => [
-                        'description' => 'The binary file',
-                    ],
-                ]
-            )
         ),
         new Put(
             uriTemplate: '/admin/product_sale_elements_product_image/{id}',
@@ -84,27 +65,23 @@ use Thelia\Model\Map\ProductSaleElementsProductImageTableMap;
             uriTemplate: '/front/product_sale_elements_product_image/{id}',
             normalizationContext: ['groups' => [self::GROUP_FRONT_READ, self::GROUP_FRONT_READ_SINGLE]]
         ),
-        new Get(
-            uriTemplate: '/front/product_sale_elements_product_image/{id}/file',
-            controller: BinaryFileController::class,
-            openapi: new Operation(
-                responses: [
-                    '200' => [
-                        'description' => 'The binary file',
-                    ],
-                ]
-            )
-        ),
     ],
     normalizationContext: ['groups' => [self::GROUP_FRONT_READ]],
 )]
 #[ApiFilter(
     filterClass: OrderFilter::class,
     properties: [
-        'position',
+        'id',
     ]
 )]
-class ProductSaleElementsProductImage extends AbstractTranslatableResource implements ItemFileResourceInterface
+#[ApiFilter(
+    filterClass: SearchFilter::class,
+    properties: [
+        'productSaleElementsId' => 'exact',
+        'productImageId' => 'exact',
+    ]
+)]
+class ProductSaleElementsProductImage implements PropelResourceInterface
 {
     use PropelResourceTrait;
 
@@ -121,50 +98,11 @@ class ProductSaleElementsProductImage extends AbstractTranslatableResource imple
     #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ])]
     public ?int $id = null;
 
-    #[Relation(targetResource: ProductSaleElements::class)]
-    #[Groups([self::GROUP_ADMIN_WRITE_FILE, self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ])]
-    public ProductSaleElements $productSaleElements;
-
-    #[Groups([self::GROUP_ADMIN_WRITE_FILE])]
-    #[ApiProperty(
-        openapiContext: [
-            'type' => 'string',
-            'format' => 'binary',
-        ]
-    )]
-    #[Assert\Image(
-        mimeTypes: [
-            'image/bmp',
-            'image/gif',
-            'image/jpeg',
-            'image/png',
-            'image/vnd.wap.wbmp',
-            'image/webp',
-            'image/xbm',
-        ],
-    )]
-    public UploadedFile $fileToUpload;
-
-    #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ, self::GROUP_ADMIN_WRITE])]
-    public bool $visible;
+    #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ, self::GROUP_ADMIN_WRITE_UPDATE])]
+    public ?int $productSaleElementsId;
 
     #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ, self::GROUP_ADMIN_WRITE_UPDATE])]
-    public ?int $position;
-
-    #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ])]
-    public ?\DateTime $createdAt;
-
-    #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ])]
-    public ?\DateTime $updatedAt;
-
-    #[Groups([self::GROUP_ADMIN_READ, self::GROUP_FRONT_READ, self::GROUP_ADMIN_WRITE])]
-    public I18nCollection $i18ns;
-
-    #[Groups([self::GROUP_ADMIN_READ_SINGLE, self::GROUP_FRONT_READ_SINGLE])]
-    public string $file;
-
-    #[Groups([self::GROUP_ADMIN_READ_SINGLE, self::GROUP_FRONT_READ_SINGLE])]
-    public ?string $fileUrl;
+    public ?int $productImageId;
 
     public function getId(): ?int
     {
@@ -178,98 +116,26 @@ class ProductSaleElementsProductImage extends AbstractTranslatableResource imple
         return $this;
     }
 
-    public function getProductSaleElements(): ProductSaleElements
+    public function getProductSaleElementsId(): ?int
     {
-        return $this->productSaleElements;
+        return $this->productSaleElementsId;
     }
 
-    public function setProductSaleElements(ProductSaleElements $productSaleElements): self
+    public function setProductSaleElementsId(?int $productSaleElementsId): static
     {
-        $this->productSaleElements = $productSaleElements;
+        $this->productSaleElementsId = $productSaleElementsId;
 
         return $this;
     }
 
-    public function getFileToUpload(): UploadedFile
+    public function getProductImageId(): ?int
     {
-        return $this->fileToUpload;
+        return $this->productImageId;
     }
 
-    public function setFileToUpload(UploadedFile $fileToUpload): self
+    public function setProductImageId(?int $productImageId): static
     {
-        $this->fileToUpload = $fileToUpload;
-
-        return $this;
-    }
-
-    public function isVisible(): bool
-    {
-        return $this->visible;
-    }
-
-    public function setVisible(bool $visible): self
-    {
-        $this->visible = $visible;
-
-        return $this;
-    }
-
-    public function getPosition(): ?int
-    {
-        return $this->position;
-    }
-
-    public function setPosition(?int $position): self
-    {
-        $this->position = $position;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTime
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(?\DateTime $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTime
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTime $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    public function getFileUrl(): ?string
-    {
-        return $this->fileUrl;
-    }
-
-    public function setFileUrl(?string $fileUrl): self
-    {
-        $this->fileUrl = $fileUrl;
-
-        return $this;
-    }
-
-    public function getFile(): string
-    {
-        return $this->file;
-    }
-
-    public function setFile(string $file): self
-    {
-        $this->file = $file;
+        $this->productImageId = $productImageId;
 
         return $this;
     }
@@ -277,25 +143,5 @@ class ProductSaleElementsProductImage extends AbstractTranslatableResource imple
     public static function getPropelRelatedTableMap(): ?TableMap
     {
         return new ProductSaleElementsProductImageTableMap();
-    }
-
-    public static function getItemType(): string
-    {
-        return 'product';
-    }
-
-    public static function getFileType(): string
-    {
-        return 'image';
-    }
-
-    public function getItemId(): string
-    {
-        return $this->getProduct()->getId();
-    }
-
-    public static function getI18nResourceClass(): string
-    {
-        return ProductSaleElementsProductImageI18n::class;
     }
 }
