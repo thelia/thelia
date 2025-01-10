2ae64232e3c7b58138c0d1cf4235d5c3c103f2bc
Lucanis <damien.foulhoux@gmail.com>
2025-01-10 15:41:08 +0100

    add correct field type for contact form message

--- core/lib/Thelia/Form/ContactForm.php
+++ core/lib/Thelia/Form/ContactForm.php
@@ -13,6 +13,7 @@
 namespace Thelia\Form;
 
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -75,7 +76,7 @@ class ContactForm extends FirewallForm
                     'for' => 'subject_contact',
                 ],
             ])
-            ->add('message', TextType::class, [
+            ->add('message', TextareaType::class, [
                 'constraints' => [
                     new NotBlank(),
                 ],
