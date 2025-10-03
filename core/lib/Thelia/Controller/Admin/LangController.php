50d227b601bb82786335f5d1f922151eb4bf6a6e
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-10-03 09:58:44 +0200

    Remove token checks in LangController methods
    Removed token validation checks from LangController.

--- core/lib/Thelia/Controller/Admin/LangController.php
+++ core/lib/Thelia/Controller/Admin/LangController.php
@@ -268,9 +268,6 @@ class LangController extends BaseAdminController
         $error_msg = false;
 
         try {
-            $this->getTokenProvider()->checkToken(
-                $this->getRequest()->query->get('_token')
-            );
 
             $deleteEvent = new LangDeleteEvent($this->getRequest()->get('language_id', 0));
 
@@ -416,10 +413,6 @@ class LangController extends BaseAdminController
         }
 
         $errorMessage = null;
-
-        $this->getTokenProvider()->checkToken(
-            $this->getRequest()->query->get('_token')
-        );
 
         try {
             $eventDispatcher->dispatch($event, $eventName);
