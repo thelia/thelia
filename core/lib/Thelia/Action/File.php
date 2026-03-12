b14a8998d3e281dc1d0549b2d04c19d3031e511e
Thomas Da Silva <tdasilva@openstudio.fr>
2026-03-12 13:20:06 +0100

    Fix clone product

--- core/lib/Thelia/Action/File.php
+++ core/lib/Thelia/Action/File.php
@@ -20,6 +20,7 @@ use Thelia\Core\Event\Product\ProductCloneEvent;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\Translation\Translator;
 use Thelia\Log\Tlog;
+use Thelia\Model\LangQuery;
 use Thelia\Model\ProductDocument;
 use Thelia\Model\ProductDocumentI18n;
 use Thelia\Model\ProductDocumentI18nQuery;
@@ -37,47 +38,103 @@ class File extends BaseAction implements EventSubscriberInterface
 {
     public function cloneFile(ProductCloneEvent $event, $eventName, EventDispatcherInterface $dispatcher): void
     {
-        $originalProductId = $event->getOriginalProduct()->getId();
-        $clonedProduct = $event->getClonedProduct();
-
         foreach ($event->getTypes() as $type) {
             if (!\in_array($type, ['images', 'documents'])) {
                 throw new \Exception(Translator::getInstance()->trans('Cloning files of type %type is not allowed.', ['%type' => $type], 'core'));
             }
 
-            $originalProductFiles = [];
-
             switch ($type) {
                 case 'images':
-                    $originalProductFiles = ProductImageQuery::create()
-                        ->findByProductId($originalProductId);
+                    $this->cloneImage($event, $eventName, $dispatcher);
                     break;
                 case 'documents':
-                    $originalProductFiles = ProductDocumentQuery::create()
-                        ->findByProductId($originalProductId);
-                    break;
+                    $this->cloneDocument($event, $eventName, $dispatcher);
             }
+        }
+    }
 
-            // Set clone's files
-            /** @var ProductDocument|ProductImage $originalProductFile */
-            foreach ($originalProductFiles as $originalProductFile) {
-                $srcPath = $originalProductFile->getUploadDir().DS.$originalProductFile->getFile();
+    public function cloneDocument(ProductCloneEvent $event, $eventName, EventDispatcherInterface $dispatcher): void
+    {
+        $originalProductId = $event->getOriginalProduct()->getId();
+        $clonedProduct = $event->getClonedProduct();
+        $originalProductFiles = ProductDocumentQuery::create()->findByProductId($originalProductId);
+
+        // Set clone's files
+        /** @var ProductDocument $originalProductFile */
+        foreach ($originalProductFiles as $originalProductFile) {
+            $srcPath = $originalProductFile->getUploadDir().DS.$originalProductFile->getFile();
+
+            if (file_exists($srcPath)) {
+                $ext = pathinfo($srcPath, \PATHINFO_EXTENSION);
+
+                $fileName = pathinfo($originalProductFile->getFile(), \PATHINFO_FILENAME).'-'.$clonedProduct->getRef().'.'.$ext;
+                $clonedProductFile = new ProductDocument();
+
+                // Copy a temporary file of the source file as it will be deleted by IMAGE_SAVE or DOCUMENT_SAVE event
+                $srcTmp = $srcPath.'.tmp';
+                copy($srcPath, $srcTmp);
+
+                // Get file mimeType
+                $finfo = new \finfo();
+                $fileMimeType = $finfo->file($srcPath, \FILEINFO_MIME_TYPE);
+
+                // Get file event's parameters
+                $clonedProductFile
+                    ->setProductId($clonedProduct->getId())
+                    ->setVisible($originalProductFile->getVisible())
+                    ->setPosition($originalProductFile->getPosition())
+                    ->setLocale($clonedProduct->getLocale())
+                    ->setTitle($clonedProduct->getTitle());
+
+                $clonedProductCopiedFile = new UploadedFile($srcPath, $fileName, $fileMimeType);
+
+                // Create and dispatch event
+                $clonedProductCreateFileEvent = new FileCreateOrUpdateEvent($clonedProduct->getId());
+                $clonedProductCreateFileEvent
+                    ->setModel($clonedProductFile)
+                    ->setUploadedFile($clonedProductCopiedFile)
+                    ->setParentName($clonedProduct->getTitle());
+
+                $dispatcher->dispatch($clonedProductCreateFileEvent, TheliaEvents::DOCUMENT_SAVE);
+
+                // Get original product document I18n
+                $originalProductFileI18ns = ProductDocumentI18nQuery::create()
+                    ->findById($originalProductFile->getId());
+
+                // Set a temporary source file as the original one
+                rename($srcTmp, $srcPath);
+
+                // Clone file's I18n
+                $this->cloneDocumentI18n($originalProductFileI18ns, $clonedProductFile, $event, $dispatcher);
+            } else {
+                Tlog::getInstance()->addWarning("Failed to find media file $srcPath");
+            }
+        }
+    }
+
+    public function cloneImage(ProductCloneEvent $event, $eventName, EventDispatcherInterface $dispatcher): void
+    {
+        $originalProductId = $event->getOriginalProduct()->getId();
+        $clonedProduct = $event->getClonedProduct();
+        $originalProductFiles = ProductImageQuery::create()->findByProductId($originalProductId);
+
+        // Set clone's files
+        /** @var ProductImage $originalProductFile */
+        foreach ($originalProductFiles as $originalProductFile) {
+            $clonedProductFile = new ProductImage();
+
+            $clonedProductFile
+                ->setProductId($clonedProduct->getId())
+                ->setVisible($originalProductFile->getVisible())
+                ->setPosition($originalProductFile->getPosition());
+
+            foreach (LangQuery::create()->findByActive(1) as $lang) {
+                $locale = $lang->getLocale();
+                $srcPath = $originalProductFile->getUploadDir().DS.$originalProductFile->setLocale($locale)->getFile();
 
                 if (file_exists($srcPath)) {
                     $ext = pathinfo($srcPath, \PATHINFO_EXTENSION);
-
-                    $clonedProductFile = [];
-                    $fileName = '';
-                    switch ($type) {
-                        case 'images':
-                            $fileName = $clonedProduct->getRef().'.'.$ext;
-                            $clonedProductFile = new ProductImage();
-                            break;
-                        case 'documents':
-                            $fileName = pathinfo($originalProductFile->getFile(), \PATHINFO_FILENAME).'-'.$clonedProduct->getRef().'.'.$ext;
-                            $clonedProductFile = new ProductDocument();
-                            break;
-                    }
+                    $fileName = $clonedProduct->getRef().'_'.$lang->getCode().'.'.$ext;
 
                     // Copy a temporary file of the source file as it will be deleted by IMAGE_SAVE or DOCUMENT_SAVE event
                     $srcTmp = $srcPath.'.tmp';
@@ -87,47 +144,31 @@ class File extends BaseAction implements EventSubscriberInterface
                     $finfo = new \finfo();
                     $fileMimeType = $finfo->file($srcPath, \FILEINFO_MIME_TYPE);
 
-                    // Get file event's parameters
+                    // Get original product image I18n
+                    $originalProductFileI18n = ProductImageI18nQuery::create()
+                        ->filterByLocale($locale)
+                        ->findOneById($originalProductFile->getId());
+
+                    // Clone file's I18n
                     $clonedProductFile
-                        ->setProductId($clonedProduct->getId())
-                        ->setVisible($originalProductFile->getVisible())
-                        ->setPosition($originalProductFile->getPosition())
-                        ->setLocale($clonedProduct->getLocale())
-                        ->setTitle($clonedProduct->getTitle());
+                        ->setLocale($originalProductFileI18n->getLocale())
+                        ->setTitle($originalProductFileI18n->getTitle())
+                        ->setDescription($originalProductFileI18n->getDescription())
+                        ->setChapo($originalProductFileI18n->getChapo())
+                        ->setPostscriptum($originalProductFileI18n->getPostscriptum());
 
                     $clonedProductCopiedFile = new UploadedFile($srcPath, $fileName, $fileMimeType);
 
-                    // Create and dispatch event
                     $clonedProductCreateFileEvent = new FileCreateOrUpdateEvent($clonedProduct->getId());
                     $clonedProductCreateFileEvent
                         ->setModel($clonedProductFile)
                         ->setUploadedFile($clonedProductCopiedFile)
-                        ->setParentName($clonedProduct->getTitle());
+                        ->setParentName($clonedProductFile->getTitle());
 
-                    $originalProductFileI18ns = [];
+                    $dispatcher->dispatch($clonedProductCreateFileEvent, TheliaEvents::IMAGE_SAVE);
 
-                    switch ($type) {
-                        case 'images':
-                            $dispatcher->dispatch($clonedProductCreateFileEvent, TheliaEvents::IMAGE_SAVE);
-
-                            // Get original product image I18n
-                            $originalProductFileI18ns = ProductImageI18nQuery::create()
-                                ->findById($originalProductFile->getId());
-                            break;
-                        case 'documents':
-                            $dispatcher->dispatch($clonedProductCreateFileEvent, TheliaEvents::DOCUMENT_SAVE);
-
-                            // Get original product document I18n
-                            $originalProductFileI18ns = ProductDocumentI18nQuery::create()
-                                ->findById($originalProductFile->getId());
-                            break;
-                    }
-
-                    // Set temporary source file as original one
+                    // Set a temporary source file as the original one
                     rename($srcTmp, $srcPath);
-
-                    // Clone file's I18n
-                    $this->cloneFileI18n($originalProductFileI18ns, $clonedProductFile, $type, $event, $dispatcher);
                 } else {
                     Tlog::getInstance()->addWarning("Failed to find media file $srcPath");
                 }
@@ -135,12 +176,12 @@ class File extends BaseAction implements EventSubscriberInterface
         }
     }
 
-    public function cloneFileI18n($originalProductFileI18ns, $clonedProductFile, $type, ProductCloneEvent $event, EventDispatcherInterface $dispatcher): void
+    public function cloneDocumentI18n($originalProductFileI18ns, $clonedProductFile, ProductCloneEvent $event, EventDispatcherInterface $dispatcher): void
     {
         // Set clone files I18n
         /** @var ProductDocumentI18n $originalProductFileI18n */
         foreach ($originalProductFileI18ns as $originalProductFileI18n) {
-            // Update file with current I18n info. Update or create I18n according to existing or absent Locale in DB
+            // Update the file with current I18n info. Update or create I18n according to existing or absent Locale in DB
             $clonedProductFile
                 ->setLocale($originalProductFileI18n->getLocale())
                 ->setTitle($originalProductFileI18n->getTitle())
@@ -152,14 +193,7 @@ class File extends BaseAction implements EventSubscriberInterface
             $clonedProductUpdateFileEvent = new FileCreateOrUpdateEvent($event->getClonedProduct()->getId());
             $clonedProductUpdateFileEvent->setModel($clonedProductFile);
 
-            switch ($type) {
-                case 'images':
-                    $dispatcher->dispatch($clonedProductUpdateFileEvent, TheliaEvents::IMAGE_UPDATE);
-                    break;
-                case 'documents':
-                    $dispatcher->dispatch($clonedProductUpdateFileEvent, TheliaEvents::DOCUMENT_UPDATE);
-                    break;
-            }
+            $dispatcher->dispatch($clonedProductUpdateFileEvent, TheliaEvents::DOCUMENT_UPDATE);
         }
     }
 
