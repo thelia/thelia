74836122d2d9abcb8197c3d682a13ba242ae1094
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2026-08-10 10:47:09 +0200

    chore: prepare Thelia 2.6.1 (#3447)
    * fix: backport Front and Tinymce 2.6.1 split-repo fixes
    
    * chore: prepare Thelia 2.6.1
    
    * fix: allow composer/composer ~2.10.0, all 2.9.x affected by security advisories
    
    * docs: add composer constraint fix to 2.6.1 changelog
    
    * style: fix cs violations in backported SitemapController

--- local/modules/Front/Controller/SitemapController.php
+++ local/modules/Front/Controller/SitemapController.php
@@ -12,9 +12,9 @@
 
 namespace Front\Controller;
 
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Thelia\Controller\Front\BaseFrontController;
-use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\HttpFoundation\Response;
 use Thelia\Model\ConfigQuery;
 use Thelia\Model\LangQuery;
@@ -47,11 +47,11 @@ class SitemapController extends BaseFrontController
     public const SITEMAP_CACHE_KEY = 'sitemap';
 
     /**
-     * @return Response
+     * @throws InvalidArgumentException
+     * @throws \SmartyException
      */
-    public function generateAction()
+    public function generateAction(): Response
     {
-        /** @var Request $request */
         $request = $this->getRequest();
 
         // the locale : fr, en,
@@ -59,6 +59,16 @@ class SitemapController extends BaseFrontController
         if ('' !== $lang) {
             if (!$this->checkLang($lang)) {
                 $this->pageNotFound();
+            }
+        } elseif (ConfigQuery::isMultiDomainActivated()) {
+            $currentHost = rtrim($request->getSchemeAndHttpHost(), '/');
+            $langs = LangQuery::create()->filterByActive(true)->find();
+            foreach ($langs as $langModel) {
+                $langUrl = rtrim($langModel->getUrl(), '/');
+                if (!empty($langUrl) && $currentHost === $langUrl) {
+                    $lang = $langModel->getCode();
+                    break;
+                }
             }
         }
 
@@ -129,7 +139,7 @@ class SitemapController extends BaseFrontController
     /**
      * Check if a lang is used.
      *
-     * @param $lang The lang code. e.g.: fr
+     * @param string $lang The lang code. e.g.: fr
      *
      * @return bool true if the language is used, otherwise false
      */
