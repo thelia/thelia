ec9727b66ba688d94d2cdebba39f01f39930247a
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2026-08-14 11:22:31 +0200

    chore: prepare Thelia 3.0.0-beta3 (#3750)

--- core/lib/Thelia/Core/TheliaKernel.php
+++ core/lib/Thelia/Core/TheliaKernel.php
@@ -85,7 +85,7 @@ class TheliaKernel extends Kernel
 {
     use MicroKernelTrait;
 
-    public const THELIA_VERSION = '3.0.0-beta1';
+    public const THELIA_VERSION = '3.0.0-beta3';
 
     protected SchemaLocator $propelSchemaLocator;
     protected PropelInitService $propelInitService;
