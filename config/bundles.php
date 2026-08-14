844291227a9467b479c3d428f7ebb67b8de58234
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2026-08-14 11:16:28 +0200

    Leave a fresh install able to serve its first page (#3749)
    * fix: stop declaring a bundle no dependency provides
    
    * fix: build the front-office assets during the install

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
-    Symfony\UX\React\ReactBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Translator\UxTranslatorBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
